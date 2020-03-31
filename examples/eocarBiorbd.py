af98727603ea8374b51b5148afec75d7d5e4f9d9
Pariterre <pariterre@hotmail.com>
2020-03-31 10:25:50 -0400

    fixed initial and final constraint for eocar

--- examples/eocarBiorbd.py
+++ examples/eocarBiorbd.py
@@ -1,25 +1,25 @@
 import biorbd
 from matplotlib import pyplot as plt
 
-import casadi_nlp
-from casadi_nlp.objective_functions import ObjectiveFunction
-from casadi_nlp.constraints import Constraint
-from casadi_nlp.dynamics import Dynamics
+import biorbd_optim
+from biorbd_optim.objective_functions import ObjectiveFunction
+from biorbd_optim.constraints import Constraint
+from biorbd_optim.dynamics import Dynamics
 
 # --- Options --- #
 # Model path
-biorbd_model = biorbd.Model("eocar.bioMod")
+biorbd_model = biorbd.Model("../models/eocar.bioMod")
 
 # Results path
 optimization_name = "eocarBiorbd"
-results_path = "Results/"
+results_path = "../Results/"
 control_results_file_name = results_path + "Controls" + optimization_name + ".txt"
 state_results_file_name = results_path + "States" + optimization_name + ".txt"
 
 # Problem parameters
 number_shooting_points = 30
 final_time = 2
-ode_solver = casadi_nlp.OdeSolver.RK
+ode_solver = biorbd_optim.OdeSolver.RK
 velocity_max = 15
 is_cyclic_constraint = False
 is_cyclic_objective = False
@@ -28,7 +28,7 @@ is_cyclic_objective = False
 objective_functions = ((ObjectiveFunction.minimize_torque, 100),)
 
 # Dynamics
-variable_type = casadi_nlp.Variable.variable_torque_driven
+variable_type = biorbd_optim.Variable.variable_torque_driven
 dynamics_func = Dynamics.forward_dynamics_torque_driven
 
 # Constraints
@@ -36,8 +36,8 @@ constraints = ((Constraint.Type.MARKERS_TO_PAIR, Constraint.Instant.START, (0, 1
                (Constraint.Type.MARKERS_TO_PAIR, Constraint.Instant.END, (0, 2)),)
 
 # Define path constraint
-X_bounds = casadi_nlp.Bounds()
-X_init = casadi_nlp.InitialConditions()
+X_bounds = biorbd_optim.Bounds()
+X_init = biorbd_optim.InitialConditions()
 ranges = []
 for i in range(biorbd_model.nbSegment()):
     segRanges = biorbd_model.segment(i).ranges()
@@ -45,20 +45,16 @@ for i in range(biorbd_model.nbSegment()):
         ranges.append(biorbd_model.segment(i).ranges()[j])
 
 for i in range(biorbd_model.nbQ()):
-    if i == 3:
-        X_bounds.first_node_min.append(0)
-        X_bounds.first_node_max.append(0)
-    else:
-        X_bounds.first_node_min.append(ranges[i].min())
-        X_bounds.first_node_max.append(ranges[i].max())
+    X_bounds.first_node_min.append(0)
+    X_bounds.first_node_max.append(0)
     X_bounds.min.append(ranges[i].min())
     X_bounds.max.append(ranges[i].max())
     if i == 3:
         X_bounds.last_node_min.append(1.57)
         X_bounds.last_node_max.append(1.57)
     else:
-        X_bounds.last_node_min.append(ranges[i].min())
-        X_bounds.last_node_max.append(ranges[i].max())
+        X_bounds.last_node_min.append(0)
+        X_bounds.last_node_max.append(0)
     X_init.init.append(0)
 for i in range(biorbd_model.nbQdot()):
     X_bounds.first_node_min.append(0)
@@ -69,8 +65,8 @@ for i in range(biorbd_model.nbQdot()):
     X_bounds.last_node_max.append(0)
     X_init.init.append(0)
 
-U_bounds = casadi_nlp.Bounds()
-U_init = casadi_nlp.InitialConditions()
+U_bounds = biorbd_optim.Bounds()
+U_init = biorbd_optim.InitialConditions()
 for i in range(biorbd_model.nbGeneralizedTorque()):
     U_bounds.min.append(-100)
     U_bounds.max.append(100)
@@ -78,7 +74,7 @@ for i in range(biorbd_model.nbGeneralizedTorque()):
 # ------------- #
 
 # --- Solve the program --- #
-nlp = casadi_nlp.OptimalControlProgram(
+nlp = biorbd_optim.OptimalControlProgram(
     biorbd_model, variable_type, dynamics_func, ode_solver, number_shooting_points, final_time,
     objective_functions, X_init, U_init, X_bounds, U_bounds,
     constraints, is_cyclic_constraint=is_cyclic_constraint, is_cyclic_objective=is_cyclic_objective)
