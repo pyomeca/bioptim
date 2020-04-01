58a5f98c23cdfa32a83b69557133fdc9bc65a6c0
paul.wegiel <paul-wegiel@orange.fr>
2020-04-01 19:03:23 -0400

    Adaptation of the layout of the objective function

--- biorbd_optim/objective_functions.py
+++ biorbd_optim/objective_functions.py
@@ -5,8 +5,10 @@ class ObjectiveFunction:
     @staticmethod
     def minimize_torque(nlp, weight=1):
         for i in range(nlp.ns):
-            nlp.J += casadi.dot(nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() + nlp.model.nbGeneralizedTorque()], \
-                                nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() + nlp.model.nbGeneralizedTorque()]) \
+            nlp.J += casadi.dot(nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() \
+                                + nlp.model.nbGeneralizedTorque()], \
+                                nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() \
+                                + nlp.model.nbGeneralizedTorque()]) \
                                 * nlp.dt * nlp.dt * weight
 
     @staticmethod
@@ -19,6 +21,10 @@ class ObjectiveFunction:
         for i in range(nlp.ns):
             nlp.J += casadi.dot(nlp.U[i][:nlp.model.nbMuscleTotal()], nlp.U[i][:nlp.model.nbMuscleTotal()]) \
                                 * nlp.dt * nlp.dt * weight
+
+    @staticmethod
+    def minimize_all_controls(nlp, weight=1):
+        raise RuntimeError("cyclic objective function not implemented yet")
 
     @staticmethod
     def cyclic(nlp, weight=1):
