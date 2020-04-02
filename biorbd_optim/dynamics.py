523efe43a25a2f7a8ea41662bc8174bd9a894370
Théophile <theophile.gousselot@gmail.com>
2020-04-02 16:37:59 -0400

    WIP(setActivation) forward dynmics muscle

--- biorbd_optim/dynamics.py
+++ biorbd_optim/dynamics.py
@@ -18,26 +18,23 @@ class Dynamics:
         qddot = biorbd.Model.ForwardDynamics(model, q, qdot, tau).to_mx()
         return vertcat(qdot, qddot)
 
-        # def forward_dynamics_torque_muscle_driven(states, controls, model):
-        #     muscles_states = []
-        #
-        #     for k in range (model.nbMuscleTotal()):
-        #         muscles_states.append(StateDynamics())
-        #
-        #
-        #     q = states[:model.nbQ()]
-        #     qdot = states[model.nbQ():]
-        #
-        #     if model.nbMuscleTotal() > 0:
-        #         for k in range (model.nbMuscleTotal()):
-        #             muscles_states[k].setActivation(controls[k])
-        #         Tau = model.muscularJointTorque(muscles_states, true, Q, QDot);
-        #
-        #     else:
-        #         Tau.setZero();
-        #
-        #     Tau += controls
-        #
-        #
-        #     qddot = biorbd.Model.ForwardDynamics(model, Q, QDot, Tau).to_mx()
-        #     return vertcat(qdot, qddot)
+    def forward_dynamics_torque_muscle_driven(states, controls, model):
+        muscles_states = []
+
+        for k in range (model.nbMuscleTotal()):
+            muscles_states.append(biorbd.StateDynamics)
+
+        q = states[:model.nbQ()]
+        qdot = states[model.nbQ():]
+
+        if model.nbMuscleTotal() > 0:
+            for k in range (model.nbMuscleTotal()):
+                muscles_states[k].setActivation(biorbd.StateDynamics, controls[k])
+            Tau = model.muscularJointTorque(muscles_states, true, Q, QDot)
+        else:
+            Tau.setZero()
+
+        Tau += controls
+
+        qddot = biorbd.Model.ForwardDynamics(model, Q, QDot, Tau).to_mx()
+        return vertcat(qdot, qddot)
