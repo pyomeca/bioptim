799879df18e463a24bb47d1e3961a38b23c2dd17
paul.wegiel <paul-wegiel@orange.fr>
2020-04-02 09:28:25 -0400

    Corrected according to comments

--- biorbd_optim/objective_functions.py
+++ biorbd_optim/objective_functions.py
@@ -5,11 +5,10 @@ class ObjectiveFunction:
     @staticmethod
     def minimize_torque(nlp, weight=1):
         for i in range(nlp.ns):
-            nlp.J += casadi.dot(nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() \
-                                + nlp.model.nbGeneralizedTorque()], \
-                                nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() \
-                                + nlp.model.nbGeneralizedTorque()]) \
-                                * nlp.dt * nlp.dt * weight
+            nlp.J += casadi.dot(
+                nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() + nlp.model.nbGeneralizedTorque()],
+                nlp.U[i][nlp.model.nbMuscleTotal():nlp.model.nbMuscleTotal() + nlp.model.nbGeneralizedTorque()]) \
+                     * nlp.dt * nlp.dt * weight
 
     @staticmethod
     def minimize_states(nlp, weight=1):
