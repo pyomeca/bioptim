fa3ef7787e9c4fef673b8ffde597e157ac0cf942
Théophile <theophile.gousselot@gmail.com>
2020-04-09 18:21:33 -0400

    Adapt eocar with new problem type.

--- examples/torque_driven_ocp/eocar.py
+++ examples/torque_driven_ocp/eocar.py
@@ -1,5 +1,5 @@
-import numpy as np
 import biorbd
+import numpy as np
 from matplotlib import pyplot as plt
 
 from biorbd_optim import OptimalControlProgram
@@ -75,16 +75,15 @@ if __name__ == "__main__":
     sol = ocp.solve()
 
     nlp = ocp.nlp[0]
+
+    q, q_dot, u = ProblemType.get_data_from_V(ocp, sol["x"])
+
     for idx in range(nlp["model"].nbQ()):
         plt.figure()
-        q = sol["x"][0 * nlp["model"].nbQ() + idx :: 3 * nlp["model"].nbQ()]
-        q_dot = sol["x"][1 * nlp["model"].nbQ() + idx :: 3 * nlp["model"].nbQ()]
-        u = np.array(sol["x"][2 * nlp["model"].nbQ() + idx :: 3 * nlp["model"].nbQ()])
-        u = np.append(u, u[-1])
         t = np.linspace(0, nlp["tf"], nlp["ns"] + 1)
-        plt.plot(t, q, label=nlp["x"][idx * 2])
-        plt.plot(t, q_dot, label=nlp["x"][1 + idx * 2])
-        plt.step(t, u, label=nlp["x"][idx], where="post")
+        plt.plot(t, q[idx], label=nlp["x"][idx * 2])
+        plt.plot(t, q_dot[idx], label=nlp["x"][1 + idx * 2])
+        plt.step(t, u[idx], label=nlp["x"][idx], where="post")
         plt.title("DoF : " + nlp["model"].nameDof()[idx].to_string())
 
     plt.legend()
