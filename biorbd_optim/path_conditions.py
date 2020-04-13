c32ba0ff486ca00eb687fe1b574b2496eaa650f7
Théophile <theophile.gousselot@gmail.com>
2020-04-13 10:38:22 -0400

    Adds detail in regulation error (size of list).

--- biorbd_optim/path_conditions.py
+++ biorbd_optim/path_conditions.py
@@ -14,7 +14,7 @@ class PathCondition:
     @staticmethod
     def regulation_private(var, nb_elements, type):
         if len(var) != nb_elements:
-            raise RuntimeError(f"Invalid number of {type}")
+            raise RuntimeError(f"Invalid number of {type} " + str(len(var)) + " != " + str(nb_elements))
 
     def expand(self, other):
         pass
