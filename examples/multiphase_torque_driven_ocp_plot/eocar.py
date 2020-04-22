d329f06470b34b11e391324f1ca72e23bff10ad1
Théophile <theophile.gousselot@gmail.com>
2020-04-22 12:11:39 -0400

    Fix objective_function calls.

--- examples/multiphase_torque_driven_ocp_plot/eocar.py
+++ examples/multiphase_torque_driven_ocp_plot/eocar.py
@@ -20,7 +20,7 @@ def prepare_ocp(biorbd_model_path="eocar.bioMod", show_online_optim=True):
 
     # Add objective functions
     objective_functions = (
-        {"weight": 100},
+        {"type": ObjectiveFunction.minimize_torque, "weight": 100},
         {"type": ObjectiveFunction.minimize_torque, "weight": 100},
     )
 
