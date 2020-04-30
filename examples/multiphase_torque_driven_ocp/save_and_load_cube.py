a7013deaf5ed92f1d21e624762716054a2987685
Théophile <theophile.gousselot@gmail.com>
2020-04-30 10:03:51 -0400

    Add example of load/save

--- examples/multiphase_torque_driven_ocp/save_and_load_cube.py
+++ examples/multiphase_torque_driven_ocp/save_and_load_cube.py
@@ -5,25 +5,21 @@ from biorbd_optim import OptimalControlProgram, ShowResult
 
 # Load eocar
 PROJECT_FOLDER = Path(__file__).parent
-spec = importlib.util.spec_from_file_location(
-    "eocar", str(PROJECT_FOLDER) + "/eocar.py"
-)
+spec = importlib.util.spec_from_file_location("eocar", str(PROJECT_FOLDER) + "/eocar.py")
 eocar = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(eocar)
 
 
 def run_and_save_ocp():
-    ocp = eocar.prepare_ocp(
-        biorbd_model_path=str(PROJECT_FOLDER) + "/eocar.bioMod",
-    )
+    ocp = eocar.prepare_ocp(biorbd_model_path=str(PROJECT_FOLDER) + "/cube.bioMod",)
     sol = ocp.solve()
-    OptimalControlProgram.save(ocp, sol, "eocar_ocp_sol")
+    OptimalControlProgram.save(ocp, sol, "cube_ocp_sol")
 
 
 if __name__ == "__main__":
     run_and_save_ocp()
 
-    ocp, sol = OptimalControlProgram.load(biorbd_model_path="eocar.bioMod", name="eocar_ocp_sol")
+    ocp, sol = OptimalControlProgram.load(biorbd_model_path="cube.bioMod", name="cube_ocp_sol.bo")
     result = ShowResult(ocp, sol)
     result.graphs()
     result.animate(40)
