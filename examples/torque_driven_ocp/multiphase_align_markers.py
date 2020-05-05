353bcc2870ed14e8a6845d82a1da90e618e4a5f6
pariterre <pariterre@hotmail.com>
2020-05-05 17:40:26 -0400

    Made multiphase 3 phases for better testing

--- examples/torque_driven_ocp/multiphase_align_markers.py
+++ examples/torque_driven_ocp/multiphase_align_markers.py
@@ -17,27 +17,31 @@ from biorbd_optim import (
 def prepare_ocp(biorbd_model_path="cube.bioMod", show_online_optim=False, ode_solver=OdeSolver.RK, long_optim=False):
     # --- Options --- #
     # Model path
-    biorbd_model = (biorbd.Model(biorbd_model_path), biorbd.Model(biorbd_model_path))
+    biorbd_model = (biorbd.Model(biorbd_model_path), biorbd.Model(biorbd_model_path), biorbd.Model(biorbd_model_path))
 
     # Problem parameters
     if long_optim:
-        number_shooting_points = (100, 1000)
+        number_shooting_points = (100, 1000, 100)
     else:
-        number_shooting_points = (20, 30)
-    final_time = (2, 5)
+        number_shooting_points = (20, 30, 20)
+    final_time = (2, 5, 4)
     torque_min, torque_max, torque_init = -100, 100, 0
 
     # Add objective functions
     objective_functions = (
-        ({"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 100},),
         (
             {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 100},
-            {"type": Objective.Mayer.MINIMIZE_TIME, "maximum": 6000, "minimum":0, "weight": 100},
+        ),
+        (
+            {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 100},
+        ),
+        (
+            {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 100},
         ),
     )
 
     # Dynamics
-    variable_type = (ProblemType.torque_driven, ProblemType.torque_driven)
+    variable_type = (ProblemType.torque_driven, ProblemType.torque_driven, ProblemType.torque_driven)
 
     # Constraints
     constraints = (
@@ -51,10 +55,11 @@ def prepare_ocp(biorbd_model_path="cube.bioMod", show_online_optim=False, ode_so
             {"type": Constraint.ALIGN_MARKERS, "instant": Instant.END, "first_marker_idx": 0, "second_marker_idx": 2,},
         ),
         ({"type": Constraint.ALIGN_MARKERS, "instant": Instant.END, "first_marker_idx": 0, "second_marker_idx": 1,},),
+        ({"type": Constraint.ALIGN_MARKERS, "instant": Instant.END, "first_marker_idx": 0, "second_marker_idx": 2,},),
     )
 
     # Path constraint
-    X_bounds = [QAndQDotBounds(biorbd_model[0]), QAndQDotBounds(biorbd_model[0])]
+    X_bounds = [QAndQDotBounds(biorbd_model[0]), QAndQDotBounds(biorbd_model[0]), QAndQDotBounds(biorbd_model[0])]
 
     for bounds in X_bounds:
         for i in range(6):
@@ -65,16 +70,19 @@ def prepare_ocp(biorbd_model_path="cube.bioMod", show_online_optim=False, ode_so
                 bounds.last_node_max[i] = 0
     X_bounds[0].first_node_min[2] = 0.0
     X_bounds[0].first_node_max[2] = 0.0
-    X_bounds[1].first_node_min[2] = 0.0
-    X_bounds[1].first_node_max[2] = 0.0
-    X_bounds[1].last_node_min[2] = 1.57
-    X_bounds[1].last_node_max[2] = 1.57
+    X_bounds[2].first_node_min[2] = 0.0
+    X_bounds[2].first_node_max[2] = 0.0
+    X_bounds[2].last_node_min[2] = 1.57
+    X_bounds[2].last_node_max[2] = 1.57
 
     # Initial guess
     X_init = InitialConditions([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
 
     # Define control path constraint
     U_bounds = [
+        Bounds(
+            [torque_min] * biorbd_model[0].nbGeneralizedTorque(), [torque_max] * biorbd_model[0].nbGeneralizedTorque(),
+        ),
         Bounds(
             [torque_min] * biorbd_model[0].nbGeneralizedTorque(), [torque_max] * biorbd_model[0].nbGeneralizedTorque(),
         ),
@@ -92,8 +100,8 @@ def prepare_ocp(biorbd_model_path="cube.bioMod", show_online_optim=False, ode_so
         number_shooting_points,
         final_time,
         objective_functions,
-        (X_init, X_init),
-        (U_init, U_init),
+        (X_init, X_init, X_init),
+        (U_init, U_init, U_init),
         X_bounds,
         U_bounds,
         constraints,
