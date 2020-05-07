bcbd66b1587e7a9c42f6857d4e719c73e1cc36f0
pariterre <pariterre@hotmail.com>
2020-05-07 17:13:05 -0400

    Used constraint instead of mapping

--- examples/getting_started/pendulum.py
+++ examples/getting_started/pendulum.py
@@ -17,12 +17,14 @@ def prepare_ocp(
     biorbd_model_path, final_time, number_shooting_points, show_online_optim=False
 ):
     # --- Options --- #
-    # Model path
     biorbd_model = biorbd.Model(biorbd_model_path)
     torque_min, torque_max, torque_init = -100, 100, 0
+    n_q = biorbd_model.nbQ()
+    n_qdot = biorbd_model.nbQdot()
+    n_tau = biorbd_model.nbGeneralizedTorque()
 
     # Add objective functions
-    objective_functions = ()
+    objective_functions = ({"type": Objective.Lagrange.MINIMIZE_TORQUE})
 
     # Dynamics
     problem_type = ProblemType.torque_driven
@@ -32,18 +34,26 @@ def prepare_ocp(
 
     # Path constraint
     X_bounds = QAndQDotBounds(biorbd_model)
-    X_bounds.first_node_min = [0, 0, 0, 0, 0, 0]
-    X_bounds.first_node_max = [0, 0, 0, 0, 0, 0]
-    X_bounds.last_node_min = [0, 0, 3.14, 0, 0, 0]
-    X_bounds.last_node_max = [0, 0, 3.14, 0, 0, 0]
+    X_bounds.first_node_min = [0] * (n_q + n_qdot)
+    X_bounds.first_node_max = [0] * (n_q + n_qdot)
+    X_bounds.last_node_min = [0] * (n_q + n_qdot)
+    X_bounds.last_node_max = [0] * (n_q + n_qdot)
+    X_bounds.last_node_min[n_q - 1] = 3.14
+    X_bounds.last_node_max[n_q - 1] = 3.14
 
     # Initial guess
-    X_init = InitialConditions([0, 0, 0, 0, 0, 0])
+    X_init = InitialConditions([0] * (n_q + n_qdot))
 
     # Define control path constraint
-    U_bounds = Bounds(min_bound=[torque_min, 0, 0], max_bound=[torque_max, 0, 0])
+    U_bounds = Bounds(min_bound=[torque_min] * n_tau, max_bound=[torque_max] * n_tau)
+    U_bounds.first_node_min[n_tau - 1] = 0
+    U_bounds.first_node_max[n_tau - 1] = 0
+    U_bounds.min[n_tau - 1] = 0
+    U_bounds.max[n_tau - 1] = 0
+    U_bounds.last_node_min[n_tau - 1] = 0
+    U_bounds.last_node_max[n_tau - 1] = 0
 
-    U_init = InitialConditions([torque_init, 0, 0])
+    U_init = InitialConditions([torque_init] * n_tau)
 
     # ------------- #
 
