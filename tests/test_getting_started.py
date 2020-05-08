43b180cc10bbb0a167292928859ff4d23e09324d
Ariane <dang.ariane@gmail.com>
2020-05-08 11:17:51 -0400

    Changed number of shooting point to 10 for pendulum tests

--- tests/test_getting_started.py
+++ tests/test_getting_started.py
@@ -21,7 +21,7 @@ def test_pendulum(ode_solver):
     ocp = pendulum.prepare_ocp(
         biorbd_model_path=str(PROJECT_FOLDER) + "/examples/getting_started/pendulum.bioMod",
         final_time=2,
-        number_shooting_points=50,
+        number_shooting_points=10,
         show_online_optim=False,
     )
     sol = ocp.solve()
@@ -29,12 +29,12 @@ def test_pendulum(ode_solver):
     # Check objective function value
     f = np.array(sol["f"])
     np.testing.assert_equal(f.shape, (1, 1))
-    np.testing.assert_almost_equal(f[0, 0], 1.5970062033278107)
+    np.testing.assert_almost_equal(f[0, 0], 0.0)
 
     # Check constraints
     g = np.array(sol["g"])
-    np.testing.assert_equal(g.shape, (200, 1))
-    np.testing.assert_almost_equal(g, np.zeros((200, 1)), decimal=-6)
+    np.testing.assert_equal(g.shape, (40, 1))
+    np.testing.assert_almost_equal(g, np.zeros((40, 1)))
 
     # Check some of the results
     states, controls = Data.get_data_from_V(ocp, sol["x"])
@@ -51,8 +51,8 @@ def test_pendulum(ode_solver):
     np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
 
     # initial and final controls
-    np.testing.assert_almost_equal(tau[:, 0], np.array((-5.9809981)))
-    np.testing.assert_almost_equal(tau[:, -1], np.array((-11.29898999)))
+    np.testing.assert_almost_equal(tau[:, 0], np.array((9.55000247, 0)))
+    np.testing.assert_almost_equal(tau[:, -1], np.array((-21.18945819, 0)))
 
 
 # Load pendulum_min_time_Mayer
@@ -69,7 +69,7 @@ def test_pendulum_min_time_mayer(ode_solver):
     ocp = pendulum_min_time_Mayer.prepare_ocp(
         biorbd_model_path=str(PROJECT_FOLDER) + "/examples/getting_started/pendulum.bioMod",
         final_time=2,
-        number_shooting_points=50,
+        number_shooting_points=10,
         show_online_optim=False,
     )
     sol = ocp.solve()
@@ -77,12 +77,12 @@ def test_pendulum_min_time_mayer(ode_solver):
     # Check objective function value
     f = np.array(sol["f"])
     np.testing.assert_equal(f.shape, (1, 1))
-    np.testing.assert_almost_equal(f[0, 0], 2.18186597955319)
+    np.testing.assert_almost_equal(f[0, 0], 0.6209213032003106)
 
     # Check constraints
     g = np.array(sol["g"])
-    np.testing.assert_equal(g.shape, (200, 1))
-    np.testing.assert_almost_equal(g, np.zeros((200, 1)), decimal=-6)
+    np.testing.assert_equal(g.shape, (40, 1))
+    np.testing.assert_almost_equal(g, np.zeros((40, 1)))
 
     # Check some of the results
     states, controls, param = Data.get_data_from_V(ocp, sol["x"], get_parameters=True)
@@ -100,11 +100,11 @@ def test_pendulum_min_time_mayer(ode_solver):
     np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
 
     # initial and final controls
-    np.testing.assert_almost_equal(tau[:, 0], np.array((-0.5898199)))
-    np.testing.assert_almost_equal(tau[:, -1], np.array((-9.39463562)))
+    np.testing.assert_almost_equal(tau[:, 0], np.array((59.95450138, 0)))
+    np.testing.assert_almost_equal(tau[:, -1], np.array((-99.99980141, 0)))
 
     # optimized time
-    np.testing.assert_almost_equal(tf, 1.5491434695819155)
+    np.testing.assert_almost_equal(tf, 0.6209213032003106)
 
 
 # Load pendulum_min_time_Lagrange
@@ -121,7 +121,7 @@ def test_pendulum_min_time_lagrange(ode_solver):
     ocp = pendulum_min_time_Lagrange.prepare_ocp(
         biorbd_model_path=str(PROJECT_FOLDER) + "/examples/getting_started/pendulum.bioMod",
         final_time=2,
-        number_shooting_points=50,
+        number_shooting_points=10,
         show_online_optim=False,
     )
     sol = ocp.solve()
@@ -129,12 +129,12 @@ def test_pendulum_min_time_lagrange(ode_solver):
     # Check objective function value
     f = np.array(sol["f"])
     np.testing.assert_equal(f.shape, (1, 1))
-    np.testing.assert_almost_equal(f[0, 0], 0.6082229673376528)
+    np.testing.assert_almost_equal(f[0, 0], 0.003628839798820405)
 
     # Check constraints
     g = np.array(sol["g"])
-    np.testing.assert_equal(g.shape, (200, 1))
-    np.testing.assert_almost_equal(g, np.zeros((200, 1)), decimal=-6)
+    np.testing.assert_equal(g.shape, (40, 1))
+    np.testing.assert_almost_equal(g, np.zeros((40, 1)), decimal=-6)
 
     # Check some of the results
     states, controls, param = Data.get_data_from_V(ocp, sol["x"], get_parameters=True)
@@ -152,8 +152,8 @@ def test_pendulum_min_time_lagrange(ode_solver):
     np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
 
     # initial and final controls
-    np.testing.assert_almost_equal(tau[:, 0], np.array((-3.7492435)))
-    np.testing.assert_almost_equal(tau[:, -1], np.array((-4.69850209)))
+    np.testing.assert_almost_equal(tau[:, 0], np.array((-20.36604829, 0)))
+    np.testing.assert_almost_equal(tau[:, -1], np.array((-99.98554494, 0)))
 
     # optimized time
-    np.testing.assert_almost_equal(tf, 1.6540746733051912)
+    np.testing.assert_almost_equal(tf, 0.6023985224766413)
