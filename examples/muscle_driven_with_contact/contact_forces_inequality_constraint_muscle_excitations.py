d2cf9921890299ec5b58ae97ef3077df553a43bd
Lea Sanchez <lea.sanchez@umontreal.ca>
2020-05-14 12:09:33 -0400

    Added example using muscle excitation with contact

---
+++ examples/muscle_driven_with_contact/contact_forces_inequality_constraint_muscle_excitations.py
@@ -0,0 +1,140 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import biorbd
+
+from biorbd_optim import (
+    Instant,
+    OptimalControlProgram,
+    Constraint,
+    Objective,
+    ProblemType,
+    BidirectionalMapping,
+    Mapping,
+    Bounds,
+    QAndQDotBounds,
+    InitialConditions,
+    ShowResult,
+    Data,
+)
+
+
+def prepare_ocp(model_path, phase_time, number_shooting_points, direction, boundary, show_online_optim=False):
+    # --- Options --- #
+    # Model path
+    biorbd_model = biorbd.Model(model_path)
+    torque_min, torque_max, torque_init = -500, 500, 0
+    activation_min, activation_max, activation_init = 0, 1, 0.5
+    tau_mapping = BidirectionalMapping(Mapping([-1, -1, -1, 0]), Mapping([3]))
+
+    # Add objective functions
+    objective_functions = ({"type": Objective.Mayer.MINIMIZE_PREDICTED_COM_HEIGHT, "weight": -1},)
+
+    # Dynamics
+    problem_type = ProblemType.muscle_excitations_and_torque_driven_with_contact
+
+    # Constraints
+    constraints = (
+        {
+            "type": Constraint.CONTACT_FORCE_INEQUALITY,
+            "direction": direction,
+            "instant": Instant.ALL,
+            "contact_force_idx": 1,
+            "boundary": boundary,
+        },
+        {
+            "type": Constraint.CONTACT_FORCE_INEQUALITY,
+            "direction": direction,
+            "instant": Instant.ALL,
+            "contact_force_idx": 2,
+            "boundary": boundary,
+        },
+    )
+
+    # Path constraint
+    nb_q = biorbd_model.nbQ()
+    nb_qdot = nb_q
+    nb_mus = biorbd_model.nbMuscleTotal()
+    pose_at_first_node = [0, 0, -0.75, 0.75]
+
+    # Initialize X_bounds
+    X_bounds = QAndQDotBounds(biorbd_model)
+    X_bounds.concatenate(
+        Bounds([activation_min] * nb_mus, [activation_max] * nb_mus)
+    )
+    X_bounds.min[:, 0] = pose_at_first_node + [0] * nb_qdot + [0.5] * nb_mus
+    X_bounds.max[:, 0] = pose_at_first_node + [0] * nb_qdot + [0.5] * nb_mus
+
+    # Initial guess
+    X_init = [InitialConditions(pose_at_first_node + [0] * nb_qdot + [0.5] * nb_mus)]
+
+    # Define control path constraint
+    U_bounds = [
+        Bounds(
+            min_bound=[torque_min] * tau_mapping.reduce.len + [activation_min] * biorbd_model.nbMuscleTotal(),
+            max_bound=[torque_max] * tau_mapping.reduce.len + [activation_max] * biorbd_model.nbMuscleTotal(),
+        )
+    ]
+
+    U_init = [
+        InitialConditions([torque_init] * tau_mapping.reduce.len + [activation_init] * biorbd_model.nbMuscleTotal())
+    ]
+    # ------------- #
+
+    return OptimalControlProgram(
+        biorbd_model,
+        problem_type,
+        number_shooting_points,
+        phase_time,
+        objective_functions,
+        X_init,
+        U_init,
+        X_bounds,
+        U_bounds,
+        constraints,
+        tau_mapping=tau_mapping,
+        show_online_optim=show_online_optim,
+    )
+
+
+if __name__ == "__main__":
+    model_path = "2segments_4dof_2contacts_1muscle.bioMod"
+    t = 0.3
+    ns = 10
+    ocp = prepare_ocp(
+        model_path=model_path,
+        phase_time=t,
+        number_shooting_points=ns,
+        direction="GREATER_THAN",
+        boundary=50,
+        show_online_optim=False,
+    )
+
+    # --- Solve the program --- #
+    sol = ocp.solve()
+
+    nlp = ocp.nlp[0]
+    nlp["model"] = biorbd.Model(model_path)
+
+    states_sol, controls_sol = Data.get_data(ocp, sol["x"])
+    q = states_sol["q"]
+    q_dot = states_sol["q_dot"]
+    activations = states_sol["muscles"]
+    tau = controls_sol["tau"]
+    excitations = controls_sol["muscles"]
+
+    x = np.concatenate((q, q_dot, activations))
+    u = np.concatenate((tau, excitations))
+    contact_forces = np.array(nlp["contact_forces_func"](x[:, :-1], u[:, :-1]))
+
+    names_contact_forces = ocp.nlp[0]["model"].contactNames()
+    for i, elt in enumerate(contact_forces):
+        plt.plot(np.linspace(0, t, ns + 1)[:-1], elt, ".-", label=f"{names_contact_forces[i].to_string()}")
+    plt.legend()
+    plt.grid()
+    plt.title("Contact forces")
+    plt.show()
+
+    # --- Show results --- #
+    result = ShowResult(ocp, sol)
+    result.animate()
+    result.graphs()
