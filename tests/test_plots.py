4454899b1f8fed68b7971db438386644226a918e
pariterre <pariterre@hotmail.com>
2020-05-15 07:21:04 -0400

    Starting to test plots

---
+++ tests/test_plots.py
@@ -0,0 +1,28 @@
+"""
+Test for file IO
+"""
+import importlib.util
+from pathlib import Path
+
+import pytest
+import numpy as np
+
+from biorbd_optim import Data, OdeSolver, ShowResult
+
+# Load align_markers
+PROJECT_FOLDER = Path(__file__).parent / ".."
+spec = importlib.util.spec_from_file_location(
+    "align_markers", str(PROJECT_FOLDER) + "/examples/torque_driven_ocp/align_markers.py"
+)
+align_markers = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(align_markers)
+
+
+def test_plot_graphs_one_phase():
+    ocp = align_markers.prepare_ocp(
+        biorbd_model_path=str(PROJECT_FOLDER) + "/examples/torque_driven_ocp/cube.bioMod"
+    )
+    sol = ocp.solve()
+
+    plt = ShowResult(ocp, sol)
+    plt.graphs()
