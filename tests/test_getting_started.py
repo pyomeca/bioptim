ae97cf6e254fcce57bf449a92eeb22a305398e8f
Théophile <theophile.gousselot@gmail.com>
2020-05-19 10:10:41 -0400

    adapts examples to merge

--- tests/test_getting_started.py
+++ tests/test_getting_started.py
@@ -11,6 +11,7 @@ from biorbd_optim import (
     Data,
     OdeSolver,
     Tests,
+    InterpolationType,
 )
 
 # Load pendulum
@@ -111,59 +112,6 @@ def test_pendulum_min_time_mayer(ode_solver):
 
     # save and load
     Tests.save_and_load(sol, ocp, "ocp_sol_bo/pendulum_min_time_mayer", False)
-
-
-# Load pendulum_min_time_Lagrange
-PROJECT_FOLDER = Path(__file__).parent / ".."
-spec = importlib.util.spec_from_file_location(
-    "pendulum_min_time_Lagrange", str(PROJECT_FOLDER) + "/examples/getting_started/pendulum_min_time_Lagrange.py",
-)
-pendulum_min_time_Lagrange = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(pendulum_min_time_Lagrange)
-
-
-@pytest.mark.parametrize("ode_solver", [OdeSolver.RK])
-def test_pendulum_min_time_lagrange(ode_solver):
-    ocp = pendulum_min_time_Lagrange.prepare_ocp(
-        biorbd_model_path=str(PROJECT_FOLDER) + "/examples/getting_started/pendulum.bioMod",
-        final_time=2,
-        number_shooting_points=10,
-        show_online_optim=False,
-    )
-    sol = ocp.solve()
-
-    # Check objective function value
-    f = np.array(sol["f"])
-    np.testing.assert_equal(f.shape, (1, 1))
-    np.testing.assert_almost_equal(f[0, 0], 0.003628839798820405)
-
-    # Check constraints
-    g = np.array(sol["g"])
-    np.testing.assert_equal(g.shape, (40, 1))
-    np.testing.assert_almost_equal(g, np.zeros((40, 1)), decimal=-6)
-
-    # Check some of the results
-    states, controls, param = Data.get_data(ocp, sol["x"], get_parameters=True)
-    q, qdot, tau = states["q"], states["q_dot"], controls["tau"]
-    tf = param["time"][0, 0]
-
-    # initial and final position
-    np.testing.assert_almost_equal(q[:, 0], np.array((0, 0)))
-    np.testing.assert_almost_equal(q[:, -1], np.array((0, 3.14)))
-
-    # initial and final velocities
-    np.testing.assert_almost_equal(qdot[:, 0], np.array((0, 0)))
-    np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
-
-    # initial and final controls
-    np.testing.assert_almost_equal(tau[:, 0], np.array((-20.36604829, 0)))
-    np.testing.assert_almost_equal(tau[:, -1], np.array((-99.98554494, 0)))
-
-    # optimized time
-    np.testing.assert_almost_equal(tf, 0.6023985224766413)
-
-    # save and load
-    Tests.save_and_load(sol, ocp, "ocp_sol_bo/pendulum_min_time_lagrange", True)
 
 
 # Load custom_constraint
