160d64b20e1438be20f584a4f0ecadabbccd16b9
EveCharbie <eve.charbonneau.1@umontreal.ca>
2020-05-28 14:18:39 -0400

    Corrections on minimize_markers_displacement
    Made function more unified, corrected minimize_markers_velocity and added example.

--- biorbd_optim/penalty.py
+++ biorbd_optim/penalty.py
@@ -42,38 +42,37 @@ class PenaltyFunctionAbstract:
                 penalty_type._add_to_penalty(ocp, nlp, val, **extra_param)
 
         @staticmethod
-        def minimize_markers_displacement(penalty_type, ocp, nlp, t, x, u, rt_idx=-1, markers_idx=(), **extra_param):
+        def minimize_markers_displacement(penalty_type, ocp, nlp, t, x, u, coordinates_system_idx=-1, markers_idx=(),
+                                          **extra_param):
             n_q = nlp["nbQ"]
             nb_rts = nlp["model"].nbSegment()
             markers_idx = PenaltyFunctionAbstract._check_and_fill_index(
                 markers_idx, nlp["model"].nbMarkers(), "markers_idx"
             )
 
-            if rt_idx < 0:
-                for i in range(len(x) - 1):
-                    val = (
-                        nlp["model"].markers(x[i + 1][:n_q])[:, markers_idx]
-                        - nlp["model"].markers(x[i][:n_q])[:, markers_idx]
-                    )
-                    penalty_type._add_to_penalty(ocp, nlp, val, **extra_param)
-            elif rt_idx < nb_rts:
-                for i in range(len(x) - 1):
-                    jcs_1 = nlp["model"].globalJCS(x[i + 1][:n_q], rt_idx).to_mx()
+            for i in range(len(x) - 1):
+                if coordinates_system_idx < 0:
+                    inv_jcs_1 = casadi.MX.eye(4)
+                    inv_jcs_0 = casadi.MX.eye(4)
+                elif coordinates_system_idx < nb_rts:
+                    jcs_1 = nlp["model"].globalJCS(x[i + 1][:n_q], coordinates_system_idx).to_mx()
                     inv_jcs_1 = casadi.vertcat(
                         casadi.horzcat(jcs_1[:3, :3], -jcs_1[:3, :3] @ jcs_1[:3, 3]), casadi.horzcat(0, 0, 0, 1)
                     )
-                    jcs_0 = nlp["model"].globalJCS(x[i][:n_q], rt_idx).to_mx()
+                    jcs_0 = nlp["model"].globalJCS(x[i][:n_q], coordinates_system_idx).to_mx()
                     inv_jcs_0 = casadi.vertcat(
                         casadi.horzcat(jcs_0[:3, :3], -jcs_0[:3, :3] @ jcs_0[:3, 3]), casadi.horzcat(0, 0, 0, 1)
                     )
-                    val = inv_jcs_1 @ casadi.vertcat(
-                        nlp["model"].markers(x[i + 1][:n_q])[:, markers_idx], 1
-                    ) - inv_jcs_0 @ casadi.vertcat(nlp["model"].markers(x[i][:n_q])[:, markers_idx], 1)
-                    penalty_type._add_to_penalty(ocp, nlp, val[:3], **extra_param)
-            else:
-                raise RuntimeError(
-                    f"Wrong choice of rt_idx. (Negative values refer to global coordinates system, positive values must be between 0 and {nb_rts})"
-                )
+                else:
+                    raise RuntimeError(
+                        f"Wrong choice of coordinates_system_idx. (Negative values refer to global coordinates system, "
+                        f"positive values must be between 0 and {nb_rts})"
+                    )
+                val = inv_jcs_1 @ casadi.vertcat(
+                    nlp["model"].markers(x[i + 1][:n_q])[:, markers_idx], 1
+                ) - inv_jcs_0 @ casadi.vertcat(nlp["model"].markers(x[i][:n_q])[:, markers_idx], 1)
+                penalty_type._add_to_penalty(ocp, nlp, val[:3], **extra_param)
+
 
         @staticmethod
         def minimize_markers_velocity(penalty_type, ocp, nlp, t, x, u, markers_idx=(), data_to_track=(), **extra_param):
@@ -86,12 +85,14 @@ class PenaltyFunctionAbstract:
                 data_to_track, [3, max(markers_idx) + 1, nlp["ns"] + 1]
             )
 
-            for i, v in enumerate(x):
-                val = (
-                    nlp["model"].markerVelocity(v[:n_q], v[n_q : n_q + n_qdot], markers_idx).to_mx()
-                    - data_to_track[:, markers_idx, t[i]]
-                )
-                penalty_type._add_to_penalty(ocp, nlp, val, **extra_param)
+            for m in markers_idx:
+                print(m)
+                for i, v in enumerate(x):
+                    val = (
+                        nlp["model"].markerVelocity(v[:n_q], v[n_q : n_q + n_qdot], m).to_mx()
+                        - data_to_track[:, markers_idx, t[i]]
+                    )
+                    penalty_type._add_to_penalty(ocp, nlp, val, **extra_param)
 
         @staticmethod
         def align_markers(penalty_type, ocp, nlp, t, x, u, first_marker_idx, second_marker_idx, **extra_param):
---
+++ examples/align/align_and_minimize_marker_velocity.py
@@ -0,0 +1,113 @@
+import biorbd
+
+from IPython import embed
+
+from biorbd_optim import (
+    Instant,
+    Axe,
+    OptimalControlProgram,
+    ProblemType,
+    Objective,
+    Constraint,
+    Bounds,
+    QAndQDotBounds,
+    InitialConditions,
+    ShowResult,
+    OdeSolver,
+)
+
+
+def prepare_ocp(biorbd_model_path, final_time, number_shooting_points, ode_solver, marker_velocity_or_displacement,
+                marker_in_first_coordinates_system):
+    # --- Options --- #
+    # Model path
+    biorbd_model = biorbd.Model(biorbd_model_path)
+    nq = biorbd_model.nbQ()
+    biorbd_model.markerNames()
+    # Problem parameters
+    torque_min, torque_max, torque_init = -100, 100, 0
+
+    # Add objective functions
+    if marker_in_first_coordinates_system:
+        coordinates_system_idx = 0
+    else:
+        coordinates_system_idx = -1
+
+    if marker_velocity_or_displacement == "disp":
+        objective_functions = (
+            {"type": Objective.Lagrange.MINIMIZE_MARKERS_DISPLACEMENT, "coordinates_system_idx": coordinates_system_idx,
+             "markers_idx": 6, "weight": 1000},
+            {"type": Objective.Lagrange.MINIMIZE_STATE, "states_idx": 6,
+             "weight": -1},
+            {"type": Objective.Lagrange.MINIMIZE_STATE, "states_idx": 7,
+             "weight": -1},
+        )
+    elif marker_velocity_or_displacement == "velo":
+        objective_functions = (
+            {"type": Objective.Lagrange.MINIMIZE_MARKERS_VELOCITY, "markers_idx": 6, "weight": 1000},
+            {"type": Objective.Lagrange.MINIMIZE_STATE, "states_idx": 6,
+             "weight": -1},
+            {"type": Objective.Lagrange.MINIMIZE_STATE, "states_idx": 7,
+             "weight": -1},
+        )
+    else:
+        raise RuntimeError("Wrong choice of marker_velocity_or_displacement, actual value is "
+                           "{marker_velocity_or_displacement}, should be 'velo' or 'disp'.")
+
+    # Dynamics
+    problem_type = ProblemType.torque_driven
+
+    # Path constraint
+    X_bounds = QAndQDotBounds(biorbd_model)
+
+    for i in range(nq, 2*nq):
+        X_bounds.min[i, :] = -10
+        X_bounds.max[i, :] = 10
+
+    # Initial guess
+    X_init = InitialConditions([0] * (biorbd_model.nbQ() + biorbd_model.nbQdot()))
+    for i in range(2):
+        X_init.init[i] = 1.5
+    for i in range(4, 6):
+        X_init.init[i] = 0.7
+    for i in range(6, 8):
+        X_init.init[i] = 0.6
+
+    # Define control path constraint
+    U_bounds = Bounds(
+        [torque_min] * biorbd_model.nbGeneralizedTorque(), [torque_max] * biorbd_model.nbGeneralizedTorque(),
+    )
+    U_init = InitialConditions([torque_init] * biorbd_model.nbGeneralizedTorque())
+
+    # ------------- #
+
+    return OptimalControlProgram(
+        biorbd_model,
+        problem_type,
+        number_shooting_points,
+        final_time,
+        X_init,
+        U_init,
+        X_bounds,
+        U_bounds,
+        objective_functions,
+        ode_solver=ode_solver,
+    )
+
+
+if __name__ == "__main__":
+    ocp = prepare_ocp(
+        biorbd_model_path="cube_and_line.bioMod",
+        number_shooting_points=30,
+        final_time=2,
+        ode_solver=OdeSolver.RK,
+        marker_velocity_or_displacement="disp", # "velo"
+        marker_in_first_coordinates_system=True,
+    )
+
+    # --- Solve the program --- #
+    sol = ocp.solve(show_online_optim=True)
+
+    # --- Show results --- #
+    result = ShowResult(ocp, sol)
+    result.animate()
