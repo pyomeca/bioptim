8344b91f314aaffef258ecf60389951ef80792aa
paul.wegiel <paul-wegiel@orange.fr>
2020-05-29 19:58:21 -0400

    negative test for mayer obj fun conflict with multiphase time constraint

--- tests/test_optimal_time.py
+++ tests/test_optimal_time.py
@@ -344,3 +344,50 @@ def test_mayer_neg_monophase_time_constraint(ode_solver):
             constraints,
             ode_solver=ode_solver,
         )
+
+spec = importlib.util.spec_from_file_location(
+    "test_mayer1_neg_multiphase_time_constraint", str(PROJECT_FOLDER) + "/examples/optimal_time_ocp/multiphase_time_constraint.py",
+)
+test_mayer1_neg_multiphase_time_constraint = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(test_mayer1_neg_multiphase_time_constraint)
+
+def test_mayer1_neg_multiphase_time_constraint():
+    with pytest.raises(RuntimeError, match="Time constraint/objective cannot declare more than once"):
+        biorbd_model, number_shooting_points, final_time, time_min, time_max, torque_min, torque_max, torque_init, problem_type, X_bounds, X_init, U_bounds, U_init = partial_ocp_parameters()
+        nb_phases = 3
+
+        objective_functions = (
+            (
+                {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 100},
+                {"type": Objective.Mayer.MINIMIZE_TIME},
+            ),
+            (),
+            ()
+        )
+        constraints =(
+            (
+                {
+                    "type": Constraint.ALIGN_MARKERS,
+                    "instant": Instant.START,
+                    "first_marker_idx": 0,
+                    "second_marker_idx": 1,
+                },
+                {"type": Constraint.TIME_CONSTRAINT, "minimum": time_min[1], "maximum": time_max[1],},
+            ),
+            (),
+            (),
+        )
+
+        OptimalControlProgram(
+            biorbd_model[:nb_phases],
+            problem_type[:nb_phases],
+            number_shooting_points[:nb_phases],
+            final_time[:nb_phases],
+            X_init[:nb_phases],
+            U_init[:nb_phases],
+            X_bounds[:nb_phases],
+            U_bounds[:nb_phases],
+            objective_functions,
+            constraints,
+            ode_solver=OdeSolver.RK,
+        )
