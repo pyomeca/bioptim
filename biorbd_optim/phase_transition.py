51c2ff1f797895911cb337793e373dcbd3b8ff73
paul.wegiel <paul-wegiel@orange.fr>
2020-05-29 12:49:09 -0400

    phase transition enhanced

--- biorbd_optim/phase_transition.py
+++ biorbd_optim/phase_transition.py
@@ -5,37 +5,38 @@ from casadi import vertcat
 
 
 class PhaseTransitionFunctions:
-    @staticmethod
-    def continuous(ocp, phase_before_idx):
-        """
-        TODO
-        """
-        return ocp.nlp[phase_before_idx]["X"][-1] - ocp.nlp[phase_before_idx + 1]["X"][0]
 
-    @staticmethod
-    def impact(ocp, phase_before_idx):
-        """
-        TODO
-        """
-        nlp_pre = ocp.nlp[phase_before_idx]
-        nlp_post = ocp.nlp[phase_before_idx + 1]
-        nbQ = nlp_pre["nbQ"]
-        q = nlp_post["X"][0][:nbQ]
-        qdot_pre = nlp_pre["X"][-1][nbQ:]
-        qdot_post = biorbd.GeneralizedVelocity(nlp_post["model"])
-        cs = nlp_post["model"].getConstraints()
-        # biorbd.Model.ComputeConstraintImpulsesDirect(nlp_post["model"], q, qdot_pre, cs, qdot_post)
+    class Functions:
+        @staticmethod
+        def continuous(ocp, phase_before_idx):
+            """
+            TODO
+            """
+            return ocp.nlp[phase_before_idx]["X"][-1] - ocp.nlp[(phase_before_idx + 1) % ocp.nb_phases]["X"][0]
 
-        # As a temporary replacement for ComputeConstraintImpulsesDirect:
-        qdot_post = nlp_post["X"][-1][nbQ:]
+        @staticmethod
+        def impact(ocp, phase_before_idx):
+            """
+            TODO
+            """
+            # Aliases
+            nlp_pre = ocp.nlp[phase_before_idx]
+            nlp_post = ocp.nlp[(phase_before_idx + 1) % ocp.nb_phases]
+            nbQ = nlp_pre["nbQ"]
+            q = nlp_post["X"][0][:nbQ]
+            qdot_pre = nlp_pre["X"][-1][nbQ:]
+            # qdot_post = nlp_post["model"].ComputeConstraintImpulsesDirect(q, qdot_pre)
 
-        val = nlp_pre["X"][-1][:nbQ] - q
-        val = vertcat(val, nlp_post["X"][0][nbQ:] - qdot_post)
-        return val
+            # As a temporary replacement for ComputeConstraintImpulsesDirect:
+            qdot_post = nlp_post["X"][-1][nbQ:]
 
-    @staticmethod
-    def custom(ocp, phase_before_idx):
-        pass
+            val = nlp_pre["X"][-1][:nbQ] - q
+            val = vertcat(val, nlp_post["X"][0][nbQ:] - qdot_post)
+            return val
+
+        @staticmethod
+        def custom(ocp, phase_before_idx):
+            pass
 
 
 class PhaseTransition(Enum):
@@ -43,6 +44,6 @@ class PhaseTransition(Enum):
     Different transitions between nlp phases.
     """
 
-    CONTINUOUS = PhaseTransitionFunctions.continuous
-    IMPACT = PhaseTransitionFunctions.impact
-    CUSTOM = PhaseTransitionFunctions.custom
+    CONTINUOUS = PhaseTransitionFunctions.Functions.continuous
+    IMPACT = PhaseTransitionFunctions.Functions.impact
+    CUSTOM = PhaseTransitionFunctions.Functions.custom
