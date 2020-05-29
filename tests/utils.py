8a0b34173783725d8eadbdc7010e764926092807
Théophile <theophile.gousselot@gmail.com>
2020-05-29 15:58:55 -0400

    test of iterations is in another file now

--- tests/utils.py
+++ tests/utils.py
@@ -18,15 +18,10 @@ from biorbd_optim import (
 
 class TestUtils:
     @staticmethod
-    def save_and_load(sol, ocp, test_solve_of_loaded=False, iterations=None):
+    def save_and_load(sol, ocp, test_solve_of_loaded=False):
         file_path = "test.bo"
-        ocp.save(sol, file_path) if iterations == None else ocp.save(sol, file_path, iterations)
-        if iterations == None:
-            ocp_load, sol_load = OptimalControlProgram.load(file_path)
-        else:
-            ocp_load, sol_load, iterations_load = OptimalControlProgram.load(file_path)
-            TestUtils.deep_assert(iterations, iterations_load)
-            TestUtils.deep_assert(iterations_load, iterations)
+        ocp.save(sol, file_path)
+        ocp_load, sol_load = OptimalControlProgram.load(file_path)
 
         TestUtils.deep_assert(sol, sol_load)
         TestUtils.deep_assert(sol_load, sol)
