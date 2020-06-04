8c345116339306c0c895d446b235118c49e3784b
pariterre <pariterre@hotmail.com>
2020-06-04 17:13:56 -0400

    Updated for biorbd-viz 1.3.0

--- biorbd_optim/plot.py
+++ biorbd_optim/plot.py
@@ -415,7 +415,7 @@ class ShowResult:
         all_bioviz = []
         for idx_phase, data in enumerate(data_interpolate["q"]):
             all_bioviz.append(BiorbdViz(loaded_model=self.ocp.nlp[idx_phase]["model"], **kwargs))
-            all_bioviz[-1].load_movement(self.ocp.nlp[idx_phase]["q_mapping"].expand.map(data).T)
+            all_bioviz[-1].load_movement(self.ocp.nlp[idx_phase]["q_mapping"].expand.map(data))
 
         b_is_visible = [True] * len(all_bioviz)
         while sum(b_is_visible):
