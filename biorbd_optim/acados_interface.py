acd80c6155b41af20bf3dd2d28e36884daaa2488
Ariane <dang.ariane@gmail.com>
2020-06-10 09:32:51 -0400

    Added RunTimeError if min and max are unequal

--- biorbd_optim/acados_interface.py
+++ biorbd_optim/acados_interface.py
@@ -50,7 +50,7 @@ def prepare_acados(self):
     # set cost module
     # TODO: test external cost_type
     acados_ocp.cost.cost_type = 'EXTERNAL'
-    acados_ocp.cost.cost_type_e = 'LINEAR_LS'
+    acados_ocp.cost.cost_type_e = 'EXTERNAL'
 
     # Cost for states and controls (default: 1.00)
     Q = 1.00 * np.eye(acados_ocp.dims.nx) #TODO Get the weights from J matrix
@@ -71,7 +71,8 @@ def prepare_acados(self):
 
     elif acados_ocp.cost.cost_type == 'EXTERNAL':
         # set Lagrange term
-        acados_ocp.model.cost_expr_ext_cost = vertcat(acados_model.x, acados_model.u).T @ acados_ocp.cost.W @ vertcat(acados_model.x, acados_model.u)
+        acados_ocp.model.cost_expr_ext_cost = self.nlp[0]['J'][0][2]
+        # acados_ocp.model.cost_expr_ext_cost = vertcat(acados_model.x, acados_model.u).T @ acados_ocp.cost.W @ vertcat(acados_model.x, acados_model.u)
 
     else:
         raise RuntimeError("Available acados cost type: 'LINEAR_LS' and 'EXTERNAL'.")
@@ -81,17 +82,22 @@ def prepare_acados(self):
         acados_ocp.cost.Vx_e = np.zeros((acados_ocp.dims.nx, acados_ocp.dims.nx))
 
     elif acados_ocp.cost.cost_type_e == 'EXTERNAL':
-        acados_ocp.model.cost_expr_ext_cost_e = acados_model.x.T @ Q @ acados_model.x
+        acados_ocp.model.cost_expr_ext_cost_e = acados_model.x.T @ Q @ acados_model.x #Not working for now
 
     else:
         raise RuntimeError("Available acados cost type: 'LINEAR_LS' and 'EXTERNAL'.")
 
-
+    #TODO: link with nlp
     acados_ocp.cost.yref = np.zeros((acados_ocp.dims.ny,))
     acados_ocp.cost.yref_e = np.ones((acados_ocp.dims.ny_e,))
 
     for i in range(self.nb_phases):
         # set constraints
+        for j in range(-1,0):
+            for k in range(self.nlp[i]['nx']):
+                if self.nlp[i]["X_bounds"].min[k, j] != self.nlp[i]["X_bounds"].max[k,j]:
+                    raise RuntimeError("The initial values must be set and fixed.")
+
         acados_ocp.constraints.x0 = np.array(self.nlp[i]["X_bounds"].min[:, 0])
         acados_ocp.dims.nbx_0 = acados_ocp.dims.nx
         acados_ocp.constraints.constr_type = 'BGH'  # TODO: put as an option in ocp?
