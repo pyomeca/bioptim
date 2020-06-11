ffb79412f91afd7d9ca8a8d48fcac0a7df6099ad
Théophile <theophile.gousselot@gmail.com>
2020-06-11 19:10:03 -0400

    Add problemcontext

--- biorbd_optim/__init__.py
+++ biorbd_optim/__init__.py
@@ -2,7 +2,7 @@ from .__version__ import __version__
 from .enums import Axe, OdeSolver, Instant, InterpolationType, PlotType
 from .constraints import Constraint
 from .objective_functions import Objective
-from .problem_type import ProblemType
+from .problem_type import ProblemType, ProblemContext
 from .plot import ShowResult
 from .path_conditions import Bounds, QAndQDotBounds, InitialConditions
 from .dynamics import Dynamics
