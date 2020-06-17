38018d0ba12642fb3e744c01f816c171501dd9f7
Ariane <dang.ariane@gmail.com>
2020-06-17 16:51:16 -0400

    Import SolverInterface in acados_interface.py

--- biorbd_optim/acados_interface.py
+++ biorbd_optim/acados_interface.py
@@ -5,23 +5,7 @@ from casadi import MX, Function, SX, vertcat
 import numpy as np
 import scipy.linalg
 from .objective_functions import ObjectiveFunction
-
-
-class SolverInterface:
-    def __init__(self):
-        self.solver = None
-
-    def configure(self, **options):
-        raise RuntimeError("SolverInterface is an abstract class")
-
-    def solve(self):
-        raise RuntimeError("SolverInterface is an abstract class")
-
-    def get_iterations(self):
-        raise RuntimeError("SolverInterface is an abstract class")
-
-    def get_optimized_value(self):
-        raise RuntimeError("SolverInterface is an abstract class")
+from .solver_interface import SolverInterface
 
 
 class AcadosInterface(SolverInterface):
