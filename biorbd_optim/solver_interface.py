b8c775591ed5082f725c001fc5203ccd41d6df85
Ariane <dang.ariane@gmail.com>
2020-06-17 16:49:30 -0400

    Added new file: solver_interface.py contains class SolverInterface

---
+++ biorbd_optim/solver_interface.py
@@ -0,0 +1,17 @@
+
+class SolverInterface:
+    def __init__(self):
+        self.solver = None
+
+    def configure(self, **options):
+        raise RuntimeError("SolverInterface is an abstract class")
+
+    def solve(self):
+        raise RuntimeError("SolverInterface is an abstract class")
+
+    def get_iterations(self):
+        raise RuntimeError("SolverInterface is an abstract class")
+
+    def get_optimized_value(self):
+        raise RuntimeError("SolverInterface is an abstract class")
+
