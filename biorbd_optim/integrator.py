642e486ded2687cb26aaf47e622b03e9016659c1
Pariterre <pariterre@hotmail.com>
2020-06-29 19:00:32 -0400

    Changed large vector for vector of vectors

--- biorbd_optim/integrator.py
+++ biorbd_optim/integrator.py
@@ -38,17 +38,17 @@ def RK4(ode, ode_opt):
         quat_number = 0
         for j in range(model.nbSegment()):
             if model.segment(j).isRotationAQuaternion():
-                quat_idx += [nb_dof, nb_dof + 1, nb_dof + 2, model.nbDof() + quat_number]
+                quat_idx.append([nb_dof, nb_dof + 1, nb_dof + 2, model.nbDof() + quat_number])
                 quat_number += 1
             nb_dof += model.segment(j).nbDof()
 
         for j in range(model.nbQuat()):
             quaternion = vertcat(
-                x[quat_idx[j * 4 + 3], i], x[quat_idx[j * 4], i], x[quat_idx[j * 4 + 1], i], x[quat_idx[j * 4 + 2], i]
+                x[quat_idx[j][3], i], x[quat_idx[j][0], i], x[quat_idx[j][1], i], x[quat_idx[j][2], i]
             )
             quaternion /= norm_fro(quaternion)
-            x[quat_idx[j * 4] : quat_idx[j * 4 + 2] + 1, i] = quaternion[1:4]
-            x[quat_idx[j * 4 + 3], i] = quaternion[0]
+            x[quat_idx[j][0] : quat_idx[j][2] + 1, i] = quaternion[1:4]
+            x[quat_idx[j][3], i] = quaternion[0]
 
         return x[:, -1], x
 
