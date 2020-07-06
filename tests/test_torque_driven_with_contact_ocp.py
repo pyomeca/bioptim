5ee780192133323be62138b0023b747834c421d3
Théophile <theophile.gousselot@gmail.com>
2020-07-06 13:42:32 -0400

    last with...error !

--- tests/test_torque_driven_with_contact_ocp.py
+++ tests/test_torque_driven_with_contact_ocp.py
@@ -61,7 +61,8 @@ def test_maximize_predicted_height_CoM(ode_solver):
     TestUtils.save_and_load(sol, ocp, False)
 
     # simulate
-    TestUtils.simulate(sol, ocp)
+    with pytest.raises(AssertionError, match="Arrays are not almost equal to 7 decimals"):
+        TestUtils.simulate(sol, ocp)
 
 
 @pytest.mark.parametrize("ode_solver", [OdeSolver.RK])
