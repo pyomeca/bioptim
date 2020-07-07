857b1134d020fb6aa43278f1bb767798de120e34
Théophile <theophile.gousselot@gmail.com>
2020-07-07 12:19:37 -0400

    new way to call prepare_ocp (pendulum)

--- examples/getting_started/pendulum_single_shooting.py
+++ examples/getting_started/pendulum_single_shooting.py
@@ -1,8 +1,19 @@
-from pendulum import prepare_ocp
+import importlib.util
+from pathlib import Path
 
 from biorbd_optim import InitialConditions, Simulate, ShowResult
 
-ocp = prepare_ocp(biorbd_model_path="pendulum.bioMod", final_time=2, number_shooting_points=10, nb_threads=2,)
+
+# It is not an optimal control, it only apply a Runge Kutta at each nodes
+
+# --- Load pendulum --- #
+PROJECT_FOLDER = Path(__file__).parent / "../.."
+spec = importlib.util.spec_from_file_location("pendulum", str(PROJECT_FOLDER) + "/examples/getting_started/pendulum.py")
+pendulum = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(pendulum)
+
+
+ocp = pendulum.prepare_ocp(biorbd_model_path="pendulum.bioMod", final_time=2, number_shooting_points=10, nb_threads=2,)
 
 X = InitialConditions([0, 0, 0, 0])
 U = InitialConditions([-1, 1])
