45faa3ef6a2d54242d458ab1d1adc654d97b1486
Pariterre <pariterre@hotmail.com>
2020-07-09 08:57:54 -0400

    moved a line for simplicity

--- examples/getting_started/custom_parameters.py
+++ examples/getting_started/custom_parameters.py
@@ -73,10 +73,10 @@ def prepare_ocp(biorbd_model_path, final_time, number_shooting_points, min_g, ma
 
     # Define the parameter to optimize
     # Give the parameter some min and max bounds
+    parameters = ParametersList()
     bound_gravity = Bounds(min_bound=min_g, max_bound=max_g, interpolation=InterpolationType.CONSTANT)
     # and an initial condition
     initial_gravity = InitialConditions((min_g + max_g) / 2)
-    parameters = ParametersList()
     parameter_objective_functions = ObjectiveList()
     parameter_objective_functions.add(
         my_target_function, weight=10, quadratic=True, custom_type=Objective.Parameter, target_value=target_g
