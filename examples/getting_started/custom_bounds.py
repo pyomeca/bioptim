503c20f1eeeff4748aa5748d4cdaec588a6a693b
paul.wegiel <paul-wegiel@orange.fr>
2020-07-14 22:02:01 -0400

    Started to add custom_bounds example

---
+++ examples/getting_started/custom_bounds.py
@@ -0,0 +1,130 @@
+import numpy as np
+import biorbd
+
+from biorbd_optim import (
+    Instant,
+    OptimalControlProgram,
+    DynamicsTypeOption,
+    DynamicsType,
+    ObjectiveOption,
+    Objective,
+    ConstraintList,
+    Constraint,
+    BoundsOption,
+    QAndQDotBounds,
+    InitialConditionsOption,
+    ShowResult,
+    InterpolationType,
+)
+
+
+def custom_min(current_shooting_point, n_elements, nb_shooting):
+    my_values = np.array([[-50, -20]] * n_elements)
+    # Linear interpolation created with custom function
+    return my_values[:, 0] + (my_values[:, -1] - my_values[:, 0]) * current_shooting_point / nb_shooting
+
+
+def custom_max(current_shooting_point, n_elements, nb_shooting):
+    my_values = np.array([[75, 100]] * n_elements)
+    # Linear interpolation created with custom function
+    return my_values[:, 0] + (my_values[:, -1] - my_values[:, 0]) * current_shooting_point / nb_shooting
+
+
+def prepare_ocp(
+    biorbd_model_path,
+    number_shooting_points,
+    final_time,
+    interpolation_type=InterpolationType.CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT,
+):
+    # --- Options --- #
+    # Model path
+    biorbd_model = biorbd.Model(biorbd_model_path)
+    nq = biorbd_model.nbQ()
+    nqdot = biorbd_model.nbQdot()
+    ntau = biorbd_model.nbGeneralizedTorque()
+    tau_min, tau_max, tau_init = -100, 100, 0
+
+    # Add objective functions
+    objective_functions = ObjectiveOption(Objective.Lagrange.MINIMIZE_TORQUE)
+
+    # Dynamics
+    dynamics = DynamicsTypeOption(DynamicsType.TORQUE_DRIVEN)
+
+    # Constraints
+    constraints = ConstraintList()
+    constraints.add(Constraint.ALIGN_MARKERS, instant=Instant.START, first_marker_idx=0, second_marker_idx=1)
+    constraints.add(Constraint.ALIGN_MARKERS, instant=Instant.END, first_marker_idx=0, second_marker_idx=2)
+
+    spline_time = None
+    # Path constraints
+    if interpolation_type == InterpolationType.CONSTANT:
+        x_min = [-100] * (nq + nqdot)
+        x_max = [100] * (nq + nqdot)
+        x_bounds = BoundsOption([x_min, x_max], interpolation=InterpolationType.CONSTANT)
+        u_min = [tau_min] * ntau
+        u_max = [tau_max] * ntau
+        u_bounds = BoundsOption([u_min, u_max], interpolation=InterpolationType.CONSTANT)
+    elif interpolation_type == InterpolationType.CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT:
+        x_min = np.random.random((3, 3)) - 100
+        x_max = np.random.random((3, 3)) + 100
+        x_bounds = BoundsOption([x_min, x_max], interpolation=InterpolationType.CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT)
+        u_min = np.random.random((3, 3)) - 100
+        u_max = np.random.random((3, 3)) + 100
+        u_bounds = BoundsOption([u_min, u_max], interpolation=InterpolationType.CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT)
+    elif interpolation_type == InterpolationType.SPLINE:
+        spline_time = np.hstack((0, np.sort(np.random.random((3,)) * final_time), final_time))
+        x_min = np.random.random((nq + nqdot, 5)) - 100
+        x_max = np.random.random((nq + nqdot, 5)) + 100
+        u_min = np.random.random((ntau, 5)) - 100
+        u_max = np.random.random((ntau, 5)) + 100
+        x_bounds = BoundsOption([x_min, x_max], interpolation=InterpolationType.SPLINE, t=spline_time)
+        u_bounds = BoundsOption([u_min, u_max], interpolation=InterpolationType.SPLINE, t=spline_time)
+    elif interpolation_type == InterpolationType.CUSTOM:
+        # The custom function refers to the one at the beginning of the file. It emulates a Linear interpolation
+        extra_params_x = {"n_elements": nq + nqdot, "nb_shooting": number_shooting_points}
+        extra_params_u = {"n_elements": ntau, "nb_shooting": number_shooting_points}
+
+        x_bounds = BoundsOption([custom_min, custom_max], interpolation=InterpolationType.CUSTOM, **extra_params_x)
+        u_bounds = BoundsOption([custom_min, custom_max], interpolation=InterpolationType.CUSTOM, **extra_params_u)
+    else:
+        raise NotImplementedError("Not implemented yet")
+
+    # else:
+    #     x_bounds = BoundsOption(QAndQDotBounds(biorbd_model))
+    #     x_bounds.min[1:6, [0, -1]] = 0
+    #     x_bounds.max[1:6, [0, -1]] = 0
+    #     x_bounds.min[2, -1] = 1.57
+    #     x_bounds.max[2, -1] = 1.57
+    #     u_bounds = BoundsOption([[tau_min] * ntau, [tau_max] * ntau])
+
+
+    # Initial guess
+    x_init = InitialConditionsOption([0] * (nq + nqdot))
+    u_init = InitialConditionsOption([tau_init] * ntau)
+    # ------------- #
+
+    return OptimalControlProgram(
+        biorbd_model,
+        dynamics,
+        number_shooting_points,
+        final_time,
+        x_init,
+        u_init,
+        x_bounds,
+        u_bounds,
+        objective_functions,
+        constraints,
+    )
+
+
+if __name__ == "__main__":
+    # for interpolation_type in InterpolationType:
+    for interpolation_type in [InterpolationType.CUSTOM]:
+        print(f"Solving problem using {interpolation_type} bounds")
+        ocp = prepare_ocp("cube.bioMod", number_shooting_points=30, final_time=2, interpolation_type=interpolation_type)
+        sol = ocp.solve()
+        print("\n")
+
+    # Print the last solution
+    result_plot = ShowResult(ocp, sol)
+    result_plot.graphs()
