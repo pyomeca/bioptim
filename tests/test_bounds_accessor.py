34d9875a54b37fb8c7522e0249ec8675914c91f3
Valentin THIRON <valentin.thiron@epita.fr>
2020-09-15 17:21:52 +0200

    Initialize test variable as a multidimensional one

--- tests/test_bounds_accessor.py
+++ tests/test_bounds_accessor.py
@@ -26,10 +26,10 @@ def test_accessors_on_bounds_option():
 
 
 def test_accessors_on_bounds_option_multidimensional():
-    x_min = [-100] * 6
-    x_max = [100] * 6
+    x_min = [[-100, -50, 0] for i in range(6)]
+    x_max = [[100, 150, 200] for i in range(6)]
     x_bounds = BoundsOption([x_min, x_max], interpolation=InterpolationType.CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT)
-    x_bounds[:3] = 0
+    x_bounds[:3, 0] = 0
     x_bounds.min[1:5, 1:] = -10
     x_bounds.max[1:5, 1:] = 10
 
@@ -39,8 +39,9 @@ def test_accessors_on_bounds_option_multidimensional():
     # Check min and max have the right value
     np.testing.assert_almost_equal(
         x_bounds.min[:],
-        np.array([[0, 0, 0], [0, -10, -10], [0, -10, -10], [-100, -10, -10], [-100, -10, -10], [-100, -100, -100]]),
+        np.array([[0, -50, 0], [0, -10, -10], [0, -10, -10], [-100, -10, -10], [-100, -10, -10], [-100, -50, 0]]),
     )
     np.testing.assert_almost_equal(
-        x_bounds.max[:], np.array([[0, 0, 0], [0, 10, 10], [0, 10, 10], [100, 10, 10], [100, 10, 10], [100, 100, 100]])
+        x_bounds.max[:],
+        np.array([[0, 150, 200], [0, 10, 10], [0, 10, 10], [100, 10, 10], [100, 10, 10], [100, 150, 200]]),
     )
