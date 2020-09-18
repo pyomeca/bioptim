0abce5b1ce00001e86bf24a8a7af6b3e5ffa2038
Steakkk <val.thiron@live.com>
2020-09-18 10:24:38 +0200

    Add test on 3 more objective functions

--- tests/test_objectives.py
+++ tests/test_objectives.py
@@ -31,6 +31,67 @@ def prepare_test_ocp():
     return ocp
 
 
+def test_objective_minimize_time():
+    ocp = prepare_test_ocp()
+    x = [np.ones((12, 1)) / 10]
+    penalty_type = Objective.Lagrange.MINIMIZE_TIME
+    penalty = ObjectiveOption(penalty_type)
+    penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
+
+    np.testing.assert_almost_equal(
+        ocp.nlp[0].J[0][0]["val"],
+        np.array(1),
+    )
+
+
+def test_objective_minimize_state():
+    ocp = prepare_test_ocp()
+    x = [np.ones((12, 1)) / 10]
+    penalty_type = Objective.Lagrange.MINIMIZE_STATE
+    penalty = ObjectiveOption(penalty_type)
+    penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
+
+    np.testing.assert_almost_equal(
+        ocp.nlp[0].J[0][0]["val"],
+        np.array(
+            [
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+            ]
+        ),
+    )
+
+
+def test_objective_track_state():
+    ocp = prepare_test_ocp()
+    x = [np.ones((12, 1)) / 10]
+    penalty_type = Objective.Lagrange.TRACK_STATE
+    penalty = ObjectiveOption(penalty_type)
+    penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
+
+    np.testing.assert_almost_equal(
+        ocp.nlp[0].J[0][0]["val"],
+        np.array(
+            [
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+                [0.1],
+            ]
+        ),
+    )
+
+
 def test_objective_minimize_marker():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
