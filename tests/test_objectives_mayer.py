b1c3dd39eec07446d06f54a9eff446c4e78eedf3
Steakkk <val.thiron@live.com>
2020-09-18 11:43:40 +0200

    Testing separately Lagrange and Mayer objective funcs

--- tests/test_objectives_mayer.py
+++ tests/test_objectives_mayer.py
@@ -32,10 +32,10 @@ def prepare_test_ocp():
     return ocp
 
 
-def test_objective_minimize_time():
+def test_objective_mayer_minimize_time():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_TIME
+    penalty_type = Objective.Mayer.MINIMIZE_TIME
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -45,10 +45,10 @@ def test_objective_minimize_time():
     )
 
 
-def test_objective_minimize_state():
+def test_objective_mayer_minimize_state():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_STATE
+    penalty_type = Objective.Mayer.MINIMIZE_STATE
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -69,10 +69,10 @@ def test_objective_minimize_state():
     )
 
 
-def test_objective_track_state():
+def test_objective_mayer_track_state():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_STATE
+    penalty_type = Objective.Mayer.TRACK_STATE
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -93,29 +93,10 @@ def test_objective_track_state():
     )
 
 
-def test_objective_minimize_markers():
+def test_objective_mayer_minimize_markers():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_MARKERS
-    penalty = ObjectiveOption(penalty_type)
-    penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
-
-    np.testing.assert_almost_equal(
-        ocp.nlp[0].J[0][0]["val"],
-        np.array(
-            [
-                [0.1, 0.99517075, 1.9901749, 1.0950042, 1, 2, 0.49750208],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0.1, -0.9948376, -1.094671, 0.000166583, 0, 0, -0.0499167],
-            ]
-        ),
-    )
-
-
-def test_objective_track_markers():
-    ocp = prepare_test_ocp()
-    x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_MARKERS
+    penalty_type = Objective.Mayer.MINIMIZE_MARKERS
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -131,10 +112,29 @@ def test_objective_track_markers():
     )
 
 
-def test_objective_minimize_markers_displacement():
+def test_objective_mayer_track_markers():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_MARKERS_DISPLACEMENT
+    penalty_type = Objective.Mayer.TRACK_MARKERS
+    penalty = ObjectiveOption(penalty_type)
+    penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
+
+    np.testing.assert_almost_equal(
+        ocp.nlp[0].J[0][0]["val"],
+        np.array(
+            [
+                [0.1, 0.99517075, 1.9901749, 1.0950042, 1, 2, 0.49750208],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0.1, -0.9948376, -1.094671, 0.000166583, 0, 0, -0.0499167],
+            ]
+        ),
+    )
+
+
+def test_objective_mayer_minimize_markers_displacement():
+    ocp = prepare_test_ocp()
+    x = [np.ones((12, 1)) / 10]
+    penalty_type = Objective.Mayer.MINIMIZE_MARKERS_DISPLACEMENT
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -144,10 +144,10 @@ def test_objective_minimize_markers_displacement():
     )
 
 
-def test_objective_minimize_markers_velocity():
+def test_objective_mayer_minimize_markers_velocity():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_MARKERS_VELOCITY
+    penalty_type = Objective.Mayer.MINIMIZE_MARKERS_VELOCITY
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -163,10 +163,10 @@ def test_objective_minimize_markers_velocity():
     )
 
 
-def test_objective_track_markers_velocity():
+def test_objective_mayer_track_markers_velocity():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_MARKERS_VELOCITY
+    penalty_type = Objective.Mayer.TRACK_MARKERS_VELOCITY
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -182,10 +182,10 @@ def test_objective_track_markers_velocity():
     )
 
 
-def test_objective_align_markers():
+def test_objective_mayer_align_markers():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.ALIGN_MARKERS
+    penalty_type = Objective.Mayer.ALIGN_MARKERS
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [], first_marker_idx=0, second_marker_idx=1)
 
@@ -201,10 +201,10 @@ def test_objective_align_markers():
     )
 
 
-def test_objective_proportional_state():
+def test_objective_mayer_proportional_state():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.PROPORTIONAL_STATE
+    penalty_type = Objective.Mayer.PROPORTIONAL_STATE
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](
         penalty, ocp, ocp.nlp[0], [], x, [], [], which_var="states", first_dof=0, second_dof=1, coef=2
@@ -216,10 +216,10 @@ def test_objective_proportional_state():
     )
 
 
-def test_objective_proportional_control():
+def test_objective_mayer_proportional_control():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.PROPORTIONAL_CONTROL
+    penalty_type = Objective.Mayer.PROPORTIONAL_CONTROL
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](
         penalty, ocp, ocp.nlp[0], [], x, [], [], which_var="controls", first_dof=0, second_dof=1, coef=2
@@ -231,10 +231,10 @@ def test_objective_proportional_control():
     )
 
 
-def test_objective_minimize_torque():
+def test_objective_mayer_minimize_torque():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_TORQUE
+    penalty_type = Objective.Mayer.MINIMIZE_TORQUE
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -244,10 +244,10 @@ def test_objective_minimize_torque():
     )
 
 
-def test_objective_track_torque():
+def test_objective_mayer_track_torque():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_TORQUE
+    penalty_type = Objective.Mayer.TRACK_TORQUE
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -257,10 +257,10 @@ def test_objective_track_torque():
     )
 
 
-def test_objective_minimize_torque_derivative():
+def test_objective_mayer_minimize_muscles_control():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_TORQUE_DERIVATIVE
+    penalty_type = Objective.Mayer.MINIMIZE_MUSCLES_CONTROL
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -270,10 +270,10 @@ def test_objective_minimize_torque_derivative():
     )
 
 
-def test_objective_minimize_muscles_control():
+def test_objective_mayer_track_muscles_control():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_MUSCLES_CONTROL
+    penalty_type = Objective.Mayer.TRACK_MUSCLES_CONTROL
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -283,10 +283,10 @@ def test_objective_minimize_muscles_control():
     )
 
 
-def test_objective_track_muscles_control():
+def test_objective_mayer_minimize_all_controls():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_MUSCLES_CONTROL
+    penalty_type = Objective.Mayer.MINIMIZE_ALL_CONTROLS
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -296,10 +296,10 @@ def test_objective_track_muscles_control():
     )
 
 
-def test_objective_minimize_all_controls():
+def test_objective_mayer_track_all_controls():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_ALL_CONTROLS
+    penalty_type = Objective.Mayer.TRACK_ALL_CONTROLS
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -309,10 +309,10 @@ def test_objective_minimize_all_controls():
     )
 
 
-def test_objective_track_all_controls():
+def test_objective_mayer_minimize_contact_forces():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_ALL_CONTROLS
+    penalty_type = Objective.Mayer.MINIMIZE_CONTACT_FORCES
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -322,10 +322,10 @@ def test_objective_track_all_controls():
     )
 
 
-def test_objective_minimize_contact_forces():
+def test_objective_mayer_minimize_contact_forces():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.MINIMIZE_CONTACT_FORCES
+    penalty_type = Objective.Mayer.TRACK_CONTACT_FORCES
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
@@ -335,23 +335,23 @@ def test_objective_minimize_contact_forces():
     )
 
 
-def test_objective_minimize_contact_forces():
+def test_objective_mayer_minimize_predicted_com_height():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.TRACK_CONTACT_FORCES
+    penalty_type = Objective.Mayer.MINIMIZE_PREDICTED_COM_HEIGHT
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [])
 
     np.testing.assert_almost_equal(
-        ocp.nlp[0].J[0],
-        np.array([]),
+        ocp.nlp[0].J[0][0]["val"],
+        np.array(0.0501274),
     )
 
 
-def test_objective_align_segment_with_custom_rt():
+def test_objective_mayer_align_segment_with_custom_rt():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.ALIGN_SEGMENT_WITH_CUSTOM_RT
+    penalty_type = Objective.Mayer.ALIGN_SEGMENT_WITH_CUSTOM_RT
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [], segment_idx=1, rt_idx=0)
 
@@ -361,10 +361,10 @@ def test_objective_align_segment_with_custom_rt():
     )
 
 
-def test_objective_align_marker_with_segment_axis():
+def test_objective_mayer_align_marker_with_segment_axis():
     ocp = prepare_test_ocp()
     x = [np.ones((12, 1)) / 10]
-    penalty_type = Objective.Lagrange.ALIGN_MARKER_WITH_SEGMENT_AXIS
+    penalty_type = Objective.Mayer.ALIGN_MARKER_WITH_SEGMENT_AXIS
     penalty = ObjectiveOption(penalty_type)
     penalty_type.value[0](penalty, ocp, ocp.nlp[0], [], x, [], [], marker_idx=0, segment_idx=1, axis=Axe.X)
 
