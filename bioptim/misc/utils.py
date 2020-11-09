dc0a754abb231018aa1c8657d853869730920684
Pariterre <pariterre@hotmail.com>
2020-11-09 18:06:27 -0500

    Force new versions of biorbd 1.4.0 and bioviz 2.0.1

--- bioptim/misc/utils.py
+++ bioptim/misc/utils.py
@@ -6,5 +6,5 @@ def check_version(tool_to_compare, min_version, max_version):
     ver = parse_version(tool_to_compare.__version__)
     if ver < parse_version(min_version):
         raise ImportError(f"{name} should be at least version {min_version}")
-    elif ver > parse_version(max_version):
+    elif ver >= parse_version(max_version):
         raise ImportError(f"{name} should be lesser than version {max_version}")
