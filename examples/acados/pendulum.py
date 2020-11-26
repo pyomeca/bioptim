fae82f65ae60185d4d6bc3f351c3af549a3c693e
fbailly <fbailly@laas.fr>
2020-11-26 12:11:04 +0100

    updated pendulum acados ex

--- examples/acados/pendulum.py
+++ examples/acados/pendulum.py
@@ -16,7 +16,7 @@ from bioptim import (
 )
 
 
-def prepare_ocp(biorbd_model_path, final_time, number_shooting_points, nb_threads, use_SX=False):
+def prepare_ocp(biorbd_model_path, final_time, number_shooting_points, use_SX=True):
     # --- Options --- #
     biorbd_model = biorbd.Model(biorbd_model_path)
     torque_min, torque_max, torque_init = -100, 100, 0
@@ -74,14 +74,13 @@ def prepare_ocp(biorbd_model_path, final_time, number_shooting_points, nb_thread
         x_bounds,
         u_bounds,
         objective_functions,
-        nb_threads=nb_threads,
         use_SX=use_SX,
     )
 
 
 if __name__ == "__main__":
     ocp = prepare_ocp(
-        biorbd_model_path="pendulum.bioMod", final_time=3, number_shooting_points=41, nb_threads=4, use_SX=True
+        biorbd_model_path="pendulum.bioMod", final_time=3, number_shooting_points=41
     )
 
     # --- Solve the program --- #
