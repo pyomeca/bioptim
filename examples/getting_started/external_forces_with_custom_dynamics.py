d72d4dfd5792451af23b6235e8a51e74af94d77c
Pariterre <pariterre@hotmail.com>
2020-12-02 18:01:51 -0500

    cleaned example

--- examples/getting_started/external_forces_with_custom_dynamics.py
+++ examples/getting_started/external_forces_with_custom_dynamics.py
@@ -3,18 +3,20 @@ import casadi as cas
 
 from bioptim import (
     OptimalControlProgram,
-    DynamicsTypeList,
+    DynamicsTypeOption,
     Problem,
-    ObjectiveList,
+    ObjectiveOption,
     DynamicsFunctions,
     Objective,
     BoundsOption,
     QAndQDotBounds,
     InitialGuessOption,
     ShowResult,
-    OdeSolver,
-    BoundsList,
 )
+
+# This example load a mass on an upward spring and must have the greatest upward velocity at end point
+# while only being able to pull on the system (the upward velocity being created by the spring).
+# This example illustrates how you can use external forces that depends on the state of the system
 
 
 def custom_dynamic(states, controls, parameters, nlp):
@@ -36,67 +38,46 @@ def custom_configure(ocp, nlp):
     Problem.configure_forward_dyn_func(ocp, nlp, custom_dynamic)
 
 
-def prepare_ocp(biorbd_model_path, ode_solver=OdeSolver.RK):
-    # --- Options --- #
-    # Model path
-    m = biorbd.Model(biorbd_model_path)
-    m.setGravity(biorbd.Vector3d(0, 0, 0))
+# Model path
+m = biorbd.Model("mass_point.bioMod")
+m.setGravity(biorbd.Vector3d(0, 0, 0))
 
-    # Problem parameters
-    number_shooting_points = 30
-    final_time = 0.5
-    tau_min, tau_max, tau_init = -100, 0, 0
+# Add objective functions (high upward velocity at end point)
+objective_functions = ObjectiveOption(Objective.Mayer.MINIMIZE_STATE, index=1, weight=-1)
 
-    # Add objective functions
-    objective_functions = ObjectiveList()
-    objective_functions.add(Objective.Mayer.MINIMIZE_STATE, index=1, weight=-1)
+# Dynamics
+dynamics = DynamicsTypeOption(custom_configure, dynamic_function=custom_dynamic)
 
-    # Dynamics
-    dynamics = DynamicsTypeList()
-    dynamics.add(custom_configure, dynamic_function=custom_dynamic)
+# Path constraint
+x_bounds = BoundsOption(QAndQDotBounds(m))
+x_bounds[:, 0] = [0] * m.nbQ() + [0] * m.nbQdot()
+x_bounds.min[:, 1] = [-1] * m.nbQ() + [-100] * m.nbQdot()
+x_bounds.max[:, 1] = [1] * m.nbQ() + [100] * m.nbQdot()
+x_bounds.min[:, 2] = [-1] * m.nbQ() + [-100] * m.nbQdot()
+x_bounds.max[:, 2] = [1] * m.nbQ() + [100] * m.nbQdot()
 
-    # Path constraint
-    X_bounds = BoundsList()
-    X_bounds.add(QAndQDotBounds(m))
-    X_bounds[0].min[:, 0] = [0] * m.nbQ() + [0] * m.nbQdot()
-    X_bounds[0].max[:, 0] = [0] * m.nbQ() + [0] * m.nbQdot()
-    X_bounds[0].min[:, 1] = [-1] * m.nbQ() + [-1000] * m.nbQdot()
-    X_bounds[0].max[:, 1] = [1] * m.nbQ() + [1000] * m.nbQdot()
-    X_bounds[0].min[:, 2] = [-1] * m.nbQ() + [-1000] * m.nbQdot()
-    X_bounds[0].max[:, 2] = [1] * m.nbQ() + [1000] * m.nbQdot()
+# Initial guess
+x_init = InitialGuessOption([0] * (m.nbQ() + m.nbQdot()))
 
-    # Initial guess
-    x_init = InitialGuessOption([0] * (m.nbQ() + m.nbQdot()))
+# Define control path constraint
+u_bounds = BoundsOption([[-100] * m.nbGeneralizedTorque(), [0] * m.nbGeneralizedTorque()])
 
-    # Define control path constraint
-    u_bounds = BoundsOption([[tau_min] * m.nbGeneralizedTorque(), [tau_max] * m.nbGeneralizedTorque()])
-
-    u_init = InitialGuessOption([tau_init] * m.nbGeneralizedTorque())
-
-    # ------------- #
-
-    return OptimalControlProgram(
+u_init = InitialGuessOption([0] * m.nbGeneralizedTorque())
+ocp = OptimalControlProgram(
         m,
         dynamics,
-        number_shooting_points,
-        final_time,
-        x_init,
-        u_init,
-        X_bounds,
-        u_bounds,
-        objective_functions,
-        ode_solver=ode_solver,
+        number_shooting_points=30,
+        phase_time=0.5,
+        x_init=x_init,
+        u_init=u_init,
+        x_bounds=x_bounds,
+        u_bounds=u_bounds,
+        objective_functions=objective_functions,
     )
 
+# --- Solve the program --- #
+sol = ocp.solve(show_online_optim=True)
 
-if __name__ == "__main__":
-    model_path = "MassPoint.bioMod"
-    ocp = prepare_ocp(biorbd_model_path=model_path)
-
-    # --- Solve the program --- #
-    sol = ocp.solve(show_online_optim=True)
-
-    # --- Show results --- #
-    result = ShowResult(ocp, sol)
-    result.animate()
-
+# --- Show results --- #
+result = ShowResult(ocp, sol)
+result.animate()
