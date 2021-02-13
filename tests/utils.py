0ebbe8db0b1618d09b1a0e5aee12b742023aa751
Pariterre <pariterre@hotmail.com>
2021-02-13 09:41:19 -0500

    Removed unnecessary if

--- tests/utils.py
+++ tests/utils.py
@@ -68,16 +68,11 @@ class TestUtils:
                     pass
 
     @staticmethod
-    def simulate(sol, ocp, decimal_value=None):
+    def simulate(sol, ocp, decimal_value=7):
         sol_from_solver = np.array(sol["x"]).squeeze()
         sol_simulation_from_solve = Simulate.from_solve(ocp, sol)["x"]
         sol_simulation_from_data = Simulate.from_data(ocp, Data.get_data(ocp, sol))["x"]
 
-        if decimal_value is None:
-            np.testing.assert_almost_equal(sol_from_solver, sol_simulation_from_solve)
-            np.testing.assert_almost_equal(sol_from_solver, sol_simulation_from_data)
-            np.testing.assert_almost_equal(sol_simulation_from_solve, sol_simulation_from_data)
-        else:
-            np.testing.assert_almost_equal(sol_from_solver, sol_simulation_from_solve, decimal=decimal_value)
-            np.testing.assert_almost_equal(sol_from_solver, sol_simulation_from_data, decimal=decimal_value)
-            np.testing.assert_almost_equal(sol_simulation_from_solve, sol_simulation_from_data, decimal=decimal_value)
+        np.testing.assert_almost_equal(sol_from_solver, sol_simulation_from_solve, decimal=decimal_value)
+        np.testing.assert_almost_equal(sol_from_solver, sol_simulation_from_data, decimal=decimal_value)
+        np.testing.assert_almost_equal(sol_simulation_from_solve, sol_simulation_from_data, decimal=decimal_value)
