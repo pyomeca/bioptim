fix broken test when no scaling

bioptim/dynamics/problem.py
@@ -579,7 +579,11 @@ class Problem:
 
         nlp.parameters = ocp.v.parameters_in_list
         nlp.p = ocp.v.parameters.cx
-        nlp.p_scaling = np.vstack([p.scaling for p in nlp.parameters])
+
+        if nlp.parameters.__len__():
+            nlp.p_scaling = np.vstack([p.scaling for p in nlp.parameters])
+        else:
+            nlp.p_scaling = ocp.cx(1)
         nlp.np = sum([p.size for p in nlp.parameters])
         mx_symbolic_params = MX.sym("p", nlp.np, 1)
 