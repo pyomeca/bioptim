86f2d630df29af1ebede42d99113aaef2a4b53bf
fbailly <fbailly@laas.fr>
2021-04-07 14:32:08 +0200

    add tests for scaling declaration

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,6 +1,6 @@
 import pytest
 from bioptim import ParameterList
-
+import numpy as np
 
 def test_wrong_parameter():
     param = ParameterList()
@@ -15,3 +15,34 @@ def test_wrong_parameter():
         "not use 'phase' but 'list_index' instead",
     ):
         param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, phase=0)
+
+def test_param_scaling():
+    param = ParameterList()
+
+    my_parameter_function = 1
+    initial_gravity = 1
+    bounds = 1
+
+    with pytest.raises(
+        ValueError,
+        match="Parameter scaling must be a numpy array",
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, scaling='a')
+
+    with pytest.raises(
+        ValueError,
+        match="Parameter scaling must be a numpy array",
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, scaling=1.0)
+
+    with pytest.raises(
+        ValueError,
+        match="Parameter scaling must be a numpy array",
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, scaling=[])
+
+    with pytest.raises(
+        ValueError,
+        match="Parameter scaling must contain only positive values",
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, scaling=np.array([-1]))
