1fec744f5183e878e8d2d9fcec377681c9851902
fbailly <fbailly@laas.fr>
2021-04-08 14:48:38 +0200

    added tests in param scaling for improving coverage

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,7 +1,7 @@
 import pytest
 from bioptim import ParameterList
 import numpy as np
-
+import regex as re
 
 def test_wrong_parameter():
     param = ParameterList()
@@ -48,3 +48,21 @@ def test_param_scaling():
         match="Parameter scaling must contain only positive values",
     ):
         param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, scaling=np.array([-1]))
+
+    with pytest.raises(
+        ValueError,
+        match="Parameter scaling must be a 1- or 2- dimensional numpy array",
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=1, scaling=np.array([[[1]]]))
+
+    with pytest.raises(
+        ValueError,
+        match=f"The shape \(2\) of the scaling of parameter gravity_z does not match the params shape."
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=3, scaling=np.array([1, 2]))
+
+    with pytest.raises(
+        ValueError,
+        match=f"Invalid ncols for Parameter Scaling \(ncols = 2\), the expected number of column is 1"
+    ):
+        param.add("gravity_z", my_parameter_function, initial_gravity, bounds, size=3, scaling=np.ones((1, 2)))
