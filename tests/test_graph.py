bc4c5356369877b34859660794632f2d1bdf5a8f
Léo Vayssac <leo.vayssac@gmail.com>
2021-05-26 16:55:12 +0200

    First tests with pytest

---
+++ tests/test_graph.py
@@ -0,0 +1,161 @@
+import pytest
+import numpy as np
+
+import biorbd
+from bioptim import (
+    OptimalControlProgram,
+    DynamicsList,
+    DynamicsFcn,
+    ObjectiveList,
+    ObjectiveFcn,
+    BoundsList,
+    QAndQDotBounds,
+    InitialGuessList,
+    ControlType,
+    OdeSolver,
+    ConstraintList,
+    ConstraintFcn,
+    Node,
+    PhaseTransitionList,
+    PhaseTransitionFcn,
+)
+
+from .utils import TestUtils
+
+def prepare_ocp_phase_transitions(
+    biorbd_model_path: str,
+    ode_solver: OdeSolver = OdeSolver.RK4(),
+    with_constraints: bool = False,
+    with_mayer: bool = False,
+    with_lagrange: bool = False,
+) -> OptimalControlProgram:
+    """
+    Parameters
+    ----------
+    biorbd_model_path: str
+        The path to the bioMod
+    ode_solver: OdeSolver
+        The type of ode solver used
+
+    Returns
+    -------
+    The ocp ready to be solved
+    """
+
+    # Model path
+    biorbd_model = (
+        biorbd.Model(biorbd_model_path),
+        biorbd.Model(biorbd_model_path),
+        biorbd.Model(biorbd_model_path),
+        biorbd.Model(biorbd_model_path),
+    )
+
+    # Problem parameters
+    n_shooting = (20, 20, 20, 20)
+    final_time = (2, 5, 4, 2)
+    tau_min, tau_max, tau_init = -100, 100, 0
+
+    # Add objective functions
+    objective_functions = ObjectiveList()
+    if with_lagrange:
+        objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_TORQUE, weight=100, phase=0)
+        objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_TORQUE, weight=100, phase=1)
+        objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_TORQUE, weight=100, phase=2)
+        objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_TORQUE, weight=100, phase=3)
+    if with_mayer:
+        objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME)
+        objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_COM_POSITION, phase=0, node=1)
+
+    # Dynamics
+    dynamics = DynamicsList()
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN)
+
+    # Constraints
+    constraints = ConstraintList()
+    if with_constraints:
+        constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.START, first_marker="m0", second_marker="m1", phase=0)
+        constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="m0", second_marker="m2", phase=0)
+        constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="m0", second_marker="m1", phase=1)
+        constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="m0", second_marker="m2", phase=2)
+        constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="m0", second_marker="m1", phase=3)
+
+    # Path constraint
+    x_bounds = BoundsList()
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+
+    x_bounds[0][[1, 3, 4, 5], 0] = 0
+    x_bounds[-1][[1, 3, 4, 5], -1] = 0
+
+    x_bounds[0][2, 0] = 0.0
+    x_bounds[2][2, [0, -1]] = [0.0, 1.57]
+
+    # Initial guess
+    x_init = InitialGuessList()
+    x_init.add([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
+    x_init.add([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
+    x_init.add([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
+    x_init.add([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
+
+    # Define control path constraint
+    u_bounds = BoundsList()
+    u_bounds.add([tau_min] * biorbd_model[0].nbGeneralizedTorque(), [tau_max] * biorbd_model[0].nbGeneralizedTorque())
+    u_bounds.add([tau_min] * biorbd_model[0].nbGeneralizedTorque(), [tau_max] * biorbd_model[0].nbGeneralizedTorque())
+    u_bounds.add([tau_min] * biorbd_model[0].nbGeneralizedTorque(), [tau_max] * biorbd_model[0].nbGeneralizedTorque())
+    u_bounds.add([tau_min] * biorbd_model[0].nbGeneralizedTorque(), [tau_max] * biorbd_model[0].nbGeneralizedTorque())
+
+    u_init = InitialGuessList()
+    u_init.add([tau_init] * biorbd_model[0].nbGeneralizedTorque())
+    u_init.add([tau_init] * biorbd_model[0].nbGeneralizedTorque())
+    u_init.add([tau_init] * biorbd_model[0].nbGeneralizedTorque())
+    u_init.add([tau_init] * biorbd_model[0].nbGeneralizedTorque())
+
+    # Define phase transitions
+    phase_transitions = PhaseTransitionList()
+    phase_transitions.add(PhaseTransitionFcn.IMPACT, phase_pre_idx=1)
+    phase_transitions.add(PhaseTransitionFcn.CONTINUOUS, phase_pre_idx=2, idx_1=1, idx_2=3)
+    phase_transitions.add(PhaseTransitionFcn.CYCLIC)
+
+    return OptimalControlProgram(
+        biorbd_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_init,
+        u_init,
+        x_bounds,
+        u_bounds,
+        objective_functions,
+        constraints,
+        ode_solver=ode_solver,
+        phase_transitions=phase_transitions,
+    )
+
+def test_simple_phase_transitions():
+    bioptim_folder = TestUtils.bioptim_folder()
+    model_path = bioptim_folder + "/examples/getting_started/cube.bioMod"
+    ocp = prepare_ocp_phase_transitions(model_path)
+    ocp.print(to_console=True, to_graph=True)
+
+def test_mayer_phase_transitions():
+    bioptim_folder = TestUtils.bioptim_folder()
+    model_path = bioptim_folder + "/examples/getting_started/cube.bioMod"
+    ocp = prepare_ocp_phase_transitions(model_path, with_mayer=True)
+    ocp.print(to_console=True, to_graph=True)
+
+def test_lagrange_phase_transitions():
+    bioptim_folder = TestUtils.bioptim_folder()
+    model_path = bioptim_folder + "/examples/getting_started/cube.bioMod"
+    ocp = prepare_ocp_phase_transitions(model_path, with_lagrange=True)
+    ocp.print(to_console=True, to_graph=True)
+
+def test_constraints_phase_transitions():
+    bioptim_folder = TestUtils.bioptim_folder()
+    model_path = bioptim_folder + "/examples/getting_started/cube.bioMod"
+    ocp = prepare_ocp_phase_transitions(model_path, with_lagrange=True, with_mayer=True, with_constraints=True)
+    ocp.print(to_console=True, to_graph=True)
