dc522f7c110ed9c8f396d9096e16bf17155233b9
Pariterre <pariterre@hotmail.com>
2021-07-05 10:57:09 +0200

    Fix a vizualisation problem with the controls

--- bioptim/dynamics/configure_problem.py
+++ bioptim/dynamics/configure_problem.py
@@ -291,7 +291,7 @@ class ConfigureProblem:
                 lambda x, u, p: x[nlp.states[name].index, :],
                 plot_type=PlotType.INTEGRATED,
                 legend=legend,
-                bounds=nlp.x_bounds[nlp.states[name].index],  # TODO This is empty (this is a bug)
+                # bounds=nlp.x_bounds[nlp.states[name].index],  # TODO This is empty (this is a bug)
             )
 
         if as_controls:
@@ -300,10 +300,10 @@ class ConfigureProblem:
             nlp.controls.append(name, cx, mx, nlp.variable_mappings[name])
             plot_type = PlotType.PLOT if nlp.control_type == ControlType.LINEAR_CONTINUOUS else PlotType.STEP
             nlp.plot[f"{name}_controls"] = CustomPlot(
-                lambda x, u, p: x[nlp.controls[name].index, :],
+                lambda x, u, p: u[nlp.controls[name].index, :],
                 plot_type=plot_type,
                 legend=legend,
-                bounds=nlp.x_bounds[nlp.controls[name].index],  # TODO This is empty (this is a bug)
+                # bounds=nlp.u_bounds[nlp.controls[name].index],  # TODO This is empty (this is a bug)
                 combine_to=f"{name}_states" if as_states and combine_plot else None,
             )
 
