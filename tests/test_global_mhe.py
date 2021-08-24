8c066239c1963d27f5cb52fb6da2e69ae7f5b80c
Pariterre <pariterre@hotmail.com>
2021-08-24 18:01:23 -0400

    Blacked

--- tests/test_global_mhe.py
+++ tests/test_global_mhe.py
@@ -8,7 +8,6 @@ from .utils import TestUtils
 
 
 def test_cyclic_nmpc():
-
     def update_functions(_nmpc, cycle_idx, _sol):
         return cycle_idx < n_cycles  # True if there are still some cycle to perform
 
@@ -31,13 +30,13 @@ def test_cyclic_nmpc():
 
     # initial and final position
     np.testing.assert_equal(q.shape, (3, n_cycles * window_len))
-    np.testing.assert_almost_equal(q[:, 0], np.array((-3.14159265,  0.12979378,  2.77623291)))
+    np.testing.assert_almost_equal(q[:, 0], np.array((-3.14159265, 0.12979378, 2.77623291)))
     np.testing.assert_almost_equal(q[:, -1], np.array((2.82743339, 0.63193395, 2.68235056)))
 
     # initial and final velocities
-    np.testing.assert_almost_equal(qdot[:, 0], np.array((6.28268908, -11.63289399,   0.37215021)))
-    np.testing.assert_almost_equal(qdot[:, -1], np.array((6.28368154, -7.73180135,  3.56900657)))
+    np.testing.assert_almost_equal(qdot[:, 0], np.array((6.28268908, -11.63289399, 0.37215021)))
+    np.testing.assert_almost_equal(qdot[:, -1], np.array((6.28368154, -7.73180135, 3.56900657)))
 
     # initial and final controls
     np.testing.assert_almost_equal(tau[:, 0], np.array((0.01984925, 17.53758229, -1.92204945)))
-    np.testing.assert_almost_equal(tau[:, -2], np.array((0.01984925, -3.09892348,  0.23160067)))
+    np.testing.assert_almost_equal(tau[:, -2], np.array((0.01984925, -3.09892348, 0.23160067)))
