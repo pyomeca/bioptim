c85a8acfa4eb84a76ae8625e4c9f5ef1e6ebf137
Pariterre <pariterre@hotmail.com>
2021-10-22 12:39:10 -0400

    blacked again

--- examples/torque_driven_ocp/trampo_quaternions.py
+++ examples/torque_driven_ocp/trampo_quaternions.py
@@ -128,7 +128,7 @@ def main():
     Prepares and solves an ocp that has quaternion in it. Animates the results
     """
 
-    ocp = prepare_ocp("models/TruncAnd2Arm_Quaternion.bioMod", n_shooting=5, final_time=0.25,)
+    ocp = prepare_ocp("models/TruncAnd2Arm_Quaternion.bioMod", n_shooting=5, final_time=0.25)
     sol = ocp.solve(Solver.IPOPT(show_online_optim=True))
 
     # Print the last solution
