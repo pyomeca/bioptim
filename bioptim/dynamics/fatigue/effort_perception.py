b832e17b67e69dfd54d8b9f8a14099ab28505c66
Pariterre <pariterre@hotmail.com>
2021-10-26 14:44:01 -0400

    Fixed effort perception when for tau_min

--- bioptim/dynamics/fatigue/effort_perception.py
+++ bioptim/dynamics/fatigue/effort_perception.py
@@ -1,4 +1,4 @@
-from casadi import gt, if_else
+from casadi import if_else, fabs
 
 from .muscle_fatigue import MuscleFatigue
 from .tau_fatigue import TauFatigue
@@ -25,6 +25,7 @@ class EffortPerception(MuscleFatigue):
         super(EffortPerception, self).__init__(scaling=scaling)
         self.effort_threshold = effort_threshold
         self.effort_factor = effort_factor
+        self.abs_effort_factor = fabs(self.effort_threshold)
         self.abs_scaling = self.scaling if self.scaling >= 0 else -self.scaling
 
     @staticmethod
@@ -49,12 +50,14 @@ class EffortPerception(MuscleFatigue):
 
     def apply_dynamics(self, target_load, *states):
         effort = states[0]
-        delta_load = (target_load - self.effort_threshold) * if_else(
-            gt(target_load - self.effort_threshold, 0),
-            1 / (self.abs_scaling - self.effort_threshold),
-            1 / self.effort_threshold
+        abs_target_load = fabs(target_load)
+
+        delta_load = (abs_target_load - self.abs_effort_factor) * if_else(
+            abs_target_load > self.abs_effort_factor,
+            1 / (self.abs_scaling - self.abs_effort_factor),
+            1 / self.abs_effort_factor
         )
-        mf_long_dot = self.effort_factor * if_else(gt(delta_load, 0), 1 - effort, effort) * delta_load
+        mf_long_dot = self.effort_factor * if_else(delta_load > 0, 1 - effort, effort) * delta_load
         return mf_long_dot
 
     @staticmethod
