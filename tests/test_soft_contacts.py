f068c83f9aafd892867c86d63b9b00fa81855740
Ipuch <puchaud.pierre@gmail.com>
2022-01-07 03:33:49 -0500

    pytest modified

--- tests/test_soft_contacts.py
+++ tests/test_soft_contacts.py
@@ -6,22 +6,23 @@ from bioptim import OdeSolver
 from .utils import TestUtils
 
 
-@pytest.mark.parametrize("n_threads", [8])
-@pytest.mark.parametrize("use_sx", [False])
-@pytest.mark.parametrize("ode_solver", [OdeSolver.RK8])
-def test_soft_contact(ode_solver, use_sx, n_threads):
+# @pytest.mark.parametrize("n_threads", [8])
+# @pytest.mark.parametrize("use_sx", [False])
+# @pytest.mark.parametrize("ode_solver", [OdeSolver.RK8])
+# def test_soft_contact(ode_solver, use_sx, n_threads):
+def test_soft_contact():
     from bioptim.examples.torque_driven_ocp import example_soft_contact as ocp_module
 
     bioptim_folder = os.path.dirname(ocp_module.__file__)
 
-    ode_solver = ode_solver()
+    ode_solver = OdeSolver.RK8()
 
     ocp = ocp_module.prepare_ocp(
         biorbd_model_path=bioptim_folder + "/models/soft_contact_sphere.bioMod",
         final_time=0.37,
         n_shooting=37,
-        n_threads=n_threads,
-        use_sx=use_sx,
+        n_threads=8,
+        use_sx=False,
         ode_solver=ode_solver,
     )
 
