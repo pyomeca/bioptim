36c700e0ebfcbe4471a1faf86eeb0d51686eabb4
Ipuch <puchaud.pierre@gmail.com>
2022-05-19 16:54:22 -0400

    small adjustement on the docstring

--- bioptim/dynamics/dynamics_evaluation.py
+++ bioptim/dynamics/dynamics_evaluation.py
@@ -7,9 +7,9 @@ class DynamicsEvaluation:
     Attributes
     ----------
     dxdt: Union[MX, SX]
-        The derivative of the states
+        The derivative of the states xdot = f(x,u,p)
     defects: Union[MX, SX]
-        defects of the dynamics for implicit transcription
+        defects of the dynamics for implicit transcription  f(xdot,x,u,p) = 0
     """
 
     def __init__(self, dxdt: Union[MX, SX] = None, defects: Union[MX, SX] = None):
