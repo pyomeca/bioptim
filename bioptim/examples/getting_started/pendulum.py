4201666d43508c22f46261e613f945d74bef6a94
Ipuch <puchaud.pierre@gmail.com>
2022-06-09 13:38:31 -0400

    restored example

--- bioptim/examples/getting_started/pendulum.py
+++ bioptim/examples/getting_started/pendulum.py
@@ -30,8 +30,8 @@ def prepare_ocp(
     biorbd_model_path: str,
     final_time: float,
     n_shooting: int,
-    ode_solver: OdeSolver = OdeSolver.IRK(),
-    use_sx: bool = False,
+    ode_solver: OdeSolver = OdeSolver.RK4(),
+    use_sx: bool = True,
     n_threads: int = 1,
 ) -> OptimalControlProgram:
     """
