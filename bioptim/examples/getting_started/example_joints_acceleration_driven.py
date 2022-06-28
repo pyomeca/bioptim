188a1bfffb141d510325e087c1ec438b68ae5be2
Francisco Pascoa <francisco.pascoa@umontreal.ca>
2022-06-28 16:43:55 -0400

    added example for JOINTS_ACCELERATION_DRIVEN

---
+++ bioptim/examples/getting_started/example_joints_acceleration_driven.py
@@ -0,0 +1,124 @@
+"""
+A very simple yet meaningful optimal control program consisting of a double pendulum starting downward and ending upward
+while requiring the minimum of generalized forces. The solver is only allowed to move accelerate the joint linking the
+pendula together.
+
+During the optimization process, the graphs are updated real-time (even though it is a bit too fast and short to really
+appreciate it). Finally, once it finished optimizing, it animates the model using the optimal solution
+"""
+
+import biorbd_casadi as biorbd
+from bioptim import (
+    OptimalControlProgram,
+    DynamicsFcn,
+    Dynamics,
+    Bounds,
+    QAndQDotBounds,
+    InitialGuess,
+    ObjectiveFcn,
+    Objective,
+    OdeSolver,
+    CostType,
+    Solver,
+)
+
+
+def prepare_ocp(
+    biorbd_model_path: str,
+    final_time: float,
+    n_shooting: int,
+    ode_solver: OdeSolver = OdeSolver.RK4(),
+    use_sx: bool = True,
+    n_threads: int = 1,
+) -> OptimalControlProgram:
+    """
+    The initialization of an ocp
+
+    Parameters
+    ----------
+    biorbd_model_path: str
+        The path to the biorbd model
+    final_time: float
+        The time in second required to perform the task
+    n_shooting: int
+        The number of shooting points to define int the direct multiple shooting program
+    ode_solver: OdeSolver = OdeSolver.RK4()
+        Which type of OdeSolver to use
+    use_sx: bool
+        If the SX variable should be used instead of MX (can be extensive on RAM)
+    n_threads: int
+        The number of threads to use in the paralleling (1 = no parallel computing)
+
+    Returns
+    -------
+    The OptimalControlProgram ready to be solved
+    """
+
+    biorbd_model = biorbd.Model(biorbd_model_path)
+
+    # Add objective functions
+    objective_functions = Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="qddot_joints")
+
+    # Dynamics
+    dynamics = Dynamics(DynamicsFcn.JOINTS_ACCELERATION_DRIVEN)
+
+    # Path constraint
+    x_bounds = QAndQDotBounds(biorbd_model)
+    x_bounds[:, [0, -1]] = 0
+    x_bounds[0, -1] = 3.14
+    x_bounds[1, -1] = 0
+
+    # Initial guess
+    n_q = biorbd_model.nbQ()
+    n_qdot = biorbd_model.nbQdot()
+    x_init = InitialGuess([0] * (n_q + n_qdot))
+
+    # Define control path constraint
+    n_qddot = biorbd_model.nbQddot() - biorbd_model.nbRoot()  # 2 - 1 = 1 in this example
+    tau_min, tau_max, tau_init = -100, 100, 0
+    u_bounds = Bounds([tau_min] * n_qddot, [tau_max] * n_qddot)
+
+    u_init = InitialGuess([tau_init] * n_qddot)
+
+    return OptimalControlProgram(
+        biorbd_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_init=x_init,
+        u_init=u_init,
+        x_bounds=x_bounds,
+        u_bounds=u_bounds,
+        objective_functions=objective_functions,
+        ode_solver=ode_solver,
+        use_sx=use_sx,
+        n_threads=n_threads,
+    )
+
+
+def main():
+    """
+    If pendulum is run as a script, it will perform the optimization and animates it
+    """
+
+    # --- Prepare the ocp --- #
+    ocp = prepare_ocp(biorbd_model_path="models/double_pendulum.bioMod", final_time=10, n_shooting=100)
+
+    # Custom plots
+    ocp.add_plot_penalty(CostType.ALL)
+
+    # --- Print ocp structure --- #
+    ocp.print(to_console=False, to_graph=False)
+
+    # --- Solve the ocp --- #
+    sol = ocp.solve(Solver.IPOPT(show_online_optim=True))
+    # sol.graphs()
+
+    # --- Show the results in a bioviz animation --- #
+    sol.detailed_cost_values()
+    sol.print_cost()
+    sol.animate(n_frames=100)
+
+
+if __name__ == "__main__":
+    main()
