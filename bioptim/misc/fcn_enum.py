ebcf8d38f07da323ae6297ce3071852c446fe0ee
Francisco Pascoa <francisco.pascoa@umontreal.ca>
2022-07-28 16:38:43 -0400

    applied review changes for fcn_enum

--- bioptim/misc/fcn_enum.py
+++ bioptim/misc/fcn_enum.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from enum import Enum
 
 
@@ -6,25 +7,20 @@ class FcnEnum(Enum):
         """
         Call the member.
         """
-        if args and kwargs:
-            return self.value[0](*args, **kwargs)
-        elif args and not kwargs:
-            return self.value[0](*args)
-        elif not args and kwargs:
-            return self.value[0](**kwargs)
-        elif not args and not kwargs:
-            return self.value[0]()
+        return self.value[0](*args, **kwargs)
 
     @staticmethod
+    @abstractmethod
     def get_type():
         """
         Returns the type of the member.
         """
-        raise NotImplementedError("Not implemented by enum.")
+        pass
 
     @staticmethod
+    @abstractmethod
     def get_fcn_types():
         """
         Returns the types of the enum.
         """
-        raise NotImplementedError("Not implemented by enum.")
+        pass
