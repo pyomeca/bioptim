20816cf6005cdbbb1f3d3bb1f73da033ddd97888
Ipuch <puchaud.pierre@gmail.com>
2022-08-01 11:56:31 -0400

    forgot this line

--- bioptim/limits/path_conditions.py
+++ bioptim/limits/path_conditions.py
@@ -930,7 +930,6 @@ class NoisedInitialGuess(InitialGuess):
             * self.noise_magnitude  # magnitude of the noise within the range defined by the bounds
             * (bounds_max_matrix - bounds_min_matrix)  # scale the noise to the range of bounds
             + bounds_min_matrix + (1 - self.noise_magnitude) * (bounds_max_matrix - bounds_min_matrix) /2 # shift the noise to the center of the range of bounds
-            + (bounds_min_matrix + bounds_max_matrix) / 2  # shift the noise to the center of bounds
         )
         # building the noised initial guess
         self.noised_initial_guess = init_instance.init + self.noise
