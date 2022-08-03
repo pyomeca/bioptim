6a2eaefb166d3268f745a37a909d65399076a31f
thasaarah <thasaarah@gmail.com>
2022-08-03 14:28:56 +0000

    removed increment

--- bioptim/optimization/optimization_vector.py
+++ bioptim/optimization/optimization_vector.py
@@ -144,14 +144,13 @@ class OptimizationVector:
             else:
                 interpolation_type = None
             if nlp.ode_solver.is_direct_collocation and interpolation_type == InterpolationType.EACH_FRAME:
-                index = 0
                 n_points = nlp.ode_solver.polynomial_degree + 1
                 x_init_vector = np.zeros(self.n_all_x)
                 init_values = self.ocp.original_values["x_init"].init
-                for state in init_values:
-                    for point in range(nlp.ns):
-                        x_init_vector[index: index + n_points] = np.linspace(state[point], state[point + 1], n_points)
-                        index += n_points
+                for index, state in enumerate(init_values):
+                    for frame in range(nlp.ns):
+                        point = (index * nlp.ns + frame) * n_points
+                        x_init_vector[point: point + n_points] = np.linspace(state[frame], state[frame + 1], n_points)
                 v_init.concatenate(InitialGuess(x_init_vector))
             else:
                 v_init.concatenate(x_init)
