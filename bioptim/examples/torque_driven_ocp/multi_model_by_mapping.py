b6d25049a0de5b6aecf384c83002248112fc763b
Charbie <eve.charbonneau.1@umontreal.ca>
2022-09-27 11:59:10 -0400

    added example of no interphase continuity

---
+++ bioptim/examples/torque_driven_ocp/multi_model_by_mapping.py
@@ -0,0 +1,122 @@
+import biorbd_casadi as biorbd
+import numpy as np
+from bioptim import (
+    OptimalControlProgram,
+    DynamicsList,
+    DynamicsFcn,
+    ObjectiveList,
+    ConstraintList,
+    ConstraintFcn,
+    BoundsList,
+    InitialGuessList,
+    Node,
+    QAndQDotBounds,
+    ObjectiveFcn,
+    BiMappingList,
+    Axis,
+    PhaseTransitionList,
+    PhaseTransitionFcn,
+    BiMapping,
+)
+
+
+def prepare_ocp(
+    biorbd_model_path: str = "models/double_pendulum.bioMod",
+    biorbd_model_path_modified_inertia: str = "models/double_pendulum_modified_inertia.bioMod",
+) -> OptimalControlProgram:
+
+    biorbd_model = (biorbd.Model(biorbd_model_path), biorbd.Model(biorbd_model_path_modified_inertia))
+
+    # Problem parameters
+    n_shooting = (40, 40)
+    final_time = (1.5, 1.5)
+    tau_min, tau_max, tau_init = -200, 200, 0
+
+    # Mapping
+    tau_mappings = BiMappingList()
+    tau_mappings.add("tau", [None, 0], [1], phase=0)
+    tau_mappings.add("tau", [None, 0], [1], phase=1)
+
+    # Add objective functions
+    objective_functions = ObjectiveList()
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=1, phase=0)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=1, phase=1)
+
+    # Constraints
+    constraints = ConstraintList()
+    constraints.add(ConstraintFcn.TIME_CONSTRAINT, node=Node.END, min_bound=0.3, max_bound=3, phase=0)
+    constraints.add(ConstraintFcn.TIME_CONSTRAINT, node=Node.END, min_bound=0.3, max_bound=3, phase=1)
+    # time constraint ##############
+
+    # Dynamics
+    dynamics = DynamicsList()
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=False)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=False)
+
+    # Path constraint
+    x_bounds = BoundsList()
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[1]))
+
+    # Phase 0
+    x_bounds[0][0, 0] = - np.pi
+    x_bounds[0][1, 0] = 0
+    x_bounds[0][0, 2] = np.pi
+    x_bounds[0][1, 2] = 0
+
+    # Phase 1
+    x_bounds[1][0, 0] = 0
+    x_bounds[1][1, 0] = 0
+    x_bounds[1][0, 2] = 2 * np.pi
+    x_bounds[1][1, 2] = 0
+
+    # Initial guess
+    x_init = InitialGuessList()
+    x_init.add([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
+    x_init.add([0] * (biorbd_model[1].nbQ() + biorbd_model[1].nbQdot()))
+
+    # Define control path constraint
+    u_bounds = BoundsList()
+    u_bounds.add([tau_min] * len(tau_mappings[0]["tau"].to_first), [tau_max] * len(tau_mappings[0]["tau"].to_first))
+    u_bounds.add([tau_min] * len(tau_mappings[1]["tau"].to_first), [tau_max] * len(tau_mappings[1]["tau"].to_first))
+
+    # Control initial guess
+    u_init = InitialGuessList()
+    u_init.add([tau_init] * len(tau_mappings[0]["tau"].to_first))
+    u_init.add([tau_init] * len(tau_mappings[1]["tau"].to_first))
+
+    phase_transitions = PhaseTransitionList()
+    phase_transitions.add(
+        PhaseTransitionFcn.DISCONTINUOUS, phase_pre_idx=0,
+    )
+
+    return OptimalControlProgram(
+        biorbd_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_init=x_init,
+        u_init=u_init,
+        x_bounds=x_bounds,
+        u_bounds=u_bounds,
+        objective_functions=objective_functions,
+        constraints=constraints,
+        variable_mappings=tau_mappings,
+        phase_transitions=phase_transitions,
+    )
+
+
+def main():
+
+    # --- Prepare the ocp --- #
+    ocp = prepare_ocp()
+
+    # --- Solve the program --- #
+    sol = ocp.solve()
+
+    # --- Show results --- #
+    sol.animate(n_frames=80)
+
+
+if __name__ == "__main__":
+    main()
