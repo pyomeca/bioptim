8ec32ad3a69a0e03528441d2db78fcf1cf61bce6
Amedeo <amedeo.ceglia@umontreal.ca>
2022-10-17 15:12:18 -0400

    Forgot to comit show online

--- bioptim/examples/muscle_driven_ocp/static_arm.py
+++ bioptim/examples/muscle_driven_ocp/static_arm.py
@@ -108,7 +108,7 @@ def main():
     ocp = prepare_ocp(biorbd_model_path="models/arm26.bioMod", final_time=0.5, n_shooting=50, weight=1000)
 
     # --- Solve the program --- #
-    sol = ocp.solve(Solver.IPOPT(show_online_optim=False))
+    sol = ocp.solve(Solver.IPOPT(show_online_optim=True))
 
     # --- Show results --- #
     sol.animate(show_meshes=True)
