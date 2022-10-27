0ef20a2161fe3e57066a5f3e7ff038929fd3ac2d
Nicolas <nicolas.richard@epfedu.fr>
2022-10-27 17:07:39 -0400

    add of MagnitudeType in enums

--- bioptim/misc/enums.py
+++ bioptim/misc/enums.py
@@ -165,3 +165,8 @@ class DefectType(Enum):
     EXPLICIT = "explicit"
     IMPLICIT = "implicit"
     NOT_APPLICABLE = "not_applicable"
+
+
+class MagnitudeType(Enum):
+    RELATIVE = 0
+    ABSOLUTE = 1
