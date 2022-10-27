271b5d4d0067a796e4a7b05c8382724b08a437eb
Charbie <eve.charbonneau.1@umontreal.ca>
2022-10-27 13:55:04 -0400

    uncommented tests since it was not my fault

--- tests/test_continuity_as_objective.py
+++ tests/test_continuity_as_objective.py
@@ -1,17 +1,17 @@
-# import numpy as np
-# from .utils import TestUtils
-#
-#
-# def test_continuity_as_objective():
-#     from bioptim.examples.getting_started import example_continuity_as_objective as ocp_module
-#
-#     np.random.seed(42)
-#     model_path = TestUtils.bioptim_folder() + "/examples/getting_started/models/pendulum_maze.bioMod"
-#
-#     ocp = ocp_module.prepare_ocp_first_pass(
-#         biorbd_model_path=model_path, final_time=1, n_shooting=30, state_continuity_weight=1000000
-#     )
-#     sol = ocp.solve()
-#
-#     expected = np.array([-0.1376, 2.9976372])
-#     np.testing.assert_almost_equal(sol.states["q"][:, -1], expected)
+import numpy as np
+from .utils import TestUtils
+
+
+def test_continuity_as_objective():
+    from bioptim.examples.getting_started import example_continuity_as_objective as ocp_module
+
+    np.random.seed(42)
+    model_path = TestUtils.bioptim_folder() + "/examples/getting_started/models/pendulum_maze.bioMod"
+
+    ocp = ocp_module.prepare_ocp_first_pass(
+        biorbd_model_path=model_path, final_time=1, n_shooting=30, state_continuity_weight=1000000
+    )
+    sol = ocp.solve()
+
+    expected = np.array([-0.1376, 2.9976372])
+    np.testing.assert_almost_equal(sol.states["q"][:, -1], expected)
--- tests/test_global_fail.py
+++ tests/test_global_fail.py
@@ -1,34 +1,34 @@
-# import pytest
-#
-# import biorbd_casadi as biorbd
-# from casadi import MX
-# from bioptim import (
-#     Node,
-#     OptimalControlProgram,
-#     Dynamics,
-#     DynamicsFcn,
-#     ConstraintList,
-# )
-#
-# from .utils import TestUtils
-#
-#
-# def test_custom_constraint_mx_fail():
-#     def custom_mx_fail(pn):
-#         if pn.u is None:
-#             return None
-#         u = pn.nlp.controls
-#         return MX.zeros(u.shape), u.cx, MX.zeros(u.shape)
-#
-#     bioptim_folder = TestUtils.bioptim_folder()
-#     model_path = bioptim_folder + "/examples/getting_started/models/cube.bioMod"
-#
-#     constraints = ConstraintList()
-#     constraints.add(custom_mx_fail, node=Node.ALL)
-#
-#     ocp = OptimalControlProgram(
-#         biorbd.Model(model_path), Dynamics(DynamicsFcn.TORQUE_DRIVEN), 30, 2, constraints=constraints
-#     )
-#
-#     with pytest.raises(RuntimeError, match="Ipopt doesn't support SX/MX types in constraints bounds"):
-#         ocp.solve()
+import pytest
+
+import biorbd_casadi as biorbd
+from casadi import MX
+from bioptim import (
+    Node,
+    OptimalControlProgram,
+    Dynamics,
+    DynamicsFcn,
+    ConstraintList,
+)
+
+from .utils import TestUtils
+
+
+def test_custom_constraint_mx_fail():
+    def custom_mx_fail(pn):
+        if pn.u is None:
+            return None
+        u = pn.nlp.controls
+        return MX.zeros(u.shape), u.cx, MX.zeros(u.shape)
+
+    bioptim_folder = TestUtils.bioptim_folder()
+    model_path = bioptim_folder + "/examples/getting_started/models/cube.bioMod"
+
+    constraints = ConstraintList()
+    constraints.add(custom_mx_fail, node=Node.ALL)
+
+    ocp = OptimalControlProgram(
+        biorbd.Model(model_path), Dynamics(DynamicsFcn.TORQUE_DRIVEN), 30, 2, constraints=constraints
+    )
+
+    with pytest.raises(RuntimeError, match="Ipopt doesn't support SX/MX types in constraints bounds"):
+        ocp.solve()
