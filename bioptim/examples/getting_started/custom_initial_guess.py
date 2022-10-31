ac6e6f0f8ff48cd6a4c43f6be0aa6c136bea5ca2
Nicolas <nicolas.richard@epfedu.fr>
2022-10-31 16:48:55 -0400

    Patch for tests and getting started file

--- bioptim/examples/getting_started/custom_initial_guess.py
+++ bioptim/examples/getting_started/custom_initial_guess.py
@@ -17,6 +17,7 @@ InterpolationType.CUSTOM: Provide a user-defined interpolation function
 from typing import Union
 import numpy as np
 import biorbd_casadi as biorbd
+from bioptim.misc.enums import MagnitudeType
 from bioptim import (
     Node,
     OptimalControlProgram,
@@ -160,10 +161,10 @@ def prepare_ocp(
             t=t,
             interpolation=initial_guess,
             bounds=x_bounds,
-            noise_magnitude=1,
+            magnitude=1,
+            magnitude_type=MagnitudeType.RELATIVE,
             n_shooting=n_shooting,
             bound_push=0.1,
-            **extra_params_x,
         )
         u_init = NoisedInitialGuess(
             u,
