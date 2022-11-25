60c88802e0844668490ab0b91894d2890b6813bc
Ipuch <pierre.puchaud@umontreal.ca>
2022-11-25 12:31:44 -0500

    cleaning main

--- bioptim/examples/custom_model/main.py
+++ bioptim/examples/custom_model/main.py
@@ -16,7 +16,6 @@ from bioptim import (
     Solver,
     Model,
     DynamicsList,
-    ObjectiveList,
 )
 
 # import the custom model
@@ -31,8 +30,6 @@ def prepare_ocp(
     final_time: float,
     n_shooting: int,
     ode_solver: OdeSolver = OdeSolver.RK4(n_integration_steps=5),
-    use_sx: bool = True,
-    n_threads: int = 1,
 ) -> OptimalControlProgram:
     """
     The initialization of an ocp
@@ -40,17 +37,13 @@ def prepare_ocp(
     Parameters
     ----------
     model: Model
-        The path to the biorbd model
+        The object of the custom model
     final_time: float
         The time in second required to perform the task
     n_shooting: int
         The number of shooting points to define int the direct multiple shooting program
     ode_solver: OdeSolver = OdeSolver.RK4()
         Which type of OdeSolver to use
-    use_sx: bool
-        If the SX variable should be used instead of MX (can be extensive on RAM)
-    n_threads: int
-        The number of threads to use in the paralleling (1 = no parallel computing)
 
     Returns
     -------
@@ -58,25 +51,21 @@ def prepare_ocp(
     """
 
     # Add objective functions
-    objective_functions = ObjectiveList()
-    # objective_functions = Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau")
+    objective_functions = Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau")
 
     # Dynamics
-    # dynamics = Dynamics(DynamicsFcn.TORQUE_DRIVEN)
-    # Declare the dynamics
     dynamics = DynamicsList()
     dynamics.add(custom_configure_my_dynamics, dynamic_function=custom_dynamics)
 
     # Path constraint
+    # the pendulum is constrained to point down with zero velocity at the beginning
+    # the pendulum has to be in vertical position pointing up
+    # at the end of the movement (3.14 rad) with zero speed (0 rad/s)
     x_bounds = Bounds(
         min_bound=np.array([[0, -6.28, 3.14], [0, -20, 0]]),
         max_bound=np.array([[0, 6.28, 3.14], [0, 20, 0]]),
         interpolation=InterpolationType.CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT,
     )
-    # x_bounds[:, [0, -1]] = 0
-    # x_bounds[0, -1] = 3.14
-    # x_bounds[0, 0] = 1
-    # x_bounds[0, -1] = 1
 
     # Initial guess
     n_q = model.nbQ()
@@ -101,14 +90,14 @@ def prepare_ocp(
         u_bounds=u_bounds,
         objective_functions=objective_functions,
         ode_solver=ode_solver,
-        use_sx=use_sx,
-        n_threads=n_threads,
+        use_sx=False,
+        n_threads=2,
     )
 
 
 def main():
     """
-    If pendulum is run as a script, it will perform the optimization and animates it
+    If main.py is run as a script, it will perform the optimization
     """
 
     # --- Prepare the ocp --- #
@@ -123,14 +112,16 @@ def main():
     # --- Solve the ocp --- #
     solver = Solver.IPOPT(show_online_optim=False)
     solver.set_maximum_iterations(100)
-
     sol = ocp.solve(solver=solver)
-    sol.graphs(show_bounds=True)
 
     # --- Show results --- #
+    sol.graphs(show_bounds=True)
     sol.detailed_cost_values()
     sol.print_cost()
-    # sol.animate(n_frames=100)
+
+    # --- Animation --- #
+    # not implemented yet
+    sol.animate()
 
 
 if __name__ == "__main__":
