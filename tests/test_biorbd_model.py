eb75f8f2841d512229a4a82fb9ddeb3c2f1a3811
Antoine <Antoine.3bee@gmail.com>
2023-01-26 11:07:32 -0100

    update

--- tests/test_biorbd_model.py
+++ tests/test_biorbd_model.py
@@ -5,40 +5,20 @@ It tests that a model path with another type than string or biorbdmodel return a
 
 import os
 import pytest
-
-import numpy as np
+import biorbd_casadi as biorbd
 from bioptim import (
-    OdeSolver,
+    BiorbdModel,
 )
 
-from .utils import TestUtils
-
-
-@pytest.mark.parametrize("model", ["/models/pendulum.bioMod"])
-def test_biorbd_model_fail(model):
+def test_biorbd_model_fail():
     # Load pendulum_min_time_Mayer
     from bioptim.examples.getting_started import pendulum as ocp_module
 
     bioptim_folder = os.path.dirname(ocp_module.__file__)
+    model_path = "/models/pendulum.bioMod"
+    BiorbdModel(bioptim_folder + model_path)
+
+    BiorbdModel(biorbd.Model(bioptim_folder + model_path))
 
     with pytest.raises(RuntimeError, match="Type must be a string or a biorbdmodel"):
-        ocp = ocp_module.prepare_ocp(
-            biorbd_model_path=[bioptim_folder + model],
-            final_time=2,
-            n_shooting=10,
-            ode_solver=OdeSolver.COLLOCATION(),
-        )
-    with pytest.raises(RuntimeError, match="Type must be a string or a biorbdmodel"):
-        ocp = ocp_module.prepare_ocp(
-            biorbd_model_path=np.array(bioptim_folder + model),
-            final_time=2,
-            n_shooting=10,
-            ode_solver=OdeSolver.COLLOCATION(),
-        )
-    with pytest.raises(RuntimeError, match="Type must be a string or a biorbdmodel"):
-        ocp = ocp_module.prepare_ocp(
-            biorbd_model_path=1,
-            final_time=2,
-            n_shooting=10,
-            ode_solver=OdeSolver.COLLOCATION(),
-        )
+        BiorbdModel(1)
