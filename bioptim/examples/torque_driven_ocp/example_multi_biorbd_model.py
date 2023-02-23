048f4ddebb84b8c9f211d6bf121240f8fbcda12d
Charbie <eve.charbonneau.1@umontreal.ca>
2023-02-23 15:05:23 -0500

    fixed +cleaned example

--- bioptim/examples/torque_driven_ocp/example_multi_biorbd_model.py
+++ bioptim/examples/torque_driven_ocp/example_multi_biorbd_model.py
@@ -1,6 +1,6 @@
 """
-This example inverts a triple pendulum with different inertia.
-The first DoF is not actuated, the second DoF in actuated with the same torque and the last DoF is independent for the two models.
+This example executes on full rotation of two triple pendulums with different inertia.
+The first DoF of each model is not actuated, the second DoF is actuated with the same torque for each model and the last DoF are independently actuated for the two models.
 """
 
 import numpy as np
@@ -22,6 +22,7 @@ def prepare_ocp(
     biorbd_model_path_modified_inertia: str = "models/triple_pendulum_modified_inertia.bioMod",
     n_shooting: int = 40,
 ) -> OptimalControlProgram:
+
     # Adding the models to the same phase
     bio_models = MultiBiorbdModel((biorbd_model_path, biorbd_model_path_modified_inertia))
 
@@ -38,7 +39,6 @@ def prepare_ocp(
     objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=1)
     objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_STATE, key="q", weight=1)
     objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, weight=1e-6)
-    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_COM_POSITION, weight=-100)
 
     # Dynamics
     dynamics = DynamicsList()
@@ -50,9 +50,11 @@ def prepare_ocp(
 
     x_bounds[0][[0, 3], 0] = -np.pi
     x_bounds[0][[1, 4], 0] = 0
+    x_bounds[0][[2, 5], 0] = 0
     x_bounds[0].min[[0, 3], 2] = np.pi - 0.1
     x_bounds[0].max[[0, 3], 2] = np.pi + 0.1
     x_bounds[0][[1, 4], 2] = 0
+    x_bounds[0][[2, 5], 2] = 0
 
     # Initial guess
     x_init = InitialGuessList()
@@ -83,16 +85,17 @@ def prepare_ocp(
 def main():
     # --- Prepare the ocp --- #
     ocp = prepare_ocp()
+    ocp.add_plot_penalty()
 
     # --- Solve the program --- #
     sol = ocp.solve()
 
-    sol.graphs()
+    sol.graphs(show_bounds=True)
 
     # --- Show results --- #
     show_solution_animation = False
     if show_solution_animation:
-        q = sol.states[0]["q"]
+        q = sol.states["q"]
         import bioviz
 
         b = bioviz.Viz("models/triple_pendulum_both_inertia.bioMod")
