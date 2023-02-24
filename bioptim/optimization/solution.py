2f3f0f97adec8f85a1c1390d659d657897ce476a
Ipuch <pierre.puchaud@umontreal.ca>
2023-02-24 14:08:52 -0500

    test again

--- bioptim/optimization/solution.py
+++ bioptim/optimization/solution.py
@@ -1539,7 +1539,7 @@ class Solution:
                 running_total += val_weighted
 
                 if penalty.node != Node.TRANSITION:
-                    node_name = f"{penalty.node[0]}" if isinstance(penalty.node[0], int) else penalty.node[0].name
+                    node_name = str(penalty.node[0]) if isinstance(penalty.node[0], int) else penalty.node[0].name
                 else:
                     node_name = penalty.node.name
 
