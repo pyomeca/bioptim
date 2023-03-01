4dcf21334b7c01bfaf7978216e9ae343dc0fea1b
Ipuch <pierre.puchaud@umontreal.ca>
2023-03-01 18:57:26 -0500

    a small PR, on the fly

--- bioptim/dynamics/configure_problem.py
+++ bioptim/dynamics/configure_problem.py
@@ -179,7 +179,8 @@ class ConfigureProblem:
             A list of fatigue elements
 
         """
-
+        if with_contact and nlp.model.nb_contacts == 0:
+            raise ValueError("No contact defined in the .bioMod, set with_contact to False")
         if nlp.model.nb_soft_contacts != 0:
             if (
                 soft_contacts_dynamics != SoftContactDynamics.CONSTRAINT
@@ -318,6 +319,9 @@ class ConfigureProblem:
             which soft contact dynamic should be used
 
         """
+        if with_contact and nlp.model.nb_contacts == 0:
+            raise ValueError("No contact defined in the .bioMod, set with_contact to False")
+
         if rigidbody_dynamics not in (RigidBodyDynamics.DAE_INVERSE_DYNAMICS, RigidBodyDynamics.ODE):
             raise NotImplementedError("TORQUE_DERIVATIVE_DRIVEN cannot be used with this enum RigidBodyDynamics yet")
 
@@ -397,6 +401,9 @@ class ConfigureProblem:
         with_passive_torque: bool
             If the dynamic with passive torque should be used
         """
+
+        if with_contact and nlp.model.nb_contacts == 0:
+            raise ValueError("No contact defined in the .bioMod, set with_contact to False")
 
         ConfigureProblem.configure_q(ocp, nlp, True, False)
         ConfigureProblem.configure_qdot(ocp, nlp, True, False)
@@ -500,6 +507,8 @@ class ConfigureProblem:
             which rigidbody dynamics should be used
 
         """
+        if with_contact and nlp.model.nb_contacts == 0:
+            raise ValueError("No contact defined in the .bioMod, set with_contact to False")
 
         if fatigue is not None and "tau" in fatigue and not with_torque:
             raise RuntimeError("Residual torques need to be used to apply fatigue on torques")
