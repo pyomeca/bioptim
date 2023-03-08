6e9ff5734fbadb8cffe98cede9a0761c589a1525
lsechoir <l.sechoirpro@outlook.fr>
2023-03-08 09:46:05 -0500

    added check_already_done + forr_loop_debug options

--- bioptim/optimization/multi_start.py
+++ bioptim/optimization/multi_start.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 from ..optimization.optimal_control_program import OptimalControlProgram
 from ..interfaces.solver_options import Solver
-
+import os
 
 class MultiStart:
     """
@@ -26,6 +26,9 @@ class MultiStart:
         solver: Solver,
         post_optimization_callback: Callable = None,
         n_pools: int = 1,
+        already_done : list = None,
+        single_process_debug_flag : bool = False,
+        check_already_done : Callable = None,
         **kwargs,
     ):
         """
@@ -45,8 +48,14 @@ class MultiStart:
         self.solver = solver
         self.post_optimization_callback = post_optimization_callback
         self.n_pools = n_pools
+        self.already_done = already_done
+        self.single_process_debug_flag = single_process_debug_flag
         self.args_dict = kwargs
         self.combined_args_to_list = self.combine_args_to_list()
+
+
+    def check_already_done(self, args_copy):
+        return self.post_optimization_callback([None], *args_copy, only_save_filename=True) in self.already_done
 
     def combine_args_to_list(self):
         """
@@ -55,19 +64,27 @@ class MultiStart:
         """
         combined_args = [instance for instance in product(*self.args_dict.values())]
         combined_args_to_list = []
-        for i in range(len(combined_args)):
+
+        for i in range(len(combined_args)) :
             combined_args_to_list += [[instance for instance in combined_args[i]]]
         return combined_args_to_list
 
     def solve_ocp_func(self, args):
-        sol = self.prepare_ocp(*args).solve(self.solver)
-        self.post_optimization_callback(sol, *args)
-        return
+        check_already_done = self.check_already_done(args)
+        if not check_already_done:
+            sol = self.prepare_ocp(*args).solve(self.solver)
+            self.post_optimization_callback(sol, *args)
+        else:
+            print("This OCP already has a saved solution in the designated folder.")
 
     def solve(self):
         """
         Run the multi-start in the pools for multi-threading
         """
-
-        with Pool(self.n_pools) as p:
-            p.map(self.solve_ocp_func, self.combined_args_to_list)
+        single_process_debug_flag = self.single_process_debug_flag
+        if single_process_debug_flag == True :
+            for args in self.combined_args_to_list :
+                self.solve_ocp_func(args)
+        else:
+            with Pool(self.n_pools) as p:
+                p.map(self.solve_ocp_func, self.combined_args_to_list)
