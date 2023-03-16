9d90027c7d297b443ed74786a87e4e0bffa152eb
Charbie <eve.charbonneau.1@umontreal.ca>
2023-03-16 11:11:08 -0400

    blacked

--- tests/test_global_inverse_optimal_control.py
+++ tests/test_global_inverse_optimal_control.py
@@ -12,10 +12,7 @@ def test_double_pendulum_torque_driven_IOCP():
     bioptim_folder = os.path.dirname(ocp_module.__file__)
     biorbd_model_path = bioptim_folder + "/models/double_pendulum.bioMod"
 
-    ocp = ocp_module.prepare_ocp(weights=[0.4, 0.3, 0.3],
-                                 coefficients=[1, 1, 1],
-                                 biorbd_model_path=biorbd_model_path
-    )
+    ocp = ocp_module.prepare_ocp(weights=[0.4, 0.3, 0.3], coefficients=[1, 1, 1], biorbd_model_path=biorbd_model_path)
 
     sol = ocp.solve()
 
@@ -35,15 +32,13 @@ def test_double_pendulum_torque_driven_IOCP():
     np.testing.assert_almost_equal(f[0, 0], 13.03787939)
 
     # initial and final position
-    np.testing.assert_almost_equal(q[:, 0], np.array([-3.14159265,  0.        ]))
-    np.testing.assert_almost_equal(q[:, -1], np.array([3.14159265, 0.        ]))
+    np.testing.assert_almost_equal(q[:, 0], np.array([-3.14159265, 0.0]))
+    np.testing.assert_almost_equal(q[:, -1], np.array([3.14159265, 0.0]))
 
     # initial and final velocities
     np.testing.assert_almost_equal(qdot[:, 0], np.array([-3.32315017, 15.70796327]))
-    np.testing.assert_almost_equal(qdot[:, -1], np.array([3.0362723 , -2.87576071]))
+    np.testing.assert_almost_equal(qdot[:, -1], np.array([3.0362723, -2.87576071]))
 
     # initial and final controls
     np.testing.assert_almost_equal(tau[:, 0], np.array([-11.49023683]))
     np.testing.assert_almost_equal(tau[:, -2], np.array([0.04617407]))
-
-
