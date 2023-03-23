e4839846f9526bf4c586e4ff4ed9f858a039a42e
Pariterre <pariterre@hotmail.com>
2023-03-23 14:52:09 -0400

    Updated MultiStart backend

--- bioptim/optimization/multi_start.py
+++ bioptim/optimization/multi_start.py
@@ -1,9 +1,10 @@
 from multiprocessing import Pool
 from itertools import product
-from typing import Callable
+from typing import Any, Callable
 
 from ..optimization.optimal_control_program import OptimalControlProgram
 from ..interfaces.solver_options import Solver
+from ..optimization.solution import Solution
 
 
 class MultiStart:
@@ -13,85 +14,72 @@ class MultiStart:
 
     Methods
     -------
-    combine_args_to_list()
-        Combine the varying arguments of the multi-start into a list of arguments to be passed to the solve_ocp function
-        inside the pools
-    run()
+    solve()
         Run the multi-start in the pools for multi-threading
-    check already_done()
-        Check if the OCP has already a saved solution
     """
 
     def __init__(
         self,
-        prepare_ocp: OptimalControlProgram,
-        solver: Solver,
-        post_optimization_callback: Callable = None,
+        combinatorial_parameters: dict[tuple, ...],
+        prepare_ocp_callback: Callable[[Any], OptimalControlProgram],
+        post_optimization_callback: Callable[[Solution, Any], None],
+        should_solve_callback: Callable[[Any], bool] = None,
+        solver: Solver = None,
         n_pools: int = 1,
-        already_done_filenames: list = None,
-        should_solve: Callable = None,
-        use_multi_process: bool = True,
-        **kwargs,
     ):
         """
         Parameters
         ----------
-        prepare_ocp: callable
-            The function to be called to prepare the optimal control problem
+        combinatorial_parameters:
+            Dictionary of tuples defined by the user to identify each of the combination that will be solved.
+        prepare_ocp_callback: Callable
+            The function which is called to prepare the optimal control problem.
+            The inputs are a combination of the combinatorial_parameters for this particular ocp.
+            The output should be the OptimalControlProgram to solve
+        post_optimization_callback: Callable
+            The function which is called after the ocp is solved.
+            The inputs are the solution and the combination of the combinatorial_parameters.
+        should_solve_callback: Callable
+            Returns if the current combination of combinatorial_parameters should [True] or not [False] be solved.
+            This is to give the opportunity to the user to skip specific combination. If the callback is not defined,
+            then all the combination are solved
         solver: Solver
-            The solver to use for the ocp
-        post_optimization_callback:
-            The function to call when the ocp is solved
+            The solver to use for the ocp. Default is IPOPT
         n_pools: int
-            The number of pools to be used for multi-threading
-        should_solve: Function
-            overwrites files if None, else returns True or False
-        use_multi_process: bool
-            True if you want to use the for loop to debug
-        args_dict :
-            Dictionary of the all the arguments
-        combined_args_to_list :
-            List of the combined arguments to pass to the solver
+            The number of pools to be used for multi-threading. If 1 is sent, then the built-in for loop is used
         """
 
-        self.prepare_ocp = prepare_ocp
-        self.solver = solver
+        self.prepare_ocp_callback = prepare_ocp_callback
         self.post_optimization_callback = post_optimization_callback
+        self.should_solve_callback = should_solve_callback
+        self.solver = solver if solver else Solver.IPOPT()
         self.n_pools = n_pools
-        self.already_done_filenames = already_done_filenames
-        self.should_solve = should_solve
-        self.use_multi_process = use_multi_process
-        self.args_dict = kwargs
-        self.combined_args_to_list = self.combine_args_to_list()
+        self.combined_ocp_parameters = self._generate_parameters_combinations(combinatorial_parameters)
 
-    def combine_args_to_list(self):
+    @staticmethod
+    def _generate_parameters_combinations(combinatorial_parameters):
         """
         Combine the varying arguments of the multi-start into a list of arguments to be passed to the solve_ocp function
         inside the pools
         """
-        combined_args = [instance for instance in product(*self.args_dict.values())]
+        combined_args = [instance for instance in product(*combinatorial_parameters.values())]
         combined_args_to_list = []
         for i in range(len(combined_args)):
             combined_args_to_list += [[instance for instance in combined_args[i]]]
         return combined_args_to_list
 
-    def solve_ocp_func(self, args):
-        def return_true(x, y):
-            return True
-
-        if self.should_solve == None:
-            self.should_solve = return_true
-        if self.should_solve(self, args):
-            sol = self.prepare_ocp(*args).solve(self.solver)
-            self.post_optimization_callback(sol, *args)
+    def _prepare_and_solve_ocp(self, ocp_parameters):
+        if self.should_solve_callback is None or self.should_solve_callback(*ocp_parameters):
+            sol = self.prepare_ocp_callback(*ocp_parameters).solve(self.solver)
+            self.post_optimization_callback(sol, *ocp_parameters)
 
     def solve(self):
         """
         Run the multi-start in the pools for multi-threading
         """
-        if self.use_multi_process:
-            with Pool(self.n_pools) as p:
-                p.map(self.solve_ocp_func, self.combined_args_to_list)
+        if self.n_pools == 1:
+            for ocp_parameters in self.combined_ocp_parameters:
+                self._prepare_and_solve_ocp(ocp_parameters)
         else:
-            for args in self.combined_args_to_list:
-                self.solve_ocp_func(args)
+            with Pool(self.n_pools) as p:
+                p.map(self._prepare_and_solve_ocp, self.combined_ocp_parameters)
