3714d1a15831bb9fff30c73b0239535f5ea1a5ce
Charbie <eve.charbonneau.1@umontreal.ca>
2023-05-09 11:26:54 -0400

    added tests for new bounds error mesage

--- tests/test_bounds_accessor.py
+++ tests/test_bounds_accessor.py
@@ -1,8 +1,19 @@
+import os
 import numpy as np
+import pytest
 from bioptim import (
     Bounds,
+    BoundsList,
     InterpolationType,
+    BiorbdModel,
+    Objective,
+    ObjectiveFcn,
+    Dynamics,
+    DynamicsFcn,
+    InitialGuess,
+    OptimalControlProgram,
 )
+
 
 
 def test_accessors_on_bounds_option():
@@ -43,3 +54,64 @@ def test_accessors_on_bounds_option_multidimensional():
         x_bounds.max[:],
         np.array([[0, 150, 200], [0, 10, 10], [0, 10, 10], [100, 10, 10], [100, 10, 10], [100, 150, 200]]),
     )
+
+def test_bounds_error_messages():
+    """
+    This tests that the error messages are properly raised. The OCP is adapted from the getting_started/pendulum.py example.
+    """
+    from bioptim.examples.getting_started import pendulum as ocp_module
+
+    bioptim_folder = os.path.dirname(ocp_module.__file__)
+    biorbd_model_path = bioptim_folder + "/models/pendulum.bioMod"
+    bio_model = BiorbdModel(biorbd_model_path)
+    n_q = bio_model.nb_q
+
+    # Add objective functions
+    objective_functions = Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau")
+
+    # Dynamics
+    dynamics = Dynamics(DynamicsFcn.TORQUE_DRIVEN)
+
+    # Define states path constraint
+    x_bounds = Bounds([-100] * n_q * 2, [100] * n_q * 2)
+    x_bounds[1, :] = 0  # Prevent the model from actively rotate
+
+    # Define control path constraint
+    u_bounds = Bounds([-100] * n_q, [100] * n_q)
+    u_bounds[1, :] = 0  # Prevent the model from actively rotate
+
+    x_init = InitialGuess([0] * n_q * 2)
+    u_init = InitialGuess([0] * n_q)
+
+    # check the error messages
+    with pytest.raises(
+        RuntimeError,
+        match="If you do not want to provide an x_bounds, you should declare x_bounds=None instead of an empty BoundsList",
+    ):
+        OptimalControlProgram(
+            bio_model,
+            dynamics,
+            n_shooting=5,
+            phase_time=1,
+            x_init=x_init,
+            u_init=u_init,
+            x_bounds=BoundsList(),
+            u_bounds=u_bounds,
+            objective_functions=objective_functions,
+        )
+
+    with pytest.raises(
+            RuntimeError,
+            match="If you do not want to provide a u_bounds, you should declare u_bounds=None instead of an empty BoundsList",
+    ):
+        OptimalControlProgram(
+            bio_model,
+            dynamics,
+            n_shooting=5,
+            phase_time=1,
+            x_init=x_init,
+            u_init=u_init,
+            x_bounds=x_bounds,
+            u_bounds=BoundsList(),
+            objective_functions=objective_functions,
+        )
