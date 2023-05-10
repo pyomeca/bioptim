7a70058eb4bb0bd00e1ad706dab78f9d75393e49
Pariterre <pariterre@hotmail.com>
2023-05-10 10:32:51 -0400

    Confirmed saved and load for assume_phase_dynamics True and False

--- tests/test_global_getting_started.py
+++ tests/test_global_getting_started.py
@@ -169,17 +169,21 @@ def test_pendulum(ode_solver, use_sx, n_threads, assume_phase_dynamics):
     TestUtils.simulate(sol)
 
 
+@pytest.mark.parametrize("assume_phase_dynamics", [True, False])
 @pytest.mark.parametrize("n_threads", [1, 2])
 @pytest.mark.parametrize("use_sx", [False, True])
 @pytest.mark.parametrize("ode_solver", [OdeSolver.RK4, OdeSolver.IRK, OdeSolver.COLLOCATION])
-def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver):
+def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver, assume_phase_dynamics):
     from bioptim.examples.getting_started import example_save_and_load as ocp_module
+
+    # For reducing time assume_phase_dynamics=False is skipped for redundant tests
+    if n_threads > 1 and not assume_phase_dynamics:
+        return
 
     bioptim_folder = os.path.dirname(ocp_module.__file__)
 
-    ode_solver = ode_solver()
-
-    if isinstance(ode_solver, OdeSolver.IRK):
+    if ode_solver == OdeSolver.IRK:
+        ode_solver = ode_solver()
         if use_sx:
             with pytest.raises(RuntimeError, match="use_sx=True and OdeSolver.IRK are not yet compatible"):
                 ocp_module.prepare_ocp(
@@ -189,6 +193,7 @@ def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver):
                     n_threads=n_threads,
                     use_sx=use_sx,
                     ode_solver=ode_solver,
+                    assume_phase_dynamics=assume_phase_dynamics,
                 )
         else:
             ocp = ocp_module.prepare_ocp(
@@ -198,6 +203,7 @@ def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver):
                 n_threads=n_threads,
                 use_sx=use_sx,
                 ode_solver=ode_solver,
+                assume_phase_dynamics=assume_phase_dynamics,
             )
             sol = ocp.solve()
 
@@ -227,6 +233,7 @@ def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver):
             # simulate
             TestUtils.simulate(sol)
     else:
+        ode_solver = ode_solver()
         ocp = ocp_module.prepare_ocp(
             biorbd_model_path=bioptim_folder + "/models/pendulum.bioMod",
             final_time=1,
@@ -234,6 +241,7 @@ def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver):
             n_threads=n_threads,
             use_sx=use_sx,
             ode_solver=ode_solver,
+            assume_phase_dynamics=assume_phase_dynamics,
         )
         sol = ocp.solve()
 
@@ -285,10 +293,8 @@ def test_pendulum_save_and_load_no_rk8(n_threads, use_sx, ode_solver):
         TestUtils.simulate(sol)
 
 
-@pytest.mark.parametrize("n_threads", [1])
 @pytest.mark.parametrize("use_sx", [False, True])
-@pytest.mark.parametrize("ode_solver", [OdeSolver.RK8])
-def test_pendulum_save_and_load_rk8(n_threads, use_sx, ode_solver):
+def test_pendulum_save_and_load_rk8(use_sx):
     if sys.platform == "win32":  # it works but not with the CI
         return
 
@@ -296,112 +302,46 @@ def test_pendulum_save_and_load_rk8(n_threads, use_sx, ode_solver):
 
     bioptim_folder = os.path.dirname(ocp_module.__file__)
 
-    ode_solver = ode_solver()
+    ocp = ocp_module.prepare_ocp(
+        biorbd_model_path=bioptim_folder + "/models/pendulum.bioMod",
+        final_time=1,
+        n_shooting=30,
+        n_threads=1,
+        use_sx=use_sx,
+        ode_solver=OdeSolver.RK8(),
+    )
+    sol = ocp.solve()
 
-    if isinstance(ode_solver, OdeSolver.IRK):
-        if use_sx:
-            with pytest.raises(RuntimeError, match="use_sx=True and OdeSolver.IRK are not yet compatible"):
-                ocp_module.prepare_ocp(
-                    biorbd_model_path=bioptim_folder + "/models/pendulum.bioMod",
-                    final_time=1,
-                    n_shooting=30,
-                    n_threads=n_threads,
-                    use_sx=use_sx,
-                    ode_solver=ode_solver,
-                )
-        else:
-            ocp = ocp_module.prepare_ocp(
-                biorbd_model_path=bioptim_folder + "/models/pendulum.bioMod",
-                final_time=1,
-                n_shooting=30,
-                n_threads=n_threads,
-                use_sx=use_sx,
-                ode_solver=ode_solver,
-            )
-            sol = ocp.solve()
+    # Check objective function value
+    f = np.array(sol.cost)
+    np.testing.assert_equal(f.shape, (1, 1))
+    np.testing.assert_almost_equal(f[0, 0], 9.821989132327003)
 
-            # Check objective function value
-            f = np.array(sol.cost)
-            np.testing.assert_equal(f.shape, (1, 1))
+    # Check constraints
+    g = np.array(sol.constraints)
+    np.testing.assert_equal(g.shape, (120, 1))
+    np.testing.assert_almost_equal(g, np.zeros((120, 1)))
 
-            # Check constraints
-            g = np.array(sol.constraints)
-            np.testing.assert_equal(g.shape, (120, 1))
-            np.testing.assert_almost_equal(g, np.zeros((120, 1)))
+    # Check some of the results
+    q, qdot, tau = (sol.states["q"], sol.states["qdot"], sol.controls["tau"])
 
-            # Check some of the results
-            q, qdot, tau = (sol.states["q"], sol.states["qdot"], sol.controls["tau"])
+    # initial and final position
+    np.testing.assert_almost_equal(q[:, 0], np.array((0, 0)))
+    np.testing.assert_almost_equal(q[:, -1], np.array((0, 3.14)))
 
-            # initial and final position
-            np.testing.assert_almost_equal(q[:, 0], np.array((0, 0)))
-            np.testing.assert_almost_equal(q[:, -1], np.array((0, 3.14)))
+    # initial and final velocities
+    np.testing.assert_almost_equal(qdot[:, 0], np.array((0, 0)))
+    np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
 
-            # initial and final velocities
-            np.testing.assert_almost_equal(qdot[:, 0], np.array((0, 0)))
-            np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
+    # initial and final controls
+    np.testing.assert_almost_equal(tau[:, 0], np.array((5.67291529, 0)))
+    np.testing.assert_almost_equal(tau[:, -2], np.array((-11.71262836, 0)))
 
-            # save and load
-            TestUtils.save_and_load(sol, ocp, True)
+    # save and load
+    TestUtils.save_and_load(sol, ocp, True)
 
-            # simulate
-            TestUtils.simulate(sol)
-    else:
-        ocp = ocp_module.prepare_ocp(
-            biorbd_model_path=bioptim_folder + "/models/pendulum.bioMod",
-            final_time=1,
-            n_shooting=30,
-            n_threads=n_threads,
-            use_sx=use_sx,
-            ode_solver=ode_solver,
-        )
-        sol = ocp.solve()
-
-        # Check objective function value
-        is_collocation = isinstance(ode_solver, OdeSolver.COLLOCATION) and not isinstance(ode_solver, OdeSolver.IRK)
-        f = np.array(sol.cost)
-        np.testing.assert_equal(f.shape, (1, 1))
-        if isinstance(ode_solver, OdeSolver.RK8):
-            np.testing.assert_almost_equal(f[0, 0], 9.821989132327003)
-        elif is_collocation:
-            pass
-        else:
-            np.testing.assert_almost_equal(f[0, 0], 9.834017207589055)
-
-        # Check constraints
-        g = np.array(sol.constraints)
-        if is_collocation:
-            np.testing.assert_equal(g.shape, (600, 1))
-            np.testing.assert_almost_equal(g, np.zeros((600, 1)))
-        else:
-            np.testing.assert_equal(g.shape, (120, 1))
-            np.testing.assert_almost_equal(g, np.zeros((120, 1)))
-
-        # Check some of the results
-        q, qdot, tau = (sol.states["q"], sol.states["qdot"], sol.controls["tau"])
-
-        # initial and final position
-        np.testing.assert_almost_equal(q[:, 0], np.array((0, 0)))
-        np.testing.assert_almost_equal(q[:, -1], np.array((0, 3.14)))
-
-        # initial and final velocities
-        np.testing.assert_almost_equal(qdot[:, 0], np.array((0, 0)))
-        np.testing.assert_almost_equal(qdot[:, -1], np.array((0, 0)))
-
-        # initial and final controls
-        if isinstance(ode_solver, OdeSolver.RK8):
-            np.testing.assert_almost_equal(tau[:, 0], np.array((5.67291529, 0)))
-            np.testing.assert_almost_equal(tau[:, -2], np.array((-11.71262836, 0)))
-        elif is_collocation:
-            pass
-        else:
-            np.testing.assert_almost_equal(tau[:, 0], np.array((5.72227268, 0)))
-            np.testing.assert_almost_equal(tau[:, -2], np.array((-11.62799294, 0)))
-
-        # save and load
-        TestUtils.save_and_load(sol, ocp, True)
-
-        # simulate
-        TestUtils.simulate(sol)
+    # simulate
+    TestUtils.simulate(sol)
 
 
 @pytest.mark.parametrize("ode_solver", [OdeSolver.RK4, OdeSolver.RK8, OdeSolver.IRK])
