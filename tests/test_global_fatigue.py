819143f2ed1b76c28fd45e27cdc73d883cd17b44
Pariterre <pariterre@hotmail.com>
2023-05-19 11:44:09 -0400

    Again

--- tests/test_global_fatigue.py
+++ tests/test_global_fatigue.py
@@ -447,6 +447,10 @@ def test_fatigable_xia_stabilized_torque_split(assume_phase_dynamics):
 def test_fatigable_michaud_torque_non_split(assume_phase_dynamics):
     from bioptim.examples.fatigue import pendulum_with_fatigue as ocp_module
 
+    if platform.system() == "Windows":
+        # This is a long test and CI is already long for Windows
+        return
+
     bioptim_folder = os.path.dirname(ocp_module.__file__)
 
     model_path = f"{bioptim_folder}/models/pendulum.bioMod"
