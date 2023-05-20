bc126bc8d0a9be905c2f8af6b503e724ae9754ba
Charbie <eve.charbonneau.1@umontreal.ca>
2023-05-19 20:36:31 -0400

    fixed the sol test (except error in comment)
    RuntimeError: Error in Function::call for 'PHASE_TRANSITION_CONTINUOUS_0_1' [MXFunction] at .../casadi/core/function.cpp:330:
           .../casadi/core/function_internal.hpp:1644: Input 1 (i1) has mismatching shape. Got 6-by-1. Allowed dimensions, in general, are:
            - The input dimension N-by-M (here 3-by-1)
            - A scalar, i.e. 1-by-1
            - M-by-N if N=1 or M=1 (i.e. a transposed vector)
            - N-by-M1 if K*M1=M for some K (argument repeated horizontally)
            - N-by-P*M, indicating evaluation with multiple arguments (P must be a multiple of 1 for consistency with previous inputs)

--- bioptim/optimization/optimal_control_program.py
+++ bioptim/optimization/optimal_control_program.py
@@ -25,7 +25,6 @@ from ..limits.constraints import (
     ConstraintFcn,
     ConstraintList,
     Constraint,
-    MultinodeConstraintFunction,
     ParameterConstraintList,
     ParameterConstraint,
 )
