952b7d059022dae618c9040a79458d670edeb5d2
Charbie <eve.charbonneau.1@umontreal.ca>
2023-05-24 15:22:34 +0200

    beginning of muscle driven -> transition to Leuven arm model

---
+++ bioptim/examples/stochastic_optimal_control/arm_reaching_muscle_driven.py
@@ -0,0 +1,141 @@
+"""
+This example replicates the results from "An approximate stochastic optimal control framework to simulate nonlinear
+neuro-musculoskeletal models in the presence of noise"(https://doi.org/10.1371/journal.pcbi.1009338).
+
+The task is to unfold the arm to reach a target further from the trunk.
+Noise is added on the motor execution (wM) and on the feedback (wEE and wEE_dot).
+The expected joint angles (x_mean) are optimized like in  deterministic OCP, but the covariance matrix is minimized to
+reduce uncertainty. This covariance matrix is computed from the expected states.
+"""
+import platform
+
+from bioptim import (
+    OptimalControlProgram,
+    DynamicsFcn,
+    Dynamics,
+    Bounds,
+    InitialGuess,
+    ObjectiveFcn,
+    Objective,
+    OdeSolver,
+    OdeSolverBase,
+    CostType,
+    Solver,
+    BiorbdModel,
+    ObjectiveList,
+)
+
+
+def prepare_ocp(
+    biorbd_model_path: str,
+    final_time: float,
+    n_shooting: int,
+    ode_solver: OdeSolverBase = OdeSolver.RK4(),
+    n_threads: int = 1,
+) -> OptimalControlProgram:
+    """
+    The initialization of an ocp
+
+    Parameters
+    ----------
+    biorbd_model_path: str
+        The path to the biorbd model
+    final_time: float
+        The time in second required to perform the task
+    n_shooting: int
+        The number of shooting points to define int the direct multiple shooting program
+    ode_solver: OdeSolverBase = OdeSolver.RK4()
+        Which type of OdeSolver to use
+    n_threads: int
+        The number of threads to use in the paralleling (1 = no parallel computing)
+
+    Returns
+    -------
+    The OptimalControlProgram ready to be solved
+    """
+
+    bio_model = BiorbdModel(biorbd_model_path)
+
+    # Add objective functions
+    objective_functions = ObjectiveList()
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau")
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_STOCHASTIC_VARIABLE, key="cov")
+
+    # Dynamics
+    dynamics = Dynamics(DynamicsFcn.TORQUE_DRIVEN, is_stochastic=True)
+
+    # Path constraint
+    x_bounds = bio_model.bounds_from_ranges(["q", "qdot"])
+    x_bounds[:, [0, -1]] = 0
+    x_bounds[1, -1] = 3.14
+
+    # Initial guess
+    n_q = bio_model.nb_q
+    n_qdot = bio_model.nb_qdot
+    x_init = InitialGuess([0] * (n_q + n_qdot))
+
+    # Define control path constraint
+    n_tau = bio_model.nb_tau
+    tau_min, tau_max, tau_init = -100, 100, 0
+    u_bounds = Bounds([tau_min] * n_tau, [tau_max] * n_tau)
+    u_bounds[1, :] = 0  # Prevent the model from actively rotate
+
+    u_init = InitialGuess([tau_init] * n_tau)
+
+    # This should probably be done automatically
+    n_stochastic = n_q + n_q**2 + n_q**2 + n_q + n_q + n_q
+    s_bounds = Bounds([tau_min] * n_stochastic, [tau_max] * n_stochastic)
+    s_init = InitialGuess([0] * n_stochastic)
+
+    # TODO: we should probably change the name stochastic_variables -> helper_variables ?
+    # TODO: add end effector position and velocity correction
+
+    return OptimalControlProgram(
+        bio_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_init=x_init,
+        u_init=u_init,
+        s_init=s_init,
+        x_bounds=x_bounds,
+        u_bounds=u_bounds,
+        s_bounds=s_bounds,
+        objective_functions=objective_functions,
+        ode_solver=ode_solver,
+        n_threads=n_threads,
+        assume_phase_dynamics=False,  # TODO: see if it can be done with assume_phase_dynamics=True
+    )
+
+
+def main():
+
+    import bioviz
+    b = bioviz.Viz("models/LeuvenArmModel.bioMod")
+    b.exec()
+
+    # --- Prepare the ocp --- #
+    # TODO change the model to their model
+    dt = 0.1  # 0.01
+    final_time = 0.8
+    n_shooting = int(final_time/dt)
+
+    # TODO: devrait-il y avoir ns ou ns+1 P ?
+    ocp = prepare_ocp(biorbd_model_path="models/arm26.bioMod", final_time=final_time, n_shooting=n_shooting)
+
+    # Custom plots
+    # ocp.add_plot_penalty(CostType.ALL)
+
+    # --- If one is interested in checking the conditioning of the problem, they can uncomment the following line --- #
+    # ocp.check_conditioning()
+
+    # --- Solve the ocp --- #
+    sol = ocp.solve(Solver.IPOPT(show_online_optim=False))  # platform.system() == "Linux"
+    # sol.graphs()
+
+    # --- Show the results in a bioviz animation --- #
+    sol.animate()
+
+
+if __name__ == "__main__":
+    main()
