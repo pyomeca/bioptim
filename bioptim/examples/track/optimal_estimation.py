e26ca85edfdf9e70433eb720f14dd434aa9c172d
Charbie <eve.charbonneau.1@umontreal.ca>
2023-06-26 13:33:30 +0200

    set 3D problem, not cute solution yet

--- bioptim/examples/track/optimal_estimation.py
+++ bioptim/examples/track/optimal_estimation.py
@@ -28,14 +28,8 @@ from bioptim import (
     OdeSolverBase,
     Node,
     Solver,
+    BiMappingList,
 )
-
-# Load track_segment_on_rt
-EXAMPLES_FOLDER = Path(__file__).parent / ".."
-spec = importlib.util.spec_from_file_location("data_to_track", str(EXAMPLES_FOLDER) + "/getting_started/pendulum.py")
-data_to_track = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(data_to_track)
-
 
 def get_markers_pos(x: DM | np.ndarray, idx_marker: int, fun: Callable, n_q: int) -> DM | np.ndarray:
     """
@@ -62,8 +56,93 @@ def get_markers_pos(x: DM | np.ndarray, idx_marker: int, fun: Callable, n_q: int
     return marker_pos
 
 
-def prepare_ocp(
-    bio_model: BiorbdModel,
+def prepare_ocp_to_track(
+    biorbd_model_path: str,
+    final_time: tuple[float],
+    n_shooting: tuple[int],
+    ode_solver: OdeSolverBase = OdeSolver.RK4(),
+    assume_phase_dynamics: bool = True,
+) -> OptimalControlProgram:
+    """
+    Prepare the ocp
+
+    Parameters
+    ----------
+    bio_model: BiorbdModel
+        The loaded biorbd model
+    final_time: float
+        The time at final node
+    n_shooting: int
+        The number of shooting points
+    markers_ref: np.ndarray
+        The markers to track
+    ode_solver: OdeSolverBase
+        The ode solver to use
+    assume_phase_dynamics: bool
+        If the dynamics equation within a phase is unique or changes at each node. True is much faster, but lacks the
+        capability to have changing dynamics within a phase. A good example of when False should be used is when
+        different external forces are applied at each node
+
+    Returns
+    -------
+    The OptimalControlProgram ready to be solved
+    """
+
+    bio_model = (BiorbdModel(biorbd_model_path), BiorbdModel(biorbd_model_path))
+
+    # Rotations of the pendulum are passive
+    variable_mappings = BiMappingList()
+    variable_mappings.add("tau", to_second=[0, 1, None, None], to_first=[0, 1], phase=0)
+    variable_mappings.add("tau", to_second=[0, 1, None, None], to_first=[0, 1], phase=1)
+
+    # Add objective functions
+    objective_functions = ObjectiveList()
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_ANGULAR_MOMENTUM, node=Node.END, weight=-1000, quadratic=False, phase=0)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", node=Node.ALL_SHOOTING, weight=1e-6, quadratic=True, phase=0)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", node=Node.ALL_SHOOTING, weight=1e-6, quadratic=True, phase=1)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", derivative=True, node=Node.ALL_SHOOTING, weight=1, quadratic=True, phase=0)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", derivative=True, node=Node.ALL_SHOOTING, weight=1, quadratic=True, phase=1)
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_MARKERS_VELOCITY, index=1, node=Node.END, weight=1000, quadratic=True, phase=1)
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_COM_POSITION, axes=Axis.Z, node=Node.END, weight=-1000, quadratic=False, phase=1)
+
+    # Dynamics
+    dynamics = DynamicsList()
+    expand = False if isinstance(ode_solver, OdeSolver.IRK) else True
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, expand=expand)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, expand=expand)
+
+    # Path constraint
+    x_bounds = BoundsList()
+    x_bounds.add("q", bounds=bio_model[0].bounds_from_ranges("q"), phase=0)
+    x_bounds.add("qdot", bounds=bio_model[0].bounds_from_ranges("qdot"), phase=0)
+    x_bounds.add("q", bounds=bio_model[1].bounds_from_ranges("q"), phase=1)
+    x_bounds.add("qdot", bounds=bio_model[1].bounds_from_ranges("qdot"), phase=1)
+    x_bounds[0]["q"][:, 0] = 0
+    x_bounds[0]["qdot"][:, 0] = 0
+
+    # Define control path constraint
+    tau_min, tau_max = -50, 50
+    u_bounds = BoundsList()
+    u_bounds.add("tau", min_bound=[tau_min] * 2, max_bound=[tau_max] * 2, phase=0)
+    u_bounds.add("tau", min_bound=[tau_min] * 2, max_bound=[tau_max] * 2, phase=1)
+
+    # ------------- #
+
+    return OptimalControlProgram(
+        bio_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_bounds=x_bounds,
+        u_bounds=u_bounds,
+        objective_functions=objective_functions,
+        variable_mappings=variable_mappings,
+        ode_solver=ode_solver,
+        assume_phase_dynamics=assume_phase_dynamics,
+    )
+
+def prepare_optimal_estimation(
+    biorbd_model_path: str,
     final_time: float,
     n_shooting: int,
     markers_ref: np.ndarray,
@@ -95,16 +174,25 @@ def prepare_ocp(
     The OptimalControlProgram ready to be solved
     """
 
+    bio_model = BiorbdModel(biorbd_model_path, biorbd_model_path)
+
     # Add objective functions
     objective_functions = ObjectiveList()
     objective_functions.add(
         ObjectiveFcn.Lagrange.TRACK_MARKERS,
-        axes=[Axis.Y, Axis.Z],
         node=Node.ALL,
         weight=100,
-        target=markers_ref[1:, :, :],
+        target=markers_ref,
+        phase=0
     )
-    objective_functions.add(ObjectiveFcn.Lagrange.TRACK_CONTROL, key="tau", weight=1e-6, quadratic=True)
+    objective_functions.add(
+        ObjectiveFcn.Lagrange.TRACK_MARKERS,
+        node=Node.ALL,
+        weight=100,
+        target=markers_ref,
+        phase=1
+    )
+    # objective_functions.add(ObjectiveFcn.Lagrange.TRACK_CONTROL, key="tau", weight=1e-6, quadratic=True)
 
     # Dynamics
     dynamics = DynamicsList()
@@ -141,17 +229,18 @@ def prepare_ocp(
 
 def main():
     """
-    Firstly, it solves the getting_started/pendulum.py example. Afterward, it gets the marker positions and joint
-    torque from the solution and uses them to track. It then creates and solves this ocp and show the results
+    1) An OCP which tries to rotate a 3D pendulum as fast as possible around the Z-axis, and then
+    stabilizes it in an inverted position is solved to generate data.
+    2) Noise is added to the marker positions to simulate experimental data.
+    3) The marker positions are tracked to find the torques that best match the experimental kinematics.
     """
 
-    biorbd_path = str(EXAMPLES_FOLDER) + "/getting_started/models/pendulum.bioMod"
-    bio_model = BiorbdModel(biorbd_path)
-    final_time = 1
-    n_shooting = 20
+    biorbd_model_path = "models/pendulum3D.bioMod"
+    final_time = (5, 5)
+    n_shooting = (30, 30)
 
-    ocp_to_track = data_to_track.prepare_ocp(
-        biorbd_model_path=biorbd_path, final_time=final_time, n_shooting=n_shooting
+    ocp_to_track = prepare_ocp_to_track(
+        biorbd_model_path=biorbd_model_path, final_time=final_time, n_shooting=n_shooting
     )
     sol = ocp_to_track.solve()
     q, qdot, tau = sol.states["q"], sol.states["qdot"], sol.controls["tau"]
