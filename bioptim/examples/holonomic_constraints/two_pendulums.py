7616d2c66ee1f1c08b3b4f3631f6d05e11caa18b
Amandine <amandine.chupin@etu.minesparis.psl.eu>
2023-06-29 16:41:20 -0400

    Tutorial and correction of one test

--- bioptim/examples/holonomic_constraints/two_pendulums.py
+++ bioptim/examples/holonomic_constraints/two_pendulums.py
@@ -45,6 +45,7 @@ def prepare_ocp(
     The ocp ready to be solved
     """
     bio_model = HolonomicBiorbdModel(biorbd_model_path)
+    # Create a holonomic constraint to create a double pendulum from two single pendulums
     constraint, constraint_jacobian, constraint_double_derivative = HolonomicConstraintFcn.superimpose_markers(
         bio_model, "marker_1", "marker_3", index=slice(1, 3), local_frame_index=0
     )
@@ -53,6 +54,8 @@ def prepare_ocp(
         constraint_jacobian=constraint_jacobian,
         constraint_double_derivative=constraint_double_derivative,
     )
+    # The rotations (joint 0 and 3) are independent. The translations (joint 1 and 2) are constrained by the holonomic
+    # constraint
     bio_model.set_dependencies(independent_joint_index=[0, 3], dependent_joint_index=[1, 2])
 
     # Add objective functions
@@ -69,9 +72,13 @@ def prepare_ocp(
 
     # Boundaries
     mapping = BiMappingList()
+    # The rotations (joint 0 and 3) are independent. The translations (joint 1 and 2) are constrained by the holonomic
+    # constraint, so we need to map the states and controls to only compute the dynamics of the independent joints
+    # The dynamics of the dependent joints will be computed from the holonomic constraint
     mapping.add("q", to_second=[0, None, None, 1], to_first=[0, 3])
     mapping.add("qdot", to_second=[0, None, None, 1], to_first=[0, 3])
     x_bounds = BoundsList()
+    # q_u and qdot_u are the states of the independent joints
     x_bounds["q_u"] = bio_model.bounds_from_ranges("q", mapping=mapping)
     x_bounds["qdot_u"] = bio_model.bounds_from_ranges("qdot", mapping=mapping)
 
@@ -87,6 +94,7 @@ def prepare_ocp(
     # Define control path constraint
     variable_bimapping = BiMappingList()
     tau_min, tau_max, tau_init = -100, 100, 0
+    # Only the rotations are controlled
     variable_bimapping.add("tau", to_second=[0, None, None, 1], to_first=[0, 3])
     u_bounds = BoundsList()
     u_bounds.add("tau", min_bound=[tau_min] * 2, max_bound=[tau_max] * 2)
@@ -124,7 +132,7 @@ def main():
     ocp, bio_model = prepare_ocp(biorbd_model_path=model_path, n_shooting=n_shooting)
 
     # --- Solve the program --- #
-    sol = ocp.solve(Solver.IPOPT(show_online_optim=False, _max_iter=500))  # show_online_optim not working yet
+    sol = ocp.solve(Solver.IPOPT())
 
     # --- Show results --- #
     q = np.zeros((4, n_shooting + 1))
