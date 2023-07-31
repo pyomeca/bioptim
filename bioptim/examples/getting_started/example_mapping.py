0b61220b51a6235964fca8da5780bdfaf0372f4d
mickaelbegon <mickaelbegon@gmail.com>
2023-07-31 14:16:11 +0400

    Add comment

--- bioptim/examples/getting_started/example_mapping.py
+++ bioptim/examples/getting_started/example_mapping.py
@@ -1,3 +1,4 @@
 """
-An example of mapping can be found at 'examples/symmetrical_torque_driven_ocp/symmetry_by_mapping.py'
+Example of mapping can be found at 'examples/symmetrical_torque_driven_ocp/symmetry_by_mapping.py'
+and 'examples/getting_started/example_inequality_constraint.py'.
 """
