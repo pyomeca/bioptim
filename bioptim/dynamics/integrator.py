5d9f1c241b685b5bf9384d9d27fb39eb8098a60e
Kevin CO <kevinco@umontreal.ca>
2023-08-03 12:04:07 -0400

    back to correct time in rk

--- bioptim/dynamics/integrator.py
+++ bioptim/dynamics/integrator.py
@@ -289,8 +289,8 @@ class RK(Integrator):
         s = stochastic_variables
 
         for i in range(1, self.n_step + 1):
-            # t_norm_init = t
-            x[:, i] = self.next_x(h, t, x[:, i - 1], u, p, s)
+            t_norm_init = (self.t_span[1]-self.t_span[0])*i/self.n_step
+            x[:, i] = self.next_x(h, t_norm_init, x[:, i - 1], u, p, s)
             if self.model.nb_quaternions > 0:
                 x[:, i] = self.model.normalize_state_quaternions(x[:, i])
 
