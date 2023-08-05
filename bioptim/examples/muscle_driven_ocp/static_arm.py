ee69ee520b24af8ecc61ef94b38bd348782bc0ab
Charbie <eve.charbonneau.1@umontreal.ca>
2023-08-05 22:36:11 +0200

    blacked

--- bioptim/examples/muscle_driven_ocp/static_arm.py
+++ bioptim/examples/muscle_driven_ocp/static_arm.py
@@ -20,7 +20,7 @@ from bioptim import (
     OdeSolver,
     OdeSolverBase,
     Solver,
-    RigidBodyDynamics
+    RigidBodyDynamics,
 )
 
 
@@ -32,7 +32,7 @@ def prepare_ocp(
     ode_solver: OdeSolverBase = OdeSolver.IRK(),
     assume_phase_dynamics: bool = True,
     expand_dynamics: bool = True,
-    rigidbody_dynamics = RigidBodyDynamics.ODE,
+    rigidbody_dynamics=RigidBodyDynamics.ODE,
 ) -> OptimalControlProgram:
     """
     Prepare the ocp
@@ -76,8 +76,12 @@ def prepare_ocp(
 
     # Dynamics
     dynamics = DynamicsList()
-    dynamics.add(DynamicsFcn.MUSCLE_DRIVEN, with_residual_torque=True, expand=expand_dynamics,
-                 rigidbody_dynamics=rigidbody_dynamics)
+    dynamics.add(
+        DynamicsFcn.MUSCLE_DRIVEN,
+        with_residual_torque=True,
+        expand=expand_dynamics,
+        rigidbody_dynamics=rigidbody_dynamics,
+    )
 
     # Path constraint
     x_bounds = BoundsList()
