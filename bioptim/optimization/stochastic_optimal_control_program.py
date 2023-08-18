7ca713b1acce1d4188fd051ace2632e5a2218b8c
Charbie <eve.charbonneau.1@umontreal.ca>
2023-08-18 11:31:58 +0200

    ns - 1 to ns (was already fixed, bad merge at some point)

--- bioptim/optimization/stochastic_optimal_control_program.py
+++ bioptim/optimization/stochastic_optimal_control_program.py
@@ -284,7 +284,7 @@ class StochasticOptimalControlProgram(OptimalControlProgram):
         multi_node_penalties = MultinodeConstraintList()
         # Constraints for M
         for i_phase, nlp in enumerate(self.nlp):
-            for i_node in range(nlp.ns - 1):  # TODO: Charbie -> check if this is correct
+            for i_node in range(nlp.ns):
                 multi_node_penalties.add(
                     MultinodeConstraintFcn.STOCHASTIC_HELPER_MATRIX_IMPLICIT,
                     nodes_phase=(i_phase, i_phase),
@@ -320,7 +320,7 @@ class StochasticOptimalControlProgram(OptimalControlProgram):
 
         # Constraints for C
         for i_phase, nlp in enumerate(self.nlp):
-            for i_node in range(nlp.ns - 1):  # TODO: Charbie -> check if this is correct
+            for i_node in range(nlp.ns):
                 multi_node_penalties.add(
                     MultinodeConstraintFcn.STOCHASTIC_DG_DW_IMPLICIT,
                     nodes_phase=(i_phase, i_phase),
