f32b61b60b760caafd1b9399d12c96cb12f6b941
Ipuch <pierre.puchaud@umontreal.ca>
2023-10-17 17:30:17 -0400

    remove comment

--- bioptim/optimization/optimal_control_program.py
+++ bioptim/optimization/optimal_control_program.py
@@ -161,11 +161,6 @@ class OptimalControlProgram:
         parameter_init: InitialGuessList = None,
         parameter_objectives: ParameterObjectiveList = None,
         parameter_constraints: ParameterConstraintList = None,
-        # external_forces: tuple[tuple[Any], ...] | list[list[Any]] = None,
-        #         external_forces: tuple[tuple[Any]]
-        #             The external_forces should be of format tuple[tuple[Any]] where the outer list is the number of "
-        #             "phases, the inner list is the number of shooting points of each phase and the dict is the Any is "
-        #             "the specific way to add external_force for the specific implementation of the biomodel
         ode_solver: list | OdeSolverBase | OdeSolver = None,
         control_type: ControlType | list = ControlType.CONSTANT,
         variable_mappings: BiMappingList = None,
@@ -646,18 +641,6 @@ class OptimalControlProgram:
         NLP.add(self, "n_threads", n_threads, True)
         self.ocp_solver = None
         self.is_warm_starting = False
-
-        # # External forces
-        # if external_forces is not None:
-
-        #     for f, nlp in zip(external_forces, self.nlp):
-        #         if f is not None and len(f) != nlp.ns:
-        #             raise RuntimeError(
-        #                 "The external_forces should be of format list[list[Any]] where the outer list is the number of "
-        #                 "phases, the inner list is the number of shooting points of each phase and the dict is the Any "
-        #                 "is the specific way to add external_force for the specific implementation of the biomodel"
-        #             )
-        #     NLP.add(self, "external_forces", external_forces, False)
 
         plot_mappings = plot_mappings if plot_mappings is not None else {}
         reshaped_plot_mappings = []
