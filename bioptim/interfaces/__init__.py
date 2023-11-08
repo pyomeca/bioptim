94cb873e402fbc201ffd2838a8b18f5de1fdad49
Ipuch <pierre.puchaud@umontreal.ca>
2023-11-08 14:23:10 -0500

    push init

---
+++ bioptim/interfaces/__init__.py
@@ -0,0 +1,9 @@
+from .ipopt_options import IPOPT
+from .acados_options import ACADOS
+from .sqp_options import SQP_METHOD
+
+
+class Solver:
+    IPOPT = IPOPT
+    SQP_METHOD = SQP_METHOD
+    ACADOS = ACADOS
--- bioptim/interfaces/solver_options.py
+++
@@ -1,9 +0,0 @@
-from .ipopt_options import IPOPT
-from .acados_options import ACADOS
-from .sqp_options import SQP_METHOD
-
-
-class Solver:
-    IPOPT = IPOPT
-    SQP_METHOD = SQP_METHOD
-    ACADOS = ACADOS
