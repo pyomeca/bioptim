Fixed penalty derivative

bioptim/interfaces/acados_interface.py
@@ -488,8 +488,8 @@ class AcadosInterface(SolverInterface):
                 acados.W_0 = linalg.block_diag(
                     acados.W_0, np.diag([objectives.weight] * objectives.function[0].numel_out())
                 )
-                x = x if objectives.function[0].sparsity_in("x").shape != (0, 0) else []
-                u = u if objectives.function[0].sparsity_in("u").shape != (0, 0) else []
+                x = x if objectives.function[0].size_in("x") != (0, 0) else []
+                u = u if objectives.function[0].size_in("u") != (0, 0) else []
                 acados.mayer_costs = vertcat(acados.mayer_costs, objectives.function[0](t, x, u, p, s).reshape((-1, 1)))
 
                 if objectives.target is not None:
@@ -501,8 +501,8 @@ class AcadosInterface(SolverInterface):
                 acados.W_e = linalg.block_diag(
                     acados.W_e, np.diag([objectives.weight] * objectives.function[0].numel_out())
                 )
-                x = x if objectives.function[0].sparsity_in("x").shape != (0, 0) else []
-                u = u if objectives.function[0].sparsity_in("u").shape != (0, 0) else []
+                x = x if objectives.function[0].size_in("x") != (0, 0) else []
+                u = u if objectives.function[0].size_in("u") != (0, 0) else []
                 acados.mayer_costs_e = vertcat(
                     acados.mayer_costs_e, objectives.function[0](t, x, u, p, s).reshape((-1, 1))
                 )

bioptim/limits/penalty_helpers.py
@@ -66,13 +66,10 @@ class PenaltyHelpers:
 
         x = get_state_decision(penalty.phase, penalty.node_idx[penalty_node_idx])
         
-        if penalty.derivative:
-            x_next = get_state_decision(penalty.phase, penalty.node_idx[penalty_node_idx + 1])
-            return x.reshape((-1, 1)), x_next[:, 0].reshape((-1, 1))
-
-        if penalty.explicit_derivative:
+        if penalty.derivative or penalty.explicit_derivative:
             x = _reshape_to_vector(x)
-            x = vertcat(x, get_state_decision(penalty.phase, penalty.node_idx[penalty_node_idx] + 1)[:, 0])
+            x_next = get_state_decision(penalty.phase, penalty.node_idx[penalty_node_idx] + 1)[:, 0]
+            x = vertcat(x_next, x) if penalty.derivative else vertcat(x, x_next) 
 
         return _reshape_to_vector(x)
 
@@ -82,7 +79,7 @@ class PenaltyHelpers:
             _u = get_control_decision(_phase, _node)
             if nlp.phase_dynamics == PhaseDynamics.ONE_PER_NODE and _node >= nlp.n_controls_nodes:
                 if isinstance(_u, (MX, SX, DM)):
-                    return type(u)()
+                    return type(_u)()
                 elif isinstance(_u, np.ndarray):
                     return np.ndarray((0, 1))
                 else:
@@ -97,19 +94,17 @@ class PenaltyHelpers:
             for phase, node in zip(phases, nodes):
                 u.append(_reshape_to_vector(_get_control_internal(phase, node)))
             return _vertcat(u)
+
         u = _get_control_internal(penalty.phase, penalty.node_idx[penalty_node_idx])
 
-        if penalty.derivative:
-            raise NotImplementedError("Derivative is not implemented yet")
-
-        if penalty.integrate or penalty.explicit_derivative:
+        if penalty.integrate or penalty.derivative or penalty.explicit_derivative:
             u = _reshape_to_vector(u)
             
             next_node = penalty.node_idx[penalty_node_idx] + 1
             step = 0  # TODO: This should be 1 for integrate if TRAPEZOIDAL
             next_u = _get_control_internal(penalty.phase, next_node)
             if np.sum(next_u.shape) > 0:
-                u = vertcat(u, next_u[:, 0])
+                u = vertcat(next_u[:, 0], u) if penalty.derivative else vertcat(u, next_u[:, 0])
 
         return _reshape_to_vector(u)
 

tests/shard1/test__global_plots.py
@@ -241,16 +241,16 @@ def test_console_objective_functions(phase_dynamics):
                         .replace(".", "_")
                         .replace("__", "_")
                     )
-                    t = MX.sym("t", *p.weighted_function[node_index].sparsity_in("t").shape)
-                    phases_dt = MX.sym("dt", *p.weighted_function[node_index].sparsity_in("dt").shape)
-                    x = MX.sym("x", *p.weighted_function[node_index].sparsity_in("x").shape)
-                    u = MX.sym("u", *p.weighted_function[node_index].sparsity_in("u").shape)
-                    if p.weighted_function[node_index].sparsity_in("u").shape == (0, 0):
+                    t = MX.sym("t", *p.weighted_function[node_index].size_in("t"))
+                    phases_dt = MX.sym("dt", *p.weighted_function[node_index].size_in("dt"))
+                    x = MX.sym("x", *p.weighted_function[node_index].size_in("x"))
+                    u = MX.sym("u", *p.weighted_function[node_index].size_in("u"))
+                    if p.weighted_function[node_index].size_in("u") == (0, 0):
                         u = MX.sym("u", 3, 1)
-                    param = MX.sym("param", *p.weighted_function[node_index].sparsity_in("p").shape)
-                    s = MX.sym("s", *p.weighted_function[node_index].sparsity_in("s").shape)
-                    weight = MX.sym("weight", *p.weighted_function[node_index].sparsity_in("weight").shape)
-                    target = MX.sym("target", *p.weighted_function[node_index].sparsity_in("target").shape)
+                    param = MX.sym("param", *p.weighted_function[node_index].size_in("p"))
+                    s = MX.sym("s", *p.weighted_function[node_index].size_in("s"))
+                    weight = MX.sym("weight", *p.weighted_function[node_index].size_in("weight"))
+                    target = MX.sym("target", *p.weighted_function[node_index].size_in("target"))
 
                     p.function[node_index] = Function(
                         name, [t, phases_dt, x, u, param, s], [np.array([range(cmp, len(p.rows) + cmp)]).T]