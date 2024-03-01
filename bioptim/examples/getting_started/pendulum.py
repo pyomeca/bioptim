dd13d3c6c8e4b8eacb31dac097d6dd1158ced050
Charbie <eve.charbonneau.1@umontreal.ca>
2024-03-01 16:17:16 -0500

    moved it to another file

--- bioptim/examples/getting_started/pendulum.py
+++ bioptim/examples/getting_started/pendulum.py
@@ -150,47 +150,7 @@ def main():
 
     # --- Show the results (graph or animation) --- #
     # sol.graphs(show_bounds=True)
-    # sol.animate(n_frames=100)
-
-
-    # Create a figure and a set of subplots
-    fig, axs = plt.subplots(2, 2, figsize=(10, 15))
-
-    # Plotting the solution for decision  
-    decision_time = sol.decision_time(to_merge=SolutionMerge.NODES, time_alignment=TimeAlignment.STATES)
-    decision_states = sol.decision_states(to_merge=SolutionMerge.NODES)
-    for i in range(2):
-        axs[0, i].step(decision_time, decision_states["q"][i, :], label="Decision q", where='post')
-
-    # Retrieve stepwise states from the solution object.  
-    stepwise_time = sol.stepwise_time(to_merge=SolutionMerge.NODES, time_alignment=TimeAlignment.STATES)
-    stepwise_states = sol.stepwise_states(to_merge=SolutionMerge.NODES)
-    for i in range(2):
-        axs[1, i].plot(stepwise_time, stepwise_states["q"][i, :], label="Stepwise q")
-
-    # Plotting the solution for decision
-    decision_time = sol.decision_time(to_merge=SolutionMerge.NODES, time_alignment=TimeAlignment.CONTROLS)
-    decision_controls = sol.decision_controls(to_merge=SolutionMerge.NODES)
-    for i in range(2):
-        axs[0, i].step(decision_time, decision_controls["tau"][i, :], label="Decision tau", where='post')
-        axs[0, i].set_xlabel("Time [s]")
-        axs[0, i].grid(True)
-        axs[0, i].legend()
-
-    # Retrieve stepwise states from the solution object.
-    stepwise_time = sol.stepwise_time(to_merge=SolutionMerge.NODES, time_alignment=TimeAlignment.CONTROLS)
-    stepwise_controls = sol.stepwise_controls(to_merge=SolutionMerge.NODES)
-    for i in range(2):
-        axs[1, i].step(stepwise_time, stepwise_controls["tau"][i, :],  label="Stepwise tau", where='post')
-        axs[1, i].set_xlabel("Time [s]")
-        axs[1, i].grid(True)
-        axs[1, i].legend()
-
-    axs[0, 0].set_title("DoF 1")
-    axs[0, 1].set_title("DoF 2")
-    axs[0, 0].set_ylabel("Decision")
-    axs[1, 0].set_ylabel("Stepwise")
-    plt.show()
+    sol.animate(n_frames=100)
 
 if __name__ == "__main__":
     main()
