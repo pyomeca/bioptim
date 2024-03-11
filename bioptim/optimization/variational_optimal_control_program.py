68e6a0e21640369e1f959cd41f0cb84b886b2e32
Charbie <eve.charbonneau.1@umontreal.ca>
2024-03-11 14:27:44 -0400

    fixed tests variationnal ocp

--- bioptim/optimization/variational_optimal_control_program.py
+++ bioptim/optimization/variational_optimal_control_program.py
@@ -243,8 +243,7 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
             dynamics_dxdt = vertcat(*dynamics_dxdt)
 
         # Note: useless but needed to run bioptim as it need to test the size of xdot
-        nlp.dynamics_func = (
-            Function(
+        nlp.dynamics_func = Function(
                 "ForwardDyn",
                 [
                     vertcat(nlp.time_mx, nlp.dt_mx),
@@ -256,8 +255,7 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
                 [dynamics_dxdt],
                 ["t_span", "x", "u", "p", "a"],
                 ["xdot"],
-            ),
-        )
+            )
 
         dt = MX.sym("time_step")
         q_prev = MX.sym("q_prev", nlp.model.nb_q, 1)
@@ -289,8 +287,7 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
         else:
             lambdas = None
 
-        nlp.implicit_dynamics_func = (
-            Function(
+        nlp.implicit_dynamics_func = Function(
                 "ThreeNodesIntegration",
                 three_nodes_input,
                 [
@@ -305,11 +302,9 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
                         lambdas,
                     )
                 ],
-            ),
-        )
+            )
 
-        nlp.implicit_dynamics_func_first_node = (
-            Function(
+        nlp.implicit_dynamics_func_first_node = Function(
                 "TwoFirstNodesIntegration",
                 two_first_nodes_input,
                 [
@@ -323,11 +318,9 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
                         lambdas,
                     )
                 ],
-            ),
-        )
+            )
 
-        nlp.implicit_dynamics_func_last_node = (
-            Function(
+        nlp.implicit_dynamics_func_last_node = Function(
                 "TwoLastNodesIntegration",
                 two_last_nodes_input,
                 [
@@ -341,14 +334,13 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
                         lambdas,
                     )
                 ],
-            ),
-        )
+            )
 
         if expand:
-            nlp.dynamics_func = (nlp.dynamics_func.expand(),)
-            nlp.implicit_dynamics_func = (nlp.implicit_dynamics_func[0].expand(),)
-            nlp.implicit_dynamics_func_first_node = (nlp.implicit_dynamics_func_first_node[0].expand(),)
-            nlp.implicit_dynamics_func_last_node = (nlp.implicit_dynamics_func_last_node[0].expand(),)
+            nlp.dynamics_func = nlp.dynamics_func.expand()
+            nlp.implicit_dynamics_func = nlp.implicit_dynamics_func.expand()
+            nlp.implicit_dynamics_func_first_node = nlp.implicit_dynamics_func_first_node.expand()
+            nlp.implicit_dynamics_func_last_node = nlp.implicit_dynamics_func_last_node.expand()
 
     def configure_torque_driven(self, ocp: OptimalControlProgram, nlp: NonLinearProgram):
         """
@@ -401,7 +393,7 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
 
         """
         if self.bio_model.has_holonomic_constraints:
-            return controllers[0].get_nlp.implicit_dynamics_func[0](
+            return controllers[0].get_nlp.implicit_dynamics_func(
                 controllers[0].dt.cx,
                 controllers[0].states["q"].cx,
                 controllers[1].states["q"].cx,
@@ -412,7 +404,7 @@ class VariationalOptimalControlProgram(OptimalControlProgram):
                 controllers[1].states["lambdas"].cx,
             )
         else:
-            return controllers[0].get_nlp.implicit_dynamics_func[0](
+            return controllers[0].get_nlp.implicit_dynamics_func(
                 controllers[0].dt.cx,
                 controllers[0].states["q"].cx,
                 controllers[1].states["q"].cx,
