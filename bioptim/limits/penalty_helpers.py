419929ad5e274b1ba8bb567f62f4a78df9f5bf23
Charbie <eve.charbonneau.1@umontreal.ca>
2024-04-05 10:38:20 -0400

    better raise

--- bioptim/limits/penalty_helpers.py
+++ bioptim/limits/penalty_helpers.py
@@ -134,7 +134,7 @@ class PenaltyHelpers:
         if penalty.multinode_penalty:
             for i_phase in penalty.nodes_phase:
                 d = get_numerical_timeseries(penalty.nodes_phase[i_phase], node, 0)  # cx_start
-                if d.shape != (0, 0):
+                if d.shape[0] != 0:
                     raise NotImplementedError(
                         "Numerical data timeseries is not implemented for multinode penalties yet."
                     )
