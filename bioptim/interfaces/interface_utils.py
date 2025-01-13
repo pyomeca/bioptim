4dde0822961d57c8afee4d45bcab756cc9e65b42
Pariterre <pariterre@hotmail.com>
2025-01-13 15:55:15 -0500

    Finalized the torch example and added a test

--- bioptim/interfaces/interface_utils.py
+++ bioptim/interfaces/interface_utils.py
@@ -1,3 +1,4 @@
+import warnings
 from time import perf_counter
 
 from casadi import Importer, Function
@@ -186,6 +187,7 @@ def _shake_tree_for_penalties(ocp, penalties_cx: CX, v: CX, v_bounds: DoubleNpAr
             penalty = penalty.expand()
         except RuntimeError:
             # This happens mostly when, for instance, there is a Newton decent in the penalty
+            warnings.warn("Could not expand during the shake_tree.")
             pass
     return penalty(vertcat(*dt, v[len(dt) :]))
 
