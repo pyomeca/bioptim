f39d77ab6ac2f1a31dc3851633fd883a32c8bb14
Ipuch <pierre.puchaud@umontreal.ca>
2025-01-30 11:32:54 -0500

    black: proper version

--- resources/plotting_server.py
+++ resources/plotting_server.py
@@ -1,7 +1,7 @@
 """
 This file is an example of how to run a bioptim Online plotting server. Apart on Macos, this is usually not the way
 to run a bioptim server as it is easier to run it as an automatic multiprocess. This is achieved by setting
-`Solver.IPOPT(online_optim=OnlineOptim.MULTIPROCESS_SERVER)`. 
+`Solver.IPOPT(online_optim=OnlineOptim.MULTIPROCESS_SERVER)`.
 If set to OnlineOptim.SERVER, then the plotting server is mandatory.
 
 Since the server runs usings sockets, it is possible to run the server on a different machine than the one running the
