79f3628d16f9b17d2ab1bf97a2d407080542bc80
Ipuch <pierre.puchaud@umontreal.ca>
2025-02-05 12:48:10 -0500

    tests: different odesolver for the different phases

--- tests/shard1/test_prepare_all_examples.py
+++ tests/shard1/test_prepare_all_examples.py
@@ -1,9 +1,7 @@
-import os
-
-from bioptim import InterpolationType, PhaseDynamics
 import numpy as np
 import pytest
 
+from bioptim import InterpolationType, PhaseDynamics, OdeSolver
 from ..utils import TestUtils
 
 
@@ -288,6 +286,20 @@ def test__getting_started__example_multiphase():
         long_optim=True,
         phase_dynamics=PhaseDynamics.SHARED_DURING_THE_PHASE,
         expand_dynamics=False,
+    )
+
+
+def test__getting_started__example_multiphase_different_ode_solvers():
+    from bioptim.examples.getting_started import example_multiphase as ocp_module
+
+    bioptim_folder = TestUtils.module_folder(ocp_module)
+
+    ocp_module.prepare_ocp(
+        biorbd_model_path=bioptim_folder + "/models/cube.bioMod",
+        long_optim=True,
+        phase_dynamics=PhaseDynamics.SHARED_DURING_THE_PHASE,
+        expand_dynamics=False,
+        ode_solver=[OdeSolver.RK1(), OdeSolver.RK4(), OdeSolver.COLLOCATION()],
     )
 
 
