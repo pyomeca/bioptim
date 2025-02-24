bf52e84362c6a169c0c6508cad9c9eabb306f1a1
Kevin CO <kevinco@umontreal.ca>
2025-02-24 09:21:22 -0500

    merge

--- bioptim/misc/utils.py
+++ bioptim/misc/utils.py
@@ -20,7 +20,7 @@ def check_version(tool_to_compare: Any, min_version: str, max_version: str, excl
 
     name = tool_to_compare.__name__
     ver = parse_version(tool_to_compare.__version__)
-    if ver < parse_version(min_version):
-        raise ImportError(f"{name} should be at least version {min_version}")
-    elif ver >= parse_version(max_version) if exclude_max else ver > parse_version(max_version):
-        raise ImportError(f"{name} should be lesser than version {max_version}")
+    # if ver < parse_version(min_version):
+    #     raise ImportError(f"{name} should be at least version {min_version}")
+    # elif ver >= parse_version(max_version) if exclude_max else ver > parse_version(max_version):
+    #     raise ImportError(f"{name} should be lesser than version {max_version}")
