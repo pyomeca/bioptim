87f42e17600ba105d59dd141605d8827c17115fc
Andrew Prosikhin <andrewpros@mail.ru>
2025-02-25 16:00:21 -0500

    Reordered types.

--- bioptim/misc/parameters_types.py
+++ bioptim/misc/parameters_types.py
@@ -3,22 +3,26 @@ import numpy as np
 
 
 AnyIterable = list[Any] | tuple[Any]
+IntIterable = list[int] | tuple[int]
+StrIterable = list[str] | tuple[str]
+
+StrIterableOptional = list[str] | tuple[str] | None
+
 AnyDict = dict[str, Any]
-AnyDictOptional = dict[str, Any] | None
 IntDict = dict[str, int]
 
+AnyDictOptional = dict[str, Any] | None
+
 AnyList = list[Any]
-AnyListOrNpArray = list[Any] | np.ndarray
 IntList = list[int]
+AnyListOrNpArray = list[Any] | np.ndarray
+
 IntListOptional = list[int] | None
 
-IntIterable = list[int] | tuple[int]
-
-StrIterable = list[str] | tuple[str]
-StrIterableOptional = list[str] | tuple[str] | None
 StrOrIterable = str | list[str]
 
 IntOptional = int | None
+
 StrOptional = str | None
 
 AnyTuple = tuple[Any]
