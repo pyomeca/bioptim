0966ba9f2144cf0cda9752ec16e9347092f5e491
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-03-05 14:42:52 +0400

    fixed the graphs on the existing examples

--- bioptim/examples/muscle_driven_with_contact/contact_forces_inequality_constraint_muscle.py
+++ bioptim/examples/muscle_driven_with_contact/contact_forces_inequality_constraint_muscle.py
@@ -114,6 +114,7 @@ def main():
     biorbd_model_path = "models/2segments_4dof_2contacts_1muscle.bioMod"
     t = 0.3
     ns = 10
+    dt = t / ns
     ocp = prepare_ocp(
         biorbd_model_path=biorbd_model_path,
         phase_time=t,
@@ -134,7 +135,9 @@ def main():
 
     x = np.concatenate((q, qdot))
     u = np.concatenate((tau, mus))
-    contact_forces = np.array(nlp.contact_forces_func(x[:, :-1], u[:, :-1], []))
+    contact_forces = np.zeros((3, nlp.ns))
+    for i_node in range(nlp.ns):
+        contact_forces[:, i_node] = np.reshape(np.array(nlp.contact_forces_func([dt*i_node, dt*(i_node+1)], x[:, i_node], u[:, i_node], [], [], [])), (3, ))
 
     names_contact_forces = ocp.nlp[0].model.contact_names
     for i, elt in enumerate(contact_forces):
@@ -146,7 +149,6 @@ def main():
 
     # --- Show results --- #
     sol.animate()
-
 
 if __name__ == "__main__":
     main()
