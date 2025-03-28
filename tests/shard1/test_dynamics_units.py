25f5179495a49871f74d25961cf0038994418e83
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-03-28 09:55:10 +0400

    blacked

--- tests/shard1/test_dynamics_units.py
+++ tests/shard1/test_dynamics_units.py
@@ -14,10 +14,12 @@ class MockData:
 
 
 from bioptim.examples.muscle_driven_with_contact import contact_forces_inverse_dynamics_constraint_muscle as ocp_module
+
 BIOPTIM_FOLDER = TestUtils.module_folder(ocp_module)
 MODEL_RIGID_CONTACT = BiorbdModel(BIOPTIM_FOLDER + "/models/2segments_4dof_2contacts_1muscle.bioMod")
 MODEL_SOFT_CONTACT = BiorbdModel(BIOPTIM_FOLDER + "/models/2segments_4dof_2soft_contacts_1muscle.bioMod")
 from bioptim.examples.getting_started import pendulum as ocp_module
+
 BIOPTIM_FOLDER = TestUtils.module_folder(ocp_module)
 MODEL_NO_CONTACT = BiorbdModel(BIOPTIM_FOLDER + "/models/pendulum.bioMod")
 
@@ -59,11 +61,14 @@ def test_check_external_forces_format_wrong_length():
 def test_check_contacts_in_biomodel_with_rigid_contact_explicit():
     _check_contacts_in_biomodel([ContactType.RIGID_EXPLICIT], MODEL_RIGID_CONTACT, 0)
 
+
 def test_check_contacts_in_biomodel_with_rigid_contact_implicit():
     _check_contacts_in_biomodel([ContactType.RIGID_IMPLICIT], MODEL_RIGID_CONTACT, 0)
 
+
 def test_check_contacts_in_biomodel_with_soft_contact_explicit():
     _check_contacts_in_biomodel([ContactType.SOFT_EXPLICIT], MODEL_SOFT_CONTACT, 0)
+
 
 def test_check_contacts_in_biomodel_with_soft_contact_implicit():
     _check_contacts_in_biomodel([ContactType.SOFT_IMPLICIT], MODEL_SOFT_CONTACT, 0)
@@ -73,13 +78,16 @@ def test_check_contacts_in_biomodel_no_contact_but_flag_true_rigid_explicit():
     with pytest.raises(ValueError):
         _check_contacts_in_biomodel([ContactType.RIGID_EXPLICIT], MODEL_NO_CONTACT, 0)
 
+
 def test_check_contacts_in_biomodel_no_contact_but_flag_true_rigid_implicit():
     with pytest.raises(ValueError):
         _check_contacts_in_biomodel([ContactType.RIGID_IMPLICIT], MODEL_NO_CONTACT, 0)
 
+
 def test_check_contacts_in_biomodel_no_contact_but_flag_true_soft_explicit():
     with pytest.raises(ValueError):
         _check_contacts_in_biomodel([ContactType.SOFT_EXPLICIT], MODEL_NO_CONTACT, 0)
+
 
 def test_check_contacts_in_biomodel_no_contact_but_flag_true_soft_implicit():
     with pytest.raises(ValueError):
@@ -89,8 +97,10 @@ def test_check_contacts_in_biomodel_no_contact_but_flag_true_soft_implicit():
 def test_check_contacts_in_biomodel_no_contact_and_flag_false():
     _check_contacts_in_biomodel([], MODEL_NO_CONTACT, 0)
 
+
 def test_check_contacts_in_biomodel_rigid_contact_and_flag_false():
     _check_contacts_in_biomodel([], MODEL_RIGID_CONTACT, 0)
+
 
 def test_check_contacts_in_biomodel_soft_contact_and_flag_false():
     _check_contacts_in_biomodel([], MODEL_SOFT_CONTACT, 0)
