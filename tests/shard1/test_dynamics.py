8eb037cd1f474ddb03b400b3777d1a9457f4b40a
Charbie <eve.charbonneau.1@umontreal.ca>
2025-04-01 13:20:28 +0400

    blacked

--- tests/shard1/test_dynamics.py
+++ tests/shard1/test_dynamics.py
@@ -298,7 +298,9 @@ def test_torque_derivative_driven(with_contact, with_external_force, cx, phase_d
     numerical_timeseries = None
     if with_external_force:
         external_forces = ExternalForceSetTimeSeries(nb_frames=nlp.ns)
-        external_forces.add("force0", "Seg0", EXTERNAL_FORCE_ARRAY[:6, :], point_of_application=EXTERNAL_FORCE_ARRAY[6:, :])
+        external_forces.add(
+            "force0", "Seg0", EXTERNAL_FORCE_ARRAY[:6, :], point_of_application=EXTERNAL_FORCE_ARRAY[6:, :]
+        )
         numerical_timeseries = {"external_forces": external_forces.to_numerical_time_series()}
 
     nlp.model = BiorbdModel(
@@ -584,7 +586,9 @@ def test_torque_activation_driven(with_contact, with_external_force, cx, phase_d
     numerical_timeseries = None
     if with_external_force:
         external_forces = ExternalForceSetTimeSeries(nb_frames=nlp.ns)
-        external_forces.add("force0", "Seg0", EXTERNAL_FORCE_ARRAY[:6, :], point_of_application=EXTERNAL_FORCE_ARRAY[6:, :])
+        external_forces.add(
+            "force0", "Seg0", EXTERNAL_FORCE_ARRAY[:6, :], point_of_application=EXTERNAL_FORCE_ARRAY[6:, :]
+        )
         numerical_timeseries = {"external_forces": external_forces.to_numerical_time_series()}
 
     nlp.model = BiorbdModel(
@@ -708,7 +712,9 @@ def test_torque_activation_driven_with_residual_torque(
     numerical_timeseries = None
     if with_external_force:
         external_forces = ExternalForceSetTimeSeries(nb_frames=nlp.ns)
-        external_forces.add("force0", "Seg0", EXTERNAL_FORCE_ARRAY[:6, :], point_of_application=EXTERNAL_FORCE_ARRAY[6:, :])
+        external_forces.add(
+            "force0", "Seg0", EXTERNAL_FORCE_ARRAY[:6, :], point_of_application=EXTERNAL_FORCE_ARRAY[6:, :]
+        )
         numerical_timeseries = {"external_forces": external_forces.to_numerical_time_series()}
 
     nlp.model = BiorbdModel(
