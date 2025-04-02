937681afb43755893361f06beabdc06daf25bf42
Charbie <eve.charbonneau.1@umontreal.ca>
2025-04-02 10:21:43 +0400

    coud not remove it

--- bioptim/dynamics/ode_solvers.py
+++ bioptim/dynamics/ode_solvers.py
@@ -163,6 +163,19 @@ class OdeSolver:
             out += nlp.states.scaled.cx_intermediates_list
             return out
 
+        def p_ode(self, nlp):
+            if nlp.control_type in (
+                    ControlType.CONSTANT,
+                    ControlType.CONSTANT_WITH_LAST_NODE,
+            ):
+                return nlp.controls.scaled.cx_start
+            elif nlp.control_type == ControlType.LINEAR_CONTINUOUS:
+                return horzcat(nlp.controls.scaled.cx_start, nlp.controls.scaled.cx_end)
+            elif nlp.control_type == ControlType.NONE:
+                return nlp.cx()
+            else:
+                raise NotImplementedError(f"The control_type {nlp.control_type} is not implemented.")
+
         def a_ode(self, nlp):
             out = [nlp.algebraic_states.scaled.cx_start]
             if not self.duplicate_starting_point:
