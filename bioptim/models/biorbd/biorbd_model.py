b7a7235e42a44cd6bce55136f4b509472b49a463
Charbie <eve.charbonneau.1@umontreal.ca>
2025-04-02 11:37:47 +0400

    blacked

--- bioptim/models/biorbd/biorbd_model.py
+++ bioptim/models/biorbd/biorbd_model.py
@@ -565,7 +565,9 @@ class BiorbdModel:
                 self.external_forces[force_slicer],
                 point_of_application_mx,
             )
-            symbolic_counter = force_slicer.stop + (3 if isinstance(force_characteristics["point_of_application"], np.ndarray) else 0)
+            symbolic_counter = force_slicer.stop + (
+                3 if isinstance(force_characteristics["point_of_application"], np.ndarray) else 0
+            )
 
         return symbolic_counter
 
