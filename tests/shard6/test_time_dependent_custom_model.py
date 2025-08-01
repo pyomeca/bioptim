b8cfec6e2495a68763d07b59cce56dd671184576
ipuch <pierre.puchaud@inria.fr>
2025-08-01 11:41:28 +0200

    fix tests again

--- tests/shard6/test_time_dependent_custom_model.py
+++ tests/shard6/test_time_dependent_custom_model.py
@@ -22,7 +22,7 @@ from bioptim import (
     PhaseDynamics,
     SolutionMerge,
     VariableScaling,
-    StateSpaceDynamics,
+    AbstractStateSpaceDynamics,
 )
 from casadi import DM, MX, SX, vertcat, exp
 import numpy as np
@@ -32,7 +32,7 @@ import pytest
 from ..utils import TestUtils
 
 
-class Model(StateSpaceDynamics):
+class Model(AbstractStateSpaceDynamics):
     def __init__(self, time_as_states: bool = False):
         super().__init__()
         self._name = None
